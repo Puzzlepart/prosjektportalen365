16b4b0f50ecdda5e35b7513a51f68dfb3ac8fa84
olemp <olemp@puzzlepart.com>
2019-04-08 16:45:29 +0200

    DeliveriesOverview: Fixed columns

--- SharePointFramework/@Shared/lib/helpers/formatDate.js
+++ SharePointFramework/@Shared/lib/helpers/formatDate.js
@@ -1,11 +1,20 @@
-export default function (dateStr) {
-    return new Date(dateStr).toLocaleString("nb-NO", {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-    });
+/**
+ * Format date
+ *
+ * @param {string} dateStr Date string
+ * @param {string} fallback Fallback if date is invalid
+ */
+export default function (dateStr, fallback) {
+    if (fallback === void 0) { fallback = ''; }
+    return dateStr ?
+        new Date(dateStr).toLocaleString("nb-NO", {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        })
+        : fallback;
 };
 //# sourceMappingURL=formatDate.js.map
