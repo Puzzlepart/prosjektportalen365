66045b2847cea3751642d5ee7e0c1eddc2aa6711
olemp <olemp@puzzlepart.com>
2019-04-08 16:42:33 +0200

    formatDate supports fallback

--- SharePointFramework/@Shared/src/helpers/formatDate.ts
+++ SharePointFramework/@Shared/src/helpers/formatDate.ts
@@ -1,10 +1,18 @@
-export default (dateStr: string): string => {
-    return new Date(dateStr).toLocaleString("nb-NO", {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-    });
+/**
+ * Format date
+ * 
+ * @param {string} dateStr Date string
+ * @param {string} fallback Fallback if date is invalid
+ */
+export default (dateStr: string, fallback: string = ''): string => {
+    return dateStr ?
+        new Date(dateStr).toLocaleString("nb-NO", {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        })
+        : fallback;
 };
