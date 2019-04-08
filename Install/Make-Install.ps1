b55a08cc00bd5099e87579b60532da5c36765da5
olemp <olemp@puzzlepart.com>
2019-04-08 12:38:05 +0200

    Minor adjustments to Make-Install PS script

--- Install/Make-Install.ps1
+++ Install/Make-Install.ps1
@@ -5,23 +5,24 @@ mkdir "$ReleasePath/Templates" >$null 2>&1
 mkdir "$ReleasePath/SiteScripts" >$null 2>&1
 mkdir "$ReleasePath/Apps" >$null 2>&1
 
-Write-Host "[INFO] Building release $($PackageJson.version)"
+Write-Host "[INFO] Building release v$($PackageJson.version)"
 
 Write-Host "[INFO] Copying resources"
 Copy-Item -Path "$PSScriptRoot/../SiteScripts/Src/" -Filter *.txt -Destination "$ReleasePath/SiteScripts" -Recurse
 Copy-Item -Path "$PSScriptRoot/Install.ps1" -Destination $ReleasePath
 
 foreach ($Solution in @("PortfolioWebParts", "ProjectExtensions", "ProjectWebParts")) {
-    Write-Host "[INFO] Building SharePoint Framework solution $($Solution)"
     Set-Location "$PSScriptRoot\..\SharePointFramework\$Solution"
+    $PackageSolutionJson = Get-Content "./config/package-solution.json" -Raw | ConvertFrom-Json
+    Write-Host "[INFO] Building SharePoint Framework solution [$($Solution)] [v$($PackageSolutionJson.solution.version)]"
     npm run-script package >$null 2>&1
     Get-ChildItem "./sharepoint/solution/" *.sppkg -Recurse | ?{-not ($_.PSIsContainer -or (Test-Path "$ReleasePath/Apps/$_"))} | Copy-Item -Destination "$ReleasePath/Apps"
 }
 
 Set-Location $PSScriptRoot
 
-Write-Host "[INFO] Building Portal PnP template"
+Write-Host "[INFO] Building [Portal] PnP template"
 Convert-PnPFolderToProvisioningTemplate -Out "$ReleasePath/Templates/Portal.pnp" -Folder "$PSScriptRoot/../Templates/Portal" -Force
 
 
-Write-Host "[INFO] Done building release $($PackageJson.version)"
+Write-Host "[INFO] Done building release v$($PackageJson.version)"
