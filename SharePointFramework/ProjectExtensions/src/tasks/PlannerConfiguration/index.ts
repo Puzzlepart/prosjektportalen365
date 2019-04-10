89cda780b1d55d97f832a68fd83c7f9462896cd0
olemp <olemp@puzzlepart.com>
2019-04-10 11:19:28 +0200

    PlannerConfiguration

--- SharePointFramework/ProjectExtensions/src/tasks/PlannerConfiguration/index.ts
+++ SharePointFramework/ProjectExtensions/src/tasks/PlannerConfiguration/index.ts
@@ -22,51 +22,54 @@ export default class PlannerConfiguration extends BaseTask {
         super(PlannerConfiguration.taskName);
     }
 
+    private async ensurePlan(title: string, existingPlans: IPlannerPlan[], owner: string) {
+        let [plan] = existingPlans.filter(p => p.title === title);
+        if (!plan) {
+            plan = await MSGraphHelper.Post(`planner/plans`, JSON.stringify({ title, owner }));
+        }
+        return plan;
+    }
+
     private async createBucket(name: string, planId: string) {
-        return await MSGraphHelper.Post('planner/buckets', JSON.stringify({
-            name,
-            planId: planId,
-            orderHint: " !"
-        }));
+        return await MSGraphHelper.Post('planner/buckets', JSON.stringify({ name, planId, orderHint: " !" }));
+    }
+
+    private async createTasks(tasks: string[], planId: string, bucketId: string) {
+        for (let i = 0; i < tasks.length; i++) {
+            await MSGraphHelper.Post('planner/tasks', JSON.stringify({ title: tasks[i], bucketId, planId }));
+        }
     }
 
     @override
     public async execute(params: IBaseTaskParams, _onProgress: OnProgressCallbackFunction): Promise<IBaseTaskParams> {
-        Logger.log({ message: '(ProjectSetupApplicationCustomizer) PlannerConfiguration', level: LogLevel.Info });
-        const plannerTasks = await (await fetch(`${params.data.hub.url}/Konfigurasjonsfiler/Planneroppgaver.txt`, { credentials: 'include' })).json();
-        let groupPlans = [];
+        Logger.log({ message: '(ProjectSetupApplicationCustomizer) PlannerConfiguration: Setting up Plans, Buckets and Task', level: LogLevel.Info });
         try {
-            groupPlans = await MSGraphHelper.Get<IPlannerPlan[]>(`groups/${params.context.pageContext.legacyPageContext.groupId}/planner/plans`, ['id', 'title']);
+            const plannerTasks = await (await fetch(`${params.data.hub.url}/Konfigurasjonsfiler/Planneroppgaver.txt`, { credentials: 'include' })).json();
+            let existingGroupPlans = [];
+            try {
+                existingGroupPlans = await MSGraphHelper.Get<IPlannerPlan[]>(`groups/${params.context.pageContext.legacyPageContext.groupId}/planner/plans`, ['id', 'title']);
+            } catch (error) {
+                existingGroupPlans = [];
+            }
+            const plans = Object.keys(plannerTasks);
+            for (let i = 0; i < plans.length; i++) {
+                let groupPlan = await this.ensurePlan(plans[i], existingGroupPlans, params.context.pageContext.legacyPageContext.groupId);
+                let planBuckets = await MSGraphHelper.Get<IPlannerBucket[]>(`planner/plans/${groupPlan.id}/buckets`, ['id', 'name', 'planId']);
+                let [defaultPlanBucket] = planBuckets;
+                if (!defaultPlanBucket) {
+                    defaultPlanBucket = await this.createBucket('Ikke startet', groupPlan.id);
+                }
+                try {
+                    await Promise.all([
+                        this.createBucket('Pågår', groupPlan.id),
+                        this.createBucket('Utsatt', groupPlan.id),
+                        this.createBucket('Venter på andre', groupPlan.id),
+                    ]);
+                } catch (error) { }
+                await this.createTasks(plannerTasks[plans[i]], groupPlan.id, defaultPlanBucket.id);
+            }
         } catch (error) {
-            groupPlans = [];
-        }
-        const plans = Object.keys(plannerTasks);
-        for (let i = 0; i < plans.length; i++) {
-            let [groupPlan] = groupPlans.filter(p => p.title === plans[i]);
-            if (!groupPlan) {
-                groupPlan = await MSGraphHelper.Post(`planner/plans`, JSON.stringify({
-                    title: plans[i],
-                    owner: params.context.pageContext.legacyPageContext.groupId,
-                }));
-            }
-            let planBuckets = await MSGraphHelper.Get<IPlannerBucket[]>(`planner/plans/${groupPlan.id}/buckets`, ['id', 'name', 'planId']);
-            let [defaultPlanBucket] = planBuckets;
-            if (!defaultPlanBucket) {
-                defaultPlanBucket = await this.createBucket('Ikke startet', groupPlan.id);
-            }
-            await Promise.all([
-                this.createBucket('Pågår', groupPlan.id),
-                this.createBucket('Utsatt', groupPlan.id),
-                this.createBucket('Venter på andre', groupPlan.id),
-            ]);
-            let planTasks = plannerTasks[plans[i]];
-            for (let j = 0; j < planTasks.length; j++) {
-                await MSGraphHelper.Post('planner/tasks', JSON.stringify({
-                    title: planTasks[j],
-                    bucketId: defaultPlanBucket.id,
-                    planId: groupPlan.id,
-                }));
-            }
+            Logger.log({ message: '(ProjectSetupApplicationCustomizer) PlannerConfiguration: Failed to set up Plans, Buckets and Tasks', level: LogLevel.Warning });
         }
         return params;
     }
