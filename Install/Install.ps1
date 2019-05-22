6e3163611ba7c4a6bc6721e9f61a6eaf7c0c3577
olemp <olemp@puzzlepart.com>
2019-05-22 09:44:56 +0200

    Install: Using Select-Object -Last 1 to get Alias

--- Install/Install.ps1
+++ Install/Install.ps1
@@ -46,7 +46,7 @@ function Connect-SharePoint {
 }
 
 [System.Uri]$Uri = $Url
-$Alias = $Uri.Segments[$Uri.Segments.Length - 1]
+$Alias = $Uri.Segments | Select-Object -Last 1
 $AdminSiteConnection = $null
 $AppCatalogSiteConnection = $null
 $SiteConnection = $null
