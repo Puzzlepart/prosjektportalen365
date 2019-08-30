2e6f6b53948510b5cf841ec05a34c1fcb44b5690
olemp <olemp@puzzlepart.com>
2019-08-30 11:15:29 +0200

    Turned of AI

--- SharePointFramework/@Shared/src/logging/ApplicationInsightsLogListener.ts
+++ SharePointFramework/@Shared/src/logging/ApplicationInsightsLogListener.ts
@@ -1,5 +1,5 @@
-import { LogListener, LogEntry } from '@pnp/logging';
-// import { AppInsights } from 'applicationinsights-js';
+import { LogListener, LogEntry, LogLevel } from '@pnp/logging';
+import { AppInsights } from 'applicationinsights-js';
 import { PageContext } from '@microsoft/sp-page-context';
 
 export class ApplicationInsightsLogListener implements LogListener {
@@ -9,31 +9,31 @@ export class ApplicationInsightsLogListener implements LogListener {
    * @param {PageContext} pageContext Page context
    */
   constructor(public pageContext: PageContext) {
-    // AppInsights.downloadAndSetup({ instrumentationKey: '3dcad20d-b2f7-4855-9421-cb128509c352' });
+    // AppInsights.downloadAndSetup({ instrumentationKey: null });
     // AppInsights.trackPageView();
   }
 
   /**
    * Log an entry
    * 
-   * @param {LogEntry} entry Entry
+   * @param {LogEntry} _entry Entry
    */
   public log(_entry: LogEntry) {
-    // AppInsights.trackTrace(entry.message, { webAbsoluteUrl: this.pageContext.web.absoluteUrl }, this._getAiSeverityLevel(entry));
+    // AppInsights.trackTrace(entry.message, {}, this._getAiSeverityLevel(entry));
   }
 
-  // /**
-  //  * Get AI severity level
-  //  * 
-  //  * @param {LogEntry} entry Entry
-  //  */
-  // private _getAiSeverityLevel(entry: LogEntry): AI.SeverityLevel {
-  //   switch (entry.level) {
-  //     case LogLevel.Error: return 3;
-  //     case LogLevel.Info: return 1;
-  //     case LogLevel.Verbose: return 0;
-  //     case LogLevel.Warning: return 2;
-  //     default: return 0;
-  //   }
-  //}
+  /**
+   * Get AI severity level
+   * 
+   * @param {LogEntry} entry Entry
+   */
+  private _getAiSeverityLevel(entry: LogEntry): AI.SeverityLevel {
+    switch (entry.level) {
+      case LogLevel.Error: return 3;
+      case LogLevel.Info: return 1;
+      case LogLevel.Verbose: return 0;
+      case LogLevel.Warning: return 2;
+      default: return 0;
+    }
+  }
 }
