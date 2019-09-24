f241a867a971a7bf128a0b8a41bbdb88e7c1bb70
olemp <olemp@puzzlepart.com>
2019-09-24 09:54:54 +0200

    .

--- SharePointFramework/ProjectWebParts/src/components/ProjectInformation/ProgressBar/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformation/ProgressBar/index.tsx
@@ -1,10 +1,9 @@
-import { stringIsNullOrEmpty } from '@pnp/common';
 import { IProgressIndicatorProps, ProgressIndicator } from 'office-ui-fabric-react/lib/ProgressIndicator';
 import * as React from 'react';
 
 // tslint:disable-next-line: naming-convention
 export const ProgressBar = (props: IProgressIndicatorProps) => {
-    if (stringIsNullOrEmpty(props.label.toString())) return null;
+    if (!props.label) return null;
     return (
         <div className={props.className}>
             <ProgressIndicator label={props.label} description={props.description} />
