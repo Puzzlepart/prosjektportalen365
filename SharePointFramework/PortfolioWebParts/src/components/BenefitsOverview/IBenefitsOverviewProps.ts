ffb1a16de26c91288cf8b948db88784ef8245610
olemp <olemp@puzzlepart.com>
2019-10-04 14:47:24 +0200

    Updated config.json for PortfolioWebParts

--- SharePointFramework/PortfolioWebParts/src/components/BenefitsOverview/IBenefitsOverviewProps.ts
+++ SharePointFramework/PortfolioWebParts/src/components/BenefitsOverview/IBenefitsOverviewProps.ts
@@ -6,3 +6,32 @@ export interface IBenefitsOverviewProps  extends IAggregatedSearchListProps {
      */
     hiddenColumns?: string[];
 }
+
+// tslint:disable-next-line: naming-convention
+export const BenefitsOverviewDefaultProps: Partial<IBenefitsOverviewProps> = {
+    selectProperties: [
+        'Path',
+        'SPWebURL',
+        'Title',
+        'ListItemId',
+        'SiteTitle',
+        'SiteId',
+        'ContentTypeID',
+        'GtDesiredValueOWSNMBR',
+        'GtMeasureIndicatorOWSTEXT',
+        'GtMeasurementUnitOWSCHCS',
+        'GtStartValueOWSNMBR',
+        'GtMeasurementValueOWSNMBR',
+        'GtMeasurementCommentOWSMTXT',
+        'GtMeasurementDateOWSDATE',
+        'GtGainsResponsibleOWSUSER',
+        'GtGainsTurnoverOWSMTXT',
+        'GtGainsTypeOWSCHCS',
+        'GtPrereqProfitAchievementOWSMTXT',
+        'GtRealizationTimeOWSDATE',
+        'GtGainLookupId',
+        'GtMeasureIndicatorLookupId',
+        'GtGainsResponsible'
+      ],
+      showExcelExportButton: true,
+};
