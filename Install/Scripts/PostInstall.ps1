e2125d58b5be5e5b24e94239b47d5d313bd1545d
olemp <olemp@puzzlepart.com>
2019-10-10 17:50:10 +0200

    PostInstall: Not enabling content types for Prosjekter to ease adding additional fields

---
+++ Install/Scripts/PostInstall.ps1
@@ -0,0 +1,14 @@
+Param(
+    [Parameter(Mandatory = $true)]
+    $Connection
+)
+
+Set-PnPList -Identity Prosjektstatus -EnableContentTypes:$false -Connection $Connection
+Set-PnPList -Identity Prosjektkolonnekonfigurasjon -EnableContentTypes:$false -Connection $Connection
+Set-PnPList -Identity Fasesjekkliste -EnableContentTypes:$false -Connection $Connection
+Set-PnPList -Identity Konfigurasjon -EnableContentTypes:$false -Connection $Connection
+Set-PnPList -Identity Listeinnhold -EnableContentTypes:$false -Connection $Connection
+Set-PnPList -Identity Porteføljevisninger -EnableContentTypes:$false -Connection $Connection
+Set-PnPList -Identity Prosjektkolonner -EnableContentTypes:$false -Connection $Connection
+Set-PnPList -Identity Ressursallokering -EnableContentTypes:$false -Connection $Connection
+Set-PnPList -Identity Statusseksjoner -EnableContentTypes:$false -Connection $Connection
