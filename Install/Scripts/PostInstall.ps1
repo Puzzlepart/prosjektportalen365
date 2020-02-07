d9813ff26d53abb28711bda94d6ae1b963a3d43c
olemp <olemp@puzzlepart.com>
2020-02-07 10:22:00 +0100

    Initial commit

--- Install/Scripts/PostInstall.ps1
+++ Install/Scripts/PostInstall.ps1
@@ -2,7 +2,7 @@ Set-PnPList -Identity Prosjektstatus -EnableContentTypes:$false
 Set-PnPList -Identity Prosjektkolonnekonfigurasjon -EnableContentTypes:$false 
 Set-PnPList -Identity Fasesjekkliste -EnableContentTypes:$false 
 Set-PnPList -Identity Konfigurasjon -EnableContentTypes:$false 
-Set-PnPList -Identity Listeinnhold -EnableContentTypes:$false 
 Set-PnPList -Identity Porteføljevisninger -EnableContentTypes:$false 
 Set-PnPList -Identity Prosjektkolonner -EnableContentTypes:$false 
 Set-PnPList -Identity Ressursallokering -EnableContentTypes:$false 
+Set-PnPList -Identity Planneroppgaver -EnableContentTypes:$false 
