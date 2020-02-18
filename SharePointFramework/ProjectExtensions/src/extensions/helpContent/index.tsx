b82f2252d2395546cdac7ded8b9f8b64f427eadc
olemp <olemp@puzzlepart.com>
2020-02-18 19:40:26 +0100

    Misc TS syntax fixes

--- SharePointFramework/ProjectExtensions/src/extensions/helpContent/index.tsx
+++ SharePointFramework/ProjectExtensions/src/extensions/helpContent/index.tsx
@@ -19,13 +19,12 @@ export default class HelpContentApplicationCustomizer extends BaseApplicationCus
     if (!this._topPlaceholder.domElement) return;
     await this._render();
     this.context.application.navigatedEvent.add(this, this._render);
-
   }
 
   /**
    * Render function
    */
-  private async _render() {
+  private async _render(): Promise<void> {
     let helpContent = await this._getHelpContent(this.properties.listName);
     let helpContentId = 'pp-help-content';
     let helpContentPlaceholder = document.getElementById(helpContentId);
@@ -43,10 +42,10 @@ export default class HelpContentApplicationCustomizer extends BaseApplicationCus
    * 
    * @param {string} listName Name of the list
    */
-  private async _getHelpContent(listName: string) {
+  private async _getHelpContent(listName: string): Promise<HelpContentModel[]> {
     let hub = await HubSiteService.GetHubSite(sp, this.context.pageContext);
     let portal = new PortalDataService().configure({ urlOrWeb: hub.web });
-    let items = await portal.getItems(listName, HelpContentModel, { ViewXml: '<View><Query><OrderBy><FieldRef Name="SortOrder" /></OrderBy><Query></View>' });
+    let items = await portal.getItems(listName, HelpContentModel, { ViewXml: '<View><Query><OrderBy><FieldRef Name="SortOrder" /></OrderBy></Query></View>' });
     items = items.filter(i => i.matchPattern(window.location.pathname)).splice(0, 3);
     for (let i = 0; i < items.length; i++) {
       if (items[i].externalUrl) {
