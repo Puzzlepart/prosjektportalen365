11454a6238bb59f62e1f345a57e3a0dd28543121
olemp <olemp@puzzlepart.com>
2020-02-19 13:54:36 +0100

    Adding consts for content type ids in BenefitsOverview

--- SharePointFramework/PortfolioWebParts/src/components/BenefitsOverview/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/BenefitsOverview/index.tsx
@@ -5,6 +5,10 @@ import styles from './BenefitsOverview.module.scss';
 import { getColumns } from './BenefitsOverviewColumns';
 import { BenefitsOverviewDefaultProps, IBenefitsOverviewProps } from './IBenefitsOverviewProps';
 
+const CONTENT_TYPE_ID_BENEFITS = '0x01004F466123309D46BAB9D5C6DE89A6CF67';
+const CONTENT_TYPE_ID_MEASUREMENTS = '0x010039EAFDC2A1624C1BA1A444FC8FE85DEC';
+const CONTENT_TYPE_ID_INDICATORS = '0x010073043EFE3E814A2BBEF96B8457623F95';
+
 export class BenefitsOverview extends React.Component<IBenefitsOverviewProps, {}> {
   public static defaultProps = BenefitsOverviewDefaultProps;
 
@@ -27,16 +31,16 @@ export class BenefitsOverview extends React.Component<IBenefitsOverviewProps, {}
    */
   private _postTransform(results: any[]): any[] {
     const benefits = results
-      .filter(res => res.ContentTypeID.indexOf('0x01004F466123309D46BAB9D5C6DE89A6CF67') === 0)
+      .filter(res => res.ContentTypeID.indexOf(CONTENT_TYPE_ID_BENEFITS) === 0)
       .map(res => new Benefit(res));
 
     const measurements = results
-      .filter(res => res.ContentTypeID.indexOf('0x010039EAFDC2A1624C1BA1A444FC8FE85DEC') === 0)
+      .filter(res => res.ContentTypeID.indexOf(CONTENT_TYPE_ID_MEASUREMENTS) === 0)
       .map(res => new BenefitMeasurement(res))
       .sort((a, b) => b.date.getTime() - a.date.getTime());
 
     const indicactors = results
-      .filter(res => res.ContentTypeID.indexOf('0x010073043EFE3E814A2BBEF96B8457623F95') === 0)
+      .filter(res => res.ContentTypeID.indexOf(CONTENT_TYPE_ID_INDICATORS) === 0)
       .map(res => {
         let indicator = new BenefitMeasurementIndicator(res)
           .setMeasurements(measurements)
