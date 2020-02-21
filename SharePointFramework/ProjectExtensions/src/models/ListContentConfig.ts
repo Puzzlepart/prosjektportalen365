b7f4f80888d3d8d6c392ebe7aba074e21f07d562
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-02-21 13:28:24 +0100

    Support for description for list content configurations (#240)
    Co-authored-by: Tarjei E. Ormestøyl <ormestoyl@gmail.com>

--- SharePointFramework/ProjectExtensions/src/models/ListContentConfig.ts
+++ SharePointFramework/ProjectExtensions/src/models/ListContentConfig.ts
@@ -1,12 +1,13 @@
 import { stringIsNullOrEmpty } from '@pnp/common';
 import { sp, Web, List } from '@pnp/sp';
 import { IListProperties } from './IListProperties';
-
+import { IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
 
 export interface IListContentConfigSPItem {
+    ContentTypeId: string;
     Id: number;
     Title: string;
-    ContentTypeId: string;
+    GtDescription: string;
     GtLccDestinationList: string;
     GtLccFields: string;
     GtLccDefault: boolean;
@@ -22,9 +23,11 @@ export enum ListContentConfigType {
 /**
  * @model ListContentConfig
  */
-export class ListContentConfig {
+export class ListContentConfig implements IDropdownOption {
     public id: number;
-    public title: string;
+    public key: string;
+    public text: string;
+    public subText: string;
     public isDefault: boolean;
     public hidden: boolean;
     public sourceListProps: IListProperties = {};
@@ -34,7 +37,9 @@ export class ListContentConfig {
 
     constructor(private _spItem: IListContentConfigSPItem, public web: Web) {
         this.id = _spItem.Id;
-        this.title = _spItem.Title;
+        this.key = `listcontentconfig_${this.id}`;
+        this.text = _spItem.Title;
+        this.subText = _spItem.GtDescription;
         this.isDefault = _spItem.GtLccDefault;
         this.hidden = _spItem.GtLccHidden;
         this._sourceList = _spItem.GtLccSourceList;
@@ -45,11 +50,6 @@ export class ListContentConfig {
         if (this._spItem.ContentTypeId.indexOf('0x0100B8B4EE61A547B247B49CFC21B67D5B7D01') !== -1) return ListContentConfigType.Planner;
         return ListContentConfigType.List;
     }
-
-    public get key() {
-        return `listcontentconfig_${this._spItem.Id.toString()}`;
-    }
-
     public get fields() {
         return !stringIsNullOrEmpty(this._spItem.GtLccFields) ? this._spItem.GtLccFields.split(',') : [];
     }
