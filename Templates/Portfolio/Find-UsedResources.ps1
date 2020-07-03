2477366ea50b6769f12b343fa722d368edd13ffe
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-07-03 10:49:23 +0200

    Removed SiteSettings_* resources

---
+++ Templates/Portfolio/Find-UsedResources.ps1
@@ -0,0 +1,28 @@
+
+$xmlFiles = Get-ChildItem *.xml -Recurse
+
+
+$usedRes = @()
+
+$xmlFiles | ForEach-Object {
+    $content = Get-Content $_.FullName | Out-String
+    [regex]::Matches($content, "{resource:(?<key>[A-Za-z0-9_]*)}", [System.Text.RegularExpressions.RegexOptions]::Multiline) | ForEach-Object {
+        $resKey = $_.Groups | Where-Object { $_.Name -eq "key" } | Select-Object -ExpandProperty value
+        $usedRes += $resKey
+    }
+}
+
+$usedRes | Select-Object -Unique | Out-File used_resources.txt -Encoding utf8 -Force
+
+$content = Get-Content ./no-NB.resx | Out-String
+
+$availableRes = @()
+
+[regex]::Matches($content, 'data name="(?<key>[A-Za-z_]*)"', [System.Text.RegularExpressions.RegexOptions]::Multiline) | ForEach-Object {
+    $resKey = $_.Groups | Where-Object { $_.Name -eq "key" } | Select-Object -ExpandProperty value
+    $availableRes += $resKey
+}
+
+$availableRes | Select-Object -Unique | Out-File available_resources.txt -Encoding utf8 -Force
+
+Compare-Object $usedRes $availableRes | Where-Object { $_.SideIndicator -eq "=>" }
