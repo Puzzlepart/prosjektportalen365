649cde37829a17aa9de69dd3b75b125d38710fa2
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-07-03 09:44:41 +0200

    More translations

--- Templates/Portfolio/Objects/Lists/script.ps1
+++ Templates/Portfolio/Objects/Lists/script.ps1
@@ -1,15 +1,28 @@
-
+$resources = @()
 
 Get-ChildItem *.xml | ForEach-Object { 
     $xml = ([xml](Get-Content $_))
     $instance = $xml.ListInstance 
-    if($instance.Title -notlike "{resource:*") {
+    $title = $instance.Title
+    $url = $instance.Url
+    if ($instance.Title -notlike "{resource:*") {
         $key = Read-Host "Key for $($instance.Title)"
-        $titleKey =" {resource:Lists_$($key)_Title}"
-        $urlKey =  "{resource:Lists_$($key)_Url}"
+        $titleKey = " {resource:Lists_$($key)_Title}"
+        $urlKey = "{resource:Lists_$($key)_Url}"
         $instance.Title = $titleKey
-        $instance.Url = $urlKey
-        
+        $instance.Url = $urlKey        
+        $resources += @"
+        <data name="$titleKey" xml:space="preserve">
+        <value>$title</value>
+      </data>
+"@    
+        $resources += @"
+<data name="$urlKey" xml:space="preserve">
+<value>$url</value>
+</data>
+"@
+        $xml.OuterXml | Out-File $_.FullName -Encoding utf8 -Force
     }
-    $xml.OuterXml | Out-File $_.FullName -Encoding utf8 -Force
 }
+
+$resources -join "" | Out-File resources.xml
