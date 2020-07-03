c0d4bb9e459c3449070c97ab176296b7befeb248
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-07-03 12:10:22 +0200

    Resources for ClientSidePages

---
+++ Templates/Portfolio/Objects/ClientSidePages/script.ps1
@@ -0,0 +1,35 @@
+$resources = @()
+
+Get-ChildItem *.xml | ForEach-Object { 
+    $xml = ([xml](Get-Content $_))
+
+    # Set instance
+    $instance = $xml.ClientSidePage
+
+    $pageName = $instance.PageName
+    $title = $instance.Title
+
+    if ($pageName -notlike "{resource:*") {
+        Write-Host "Generating resources for $($pageName)" -ForegroundColor Cyan
+        $key = Read-Host "Key for $pageName"
+        $pageNameKey = "SiteFields_$($key)_PageName"
+        $titleKey = "SiteFields_$($key)_Title"
+        $instance.PageName = "{resource:$pageNameKey}"
+        $instance.Title = "{resource:$titleKey}"
+        $resources += @"
+        <data name="$pageNameKey" xml:space="preserve">
+        <value>$pageName</value>
+      </data>
+"@    
+        $resources += @"
+<data name="$titleKey" xml:space="preserve">
+<value>$title</value>
+</data>
+"@
+        $xml.OuterXml | Out-File $_.FullName -Encoding utf8 -Force
+    }
+}
+
+Write-Host "Generated $($resources.Length) resources" -ForegroundColor Cyan
+
+$resources -join "" | Out-File resources.xml
