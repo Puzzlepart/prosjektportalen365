0f24d2b7d729a013299934b6aa99afecdd29e799
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-07-08 15:54:25 +0200

    Updated generateTemplate

--- Templates/Portfolio/generateTemplate.js
+++ Templates/Portfolio/generateTemplate.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const path = require('path')
+const pkg = require('../../package.json')
 const Jtr = require('@ptkdev/json-token-replace')
 const jtr = new Jtr()
 const resources = require('./Resources.json')
+const template = require('./Prosjektmaler/_Template.json')
 const output = {
     'en-US': 'DefaultTemplate.txt',
     'no-NB': 'Standardmal.txt'
@@ -12,8 +14,13 @@ Object.keys(resources).forEach(key => {
     console.log('Generating template for ' + key)
     let content = jtr.replace(
         resources[key],
-        require('./Prosjektmaler/Template.json'),
+        template,
         '{resource:', '}'
+    )
+    content = jtr.replace(
+        pkg,
+        content,
+        '{', '}'
     )
 
     fs.writeFile(path.join(__dirname, 'Prosjektmaler', output[key]), JSON.stringify(content, null, 4), () => {
