741cd43d6cf163d5f211b0c85381e716817cd706
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-07-09 12:25:35 +0200

    Build-Release: Logging done

--- Install/Build-Release.ps1
+++ Install/Build-Release.ps1
@@ -26,15 +26,16 @@ mkdir "$ReleasePath/Apps" >$null 2>&1
 Write-Host "Building release [v$($PackageJson.version)]" -ForegroundColor Cyan
 
 #region Copying source files
-Write-Host "[INFO] Copying Install.ps1, PostInstall.ps1 and site script source files..." -NoNewline
+Write-Host "[INFO] Copying Install.ps1, PostInstall.ps1 and site script source files...  " -NoNewline
 Copy-Item -Path "$PSScriptRoot/../SiteScripts/Src/*.txt" -Filter *.txt -Destination "$ReleasePath/SiteScripts" -Force
 Copy-Item -Path "$PSScriptRoot/Install.ps1" -Destination $ReleasePath -Force
 Copy-Item -Path "$PSScriptRoot/Scripts/*.ps1" -Destination "$ReleasePath/Scripts" -Force
 Copy-Item -Path "$PSScriptRoot/SearchConfiguration.xml" -Destination $ReleasePath -Force
 Write-Host "DONE" -ForegroundColor Green
 
-Write-Host "[INFO] Copying SharePointPnPPowerShellOnline bundle"
+Write-Host "[INFO] Copying SharePointPnPPowerShellOnline bundle...  " -NoNewline
 Copy-Item -Path "$PSScriptRoot/SharePointPnPPowerShellOnline" -Filter * -Destination $ReleasePath -Force -Recurse
+Write-Host "DONE" -ForegroundColor Green
 
 (Get-Content "$ReleasePath/Install.ps1") -Replace 'VERSION_PLACEHOLDER', $PackageJson.version | Set-Content "$ReleasePath/Install.ps1"
 #endregion
@@ -43,14 +44,15 @@ Copy-Item -Path "$PSScriptRoot/SharePointPnPPowerShellOnline" -Filter * -Destina
 #region Clean node_modules for all SharePoint Framework solutions
 if ($CleanNodeModules.IsPresent) {
     $Solutions | ForEach-Object {
-        Write-Host "[INFO] Clearing node_modules for SPFx solution [$_]"
+        Write-Host "[INFO] Clearing node_modules for SPFx solution [$_]...  " -NoNewline
         rimraf "$($PSScriptRoot)\..\SharePointFramework\$_\node_modules\"
+        Write-Host "DONE" -ForegroundColor Green
     }
 }
 #endregion
 
 #region Package SharePoint Framework solutions
-Write-Host "[INFO] Building SharePointFramework\@Shared"
+Write-Host "[INFO] Building SharePointFramework\@Shared...  " -NoNewline
 Set-Location "$PSScriptRoot\..\SharePointFramework\@Shared"
 
 # https://github.com/SharePoint/sp-dev-docs/issues/2916
@@ -64,21 +66,24 @@ if (-not $SkipBuildSharePointFramework.IsPresent) {
         npm run build   
     }
 }
+Write-Host "DONE" -ForegroundColor Green
 
-foreach ($Solution in $Solutions) {
-    Set-Location "$($PSScriptRoot)\..\SharePointFramework\$($Solution)"
+$Solutions | ForEach-Object {
+    Set-Location "$($PSScriptRoot)\..\SharePointFramework\$_"
     $PackageSolutionJson = Get-Content "./config/package-solution.json" -Raw | ConvertFrom-Json
-    Write-Host "[INFO] Packaging SPFx solution [$($Solution)] [v$($PackageSolutionJson.solution.version)]"
+    Write-Host "[INFO] Packaging SPFx solution [$_] [v$($PackageSolutionJson.solution.version)]...  " -NoNewline
     # https://github.com/SharePoint/sp-dev-docs/issues/2916
     if (-not $SkipBuildSharePointFramework.IsPresent) {
         if ($Silent.IsPresent) {
-        npm install --no-package-lock --no-progress --silent --no-audit 
-        npm run package >$null 2>&1
-        } else {
-        npm install --no-package-lock
-        npm run package 
+            npm install --no-package-lock --no-progress --silent --no-audit 
+            npm run package >$null 2>&1
+        }
+        else {
+            npm install --no-package-lock
+            npm run package 
         }
     }
+    Write-Host "DONE" -ForegroundColor Green
     Get-ChildItem "./sharepoint/solution/" *.sppkg -Recurse | Where-Object { -not ($_.PSIsContainer -or (Test-Path "$ReleasePath/Apps/$_")) } | Copy-Item -Destination "$ReleasePath/Apps" -Force
 }
 #endregion
@@ -86,10 +91,11 @@ foreach ($Solution in $Solutions) {
 
 #region Build PnP templates
 Set-Location $PSScriptRoot
-Write-Host "[INFO] Building [Portfolio] PnP template"
+Write-Host "[INFO] Building [Portfolio] PnP template...  " -NoNewline
 Convert-PnPFolderToProvisioningTemplate -Out "$ReleasePath/Templates/Portfolio.pnp" -Folder "$PSScriptRoot/../Templates/Portfolio" -Force
+Write-Host "DONE" -ForegroundColor Green
 
-Write-Host "[INFO] Building PnP content templates"
+Write-Host "[INFO] Building PnP content templates...  " -NoNewline
 Set-Location "$PSScriptRoot/../Templates"
 if ($Silent.IsPresent) {
     npm install --no-package-lock --no-progress --silent --no-audit 
@@ -102,10 +108,12 @@ else {
 Get-ChildItem "./Content" -Directory | ForEach-Object {
     Convert-PnPFolderToProvisioningTemplate -Out "$ReleasePath/Templates/$($_.BaseName).pnp" -Folder $_.FullName -Force
 }
+Write-Host "DONE" -ForegroundColor Green
 Set-Location $PSScriptRoot
 
-Write-Host "[INFO] Building [Taxonomy] PnP template"
+Write-Host "[INFO] Building [Taxonomy] PnP template....  " -NoNewline
 Convert-PnPFolderToProvisioningTemplate -Out "$ReleasePath/Templates/Taxonomy.pnp" -Folder "$PSScriptRoot/../Templates/Taxonomy" -Force
+Write-Host "DONE" -ForegroundColor Green
 #endregion
 
 $sw.Stop()
