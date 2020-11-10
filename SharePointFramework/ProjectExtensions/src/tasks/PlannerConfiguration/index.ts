f7099fe4e8b522cadf721b97825b04aaa7e5f105
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-10 17:54:17 +0100

    Error handling for PlannerConfiguration task (#331)

--- SharePointFramework/ProjectExtensions/src/tasks/PlannerConfiguration/index.ts
+++ SharePointFramework/ProjectExtensions/src/tasks/PlannerConfiguration/index.ts
@@ -1,18 +1,18 @@
 import { PageContext } from '@microsoft/sp-page-context'
+import { getGUID } from '@pnp/common'
 import { IProjectSetupData } from 'extensions/projectSetup'
 import { default as MSGraphHelper } from 'msgraph-helper'
+import { format } from 'office-ui-fabric-react/lib/Utilities'
 import * as strings from 'ProjectExtensionsStrings'
-import * as formatString from 'string-format'
+import { sleep } from 'shared/lib/util'
 import { BaseTask, BaseTaskError, IBaseTaskParams } from '../@BaseTask'
 import { OnProgressCallbackFunction } from '../OnProgressCallbackFunction'
-import { IPlannerPlan,IPlannerConfiguration,IPlannerBucket } from './types'
-import { TypedHash, getGUID } from '@pnp/common'
-
+import { IPlannerBucket, IPlannerConfiguration, IPlannerPlan } from './types'
 /**
  * @class PlannerConfiguration
  */
 export class PlannerConfiguration extends BaseTask {
-    public taskName = 'PlannerConfiguration';
+    public taskName = 'Planner';
 
     /**
      * Constructor
@@ -37,20 +37,24 @@ export class PlannerConfiguration extends BaseTask {
      * @param {string} defaultBucketName Default bucket name
      */
     private async _createPlan(pageContext: PageContext, onProgress: OnProgressCallbackFunction): Promise<IPlannerPlan> {
-        const planTitle = pageContext.web.title
-        const owner = pageContext.legacyPageContext.groupId
-        const existingGroupPlans = await this._fetchPlans(owner)
-        this.logInformation(`Creating plan ${planTitle}`)
-        const plan = await this._ensurePlan(planTitle, existingGroupPlans, pageContext.legacyPageContext.groupId)
-        const existingBuckets = await this._fetchBuckets(plan.id)
-        for (let i = 0; i < Object.keys(this._configuration).length; i++) {
-            const bucketName = Object.keys(this._configuration)[i]
-            this.logInformation(`Ensuring bucket ${bucketName} for plan ${planTitle}`)
-            const bucket = await this._ensureBucket(bucketName, existingBuckets, plan.id)
-            onProgress(strings.PlannerConfigurationText, formatString(strings.CreatingPlannerTaskText, bucketName), 'PlannerLogo')
-            await this._createTasks(plan.id, bucket)
+        try {
+            const planTitle = pageContext.web.title
+            const owner = pageContext.legacyPageContext.groupId
+            const existingGroupPlans = await this._fetchPlans(owner)
+            this.logInformation(`Creating plan ${planTitle}`)
+            const plan = await this._ensurePlan(planTitle, existingGroupPlans, owner)
+            const existingBuckets = await this._fetchBuckets(plan.id)
+            for (let i = 0; i < Object.keys(this._configuration).length; i++) {
+                const bucketName = Object.keys(this._configuration)[i]
+                this.logInformation(`Ensuring bucket ${bucketName} for plan ${planTitle}`)
+                const bucket = await this._ensureBucket(bucketName, existingBuckets, plan.id)
+                onProgress(strings.PlannerConfigurationText, format(strings.CreatingPlannerTaskText, bucketName), 'PlannerLogo')
+                await this._createTasks(plan.id, bucket)
+            }
+            return plan
+        } catch (error) {
+            throw new Error(`_createPlan: ${error.message}`)
         }
-        return plan
     }
 
     /**
@@ -61,16 +65,33 @@ export class PlannerConfiguration extends BaseTask {
      * @param {string} owner Owner (group id) 
      */
     private async _ensurePlan(title: string, existingPlans: IPlannerPlan[], owner: string): Promise<IPlannerPlan> {
-        let [plan] = existingPlans.filter(p => p.title === title)
-        if (!plan) {
-            plan = await MSGraphHelper.Post('planner/plans', JSON.stringify({ title, owner }))
+        try {
+            let [plan] = existingPlans.filter(p => p.title === title)
+            if (!plan) {
+                plan = await MSGraphHelper.Post('planner/plans', JSON.stringify({ title, owner }))
+            }
+            await this._setupLabels(plan)
+            return plan
+        } catch (error) {
+            throw error
         }
+    }
+
+    /**
+     * Sets up labels for the plan
+     * 
+     * @param {IPlannerPlan} plan Plan
+     * @param {number} delay Delay in seconds before updating the plan to ensure it's created properly
+     */
+    private async _setupLabels(plan: IPlannerPlan, delay: number = 5) {
+        this.logInformation(`Sleeping ${delay} seconds before updating the plan with labels`)
+        await sleep(delay)
         if (this._labels.length > 0) {
+            this.logInformation(`Sleeping before updating the plan with labels ${JSON.stringify(this._labels)}`)
             const eTag = (await MSGraphHelper.Get(`planner/plans/${plan.id}/details`))['@odata.etag']
             const categoryDescriptions = this._labels.splice(0, 6).reduce((obj, value, idx) => ({ ...obj, [`category${idx + 1}`]: value }), {})
             await MSGraphHelper.Patch(`planner/plans/${plan.id}/details`, JSON.stringify({ categoryDescriptions }), eTag)
         }
-        return plan
     }
 
     /**
@@ -81,11 +102,19 @@ export class PlannerConfiguration extends BaseTask {
      * @param {string} planId Plan Id 
      */
     private async _ensureBucket(name: string, existingBuckets: IPlannerBucket[], planId: string) {
-        let [bucket] = existingBuckets.filter(p => p.name === name)
-        if (!bucket) {
-            bucket = await MSGraphHelper.Post('planner/buckets', JSON.stringify({ name, planId, orderHint: ' !' }))
+        try {
+            let [bucket] = existingBuckets.filter(p => p.name === name)
+            if (!bucket) {
+                bucket = await MSGraphHelper.Post('planner/buckets', JSON.stringify({
+                    name,
+                    planId,
+                    orderHint: ' !'
+                }))
+            }
+            return bucket
+        } catch (error) {
+            throw error
         }
-        return bucket
     }
 
     /**
@@ -93,8 +122,10 @@ export class PlannerConfiguration extends BaseTask {
      * 
      * @param {string} planId Plan Id 
      * @param {IPlannerBucket} bucket Bucket 
+     * @param {Record<string, boolean>} appliedCategories Categories to apply to the task 
+     * @param {number} delay Delay in seconds before updating the plan to ensure it's created properly
      */
-    private async _createTasks(planId: string, bucket: IPlannerBucket) {
+    private async _createTasks(planId: string, bucket: IPlannerBucket, appliedCategories: Record<string, boolean> = { category1: true }, delay: number = 1) {
         const tasks = Object.keys(this._configuration[bucket.name])
         for (let i = 0; i < tasks.length; i++) {
             const name = tasks[i]
@@ -105,10 +136,12 @@ export class PlannerConfiguration extends BaseTask {
                     title: name,
                     bucketId: bucket.id,
                     planId,
-                    appliedCategories: { category1: true },
+                    appliedCategories,
                 }))
                 if (checklist || attachments) {
-                    const taskDetails: TypedHash<any> = {
+                    this.logInformation(`Sleeping ${delay} seconds before updating task details for ${name}`)
+                    await sleep(delay)
+                    const taskDetails: Record<string, any> = {
                         checklist: checklist
                             ? checklist.reduce((obj, title) => ({
                                 ...obj,
@@ -133,7 +166,7 @@ export class PlannerConfiguration extends BaseTask {
                 }
                 this.logInformation(`Succesfully created task ${name} in bucket ${bucket.name}`, { taskId: task.id, checklist })
             } catch (error) {
-                this.logInformation(`Failed to create task ${name} in bucket ${bucket.name}`)
+                this.logWarning(`Failed to create task ${name} in bucket ${bucket.name}`)
             }
         }
     }
