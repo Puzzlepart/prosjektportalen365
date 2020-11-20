feab9f989ae2471e4294bce487ca1564c18bc877
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-20 22:33:25 +0100

    $BasePath

--- Install/Install.ps1
+++ Install/Install.ps1
@@ -286,6 +286,7 @@ if (-not $Upgrade.IsPresent) {
 #region Applying PnP templates 
 if (-not $SkipTemplate.IsPresent) {
     Try {
+        $BasePath =  "$PSScriptRoot\Templates"
         Connect-SharePoint -Url $AdminSiteUrl -ErrorAction Stop
         Set-PnPTenantSite -NoScriptSite:$false -Url $Url -ErrorAction SilentlyContinue >$null 2>&1        
         Disconnect-PnPOnline
@@ -300,25 +301,25 @@ if (-not $SkipTemplate.IsPresent) {
         }
         if (-not $SkipTaxonomy.IsPresent -and -not $Upgrade.IsPresent) {
             Write-Host "[INFO] Applying PnP template [Taxonomy] to [$Url]"
-            Apply-PnPProvisioningTemplate "$PSScriptRoot\Templates\Taxonomy.pnp" -ErrorAction Stop
+            Apply-PnPProvisioningTemplate "$BasePath\Taxonomy.pnp" -ErrorAction Stop
             Write-Host "[SUCCESS] Successfully applied PnP template [Taxonomy] to [$Url]" -ForegroundColor Green
         }
         
         Write-Host "[INFO] Applying PnP template [Portfolio] to [$Url]"
-        $Instance = Read-PnPProvisioningTemplate "$PSScriptRoot\Templates\Portfolio.pnp"
+        $Instance = Read-PnPProvisioningTemplate "$BasePath\Portfolio.pnp"
         $Instance.SupportedUILanguages[0].LCID = $LanguageId
         Apply-PnPProvisioningTemplate -InputInstance $Instance -Handlers SupportedUILanguages
-        Apply-PnPProvisioningTemplate "$PSScriptRoot\Templates\Portfolio.pnp" -ExcludeHandlers SupportedUILanguages -ErrorAction Stop
+        Apply-PnPProvisioningTemplate "$BasePath\Portfolio.pnp" -ExcludeHandlers SupportedUILanguages -ErrorAction Stop
         Write-Host "[SUCCESS] Successfully applied PnP template [Portfolio] to [$Url]" -ForegroundColor Green
 
         if ($Upgrade.IsPresent) {
             Write-Host "[INFO] Applying PnP content template (Handlers:Files) to [$Url]"
-            Apply-PnPProvisioningTemplate "$PSScriptRoot\Templates\Portfolio_content.$LanguageCode.pnp" -Handlers Files -ErrorAction Stop
+            Apply-PnPProvisioningTemplate "$BasePath\Portfolio_content.$LanguageCode.pnp" -Handlers Files -ErrorAction Stop
             Write-Host "[SUCCESS] Successfully applied PnP content template to [$Url]" -ForegroundColor Green
         }
         else {
             Write-Host "[INFO] Applying PnP content template to [$Url]"
-            Apply-PnPProvisioningTemplate "$PSScriptRoot\Templates\Portfolio_content.$LanguageCode.pnp" -ErrorAction Stop
+            Apply-PnPProvisioningTemplate "$BasePath\Portfolio_content.$LanguageCode.pnp" -ErrorAction Stop
             Write-Host "[SUCCESS] Successfully applied PnP content template to [$Url]" -ForegroundColor Green
         }
         
