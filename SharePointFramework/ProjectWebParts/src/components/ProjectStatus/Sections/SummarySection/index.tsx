Puzzlepart/prosjektportalen365

commit 5d259bf178487d674cebfa70d6148f45cbd5cd00

Only showing status section if status field has value

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/SummarySection/index.tsx
@@ -58,11 +58,13 @@ export class SummarySection extends BaseSection<ISummarySectionProps, ISummarySe
         props.comment = props.value
         props.value = ''
       }
-      return (
-        <div key={idx} className='ms-Grid-col ms-sm6'>
-          <StatusElement {...props} />
-        </div>
-      )
+      if (props.value) {
+        return (
+          <div key={idx} className='ms-Grid-col ms-sm6'>
+            <StatusElement {...props} />
+          </div>
+        );
+      }
     })
   }
 }

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/index.tsx
@@ -277,7 +277,11 @@ export class ProjectStatus extends React.Component<IProjectStatusProps, IProject
             )
           }
           case SectionType.StatusSection: {
-            return <StatusSection {...baseProps} />
+            if (baseProps.headerProps.value) {
+              return <StatusSection {...baseProps} />
+            } else {
+              return null;
+            }
           }
           case SectionType.ProjectPropertiesSection: {
             return (

commit 3ec333e11b1e9d08c3dc52cda164ed7227f1fa5d

Fixed issue introduced in 5d259bf178487d674cebfa70d6148f45cbd5cd00

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/SummarySection/index.tsx
@@ -58,7 +58,7 @@ export class SummarySection extends BaseSection<ISummarySectionProps, ISummarySe
         props.comment = props.value
         props.value = ''
       }
-      if (props.value) {
+      if (props.value || sec.fieldName === 'GtOverallStatus') {
         return (
           <div key={idx} className='ms-Grid-col ms-sm6'>
             <StatusElement {...props} />