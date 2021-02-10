444724b26b9bfcd51fdd015fe6b8db47a963fe1b
Martin Ruud <m.ruud93@gmail.com>
2021-02-10 15:26:22 +0100

    Added innerHTML to enable newline

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/StatusElement/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/StatusElement/index.tsx
@@ -11,6 +11,9 @@ export const StatusElement = ({
   iconSize = 30,
   iconColor
 }: IStatusElementProps) => {
+  
+  // Replace \n with corresponding HTML tag to enable newlines in the comment.
+  const htmlComment = {__html: comment.replace(/\n/g,"<br />")};
   return (
     <div className={styles.statusElement}>
       <div className={styles.container}>
@@ -20,7 +23,7 @@ export const StatusElement = ({
         <div className={styles.statusContent}>
           <div className={styles.statusElementLabel}>{label}</div>
           <div className={styles.statusElementValue}>{value}</div>
-          <div className={styles.statusElementComment}>{comment}</div>
+          <div className={styles.statusElementComment} dangerouslySetInnerHTML={htmlComment}></div>
         </div>
       </div>
     </div>
