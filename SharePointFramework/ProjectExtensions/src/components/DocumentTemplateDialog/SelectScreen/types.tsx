126d97ab42ec904ff99182901a6dce46dd10e8fd
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-11 10:09:24 +0100

    SelectScreen

--- SharePointFramework/ProjectExtensions/src/components/DocumentTemplateDialog/SelectScreen/types.tsx
+++ SharePointFramework/ProjectExtensions/src/components/DocumentTemplateDialog/SelectScreen/types.tsx
@@ -1,15 +1,10 @@
-import { IColumn, Selection } from 'office-ui-fabric-react/lib/DetailsList'
+import { Selection } from 'office-ui-fabric-react/lib/DetailsList'
 
 export interface ISelectScreenProps<ItemType = any> {
   /**
    * Selection
    */
   selection: Selection
-
-  /**
-   * Columns
-   */
-  columns?: IColumn[]
 
   /**
    * Selected items
