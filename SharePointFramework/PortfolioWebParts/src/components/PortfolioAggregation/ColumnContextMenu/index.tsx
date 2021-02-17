c0e8abb8463b21263561076f400922b5fb828c9b
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-17 14:40:26 +0100

    Updated ColumnContextMenu

--- SharePointFramework/PortfolioWebParts/src/components/PortfolioAggregation/ColumnContextMenu/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/PortfolioAggregation/ColumnContextMenu/index.tsx
@@ -1,15 +1,16 @@
-import { ContextualMenu, ContextualMenuItemType } from 'office-ui-fabric-react/lib/ContextualMenu'
+import { DisplayMode } from '@microsoft/sp-core-library'
+import { ContextualMenu, ContextualMenuItemType, IContextualMenuItem } from 'office-ui-fabric-react/lib/ContextualMenu'
 import { format } from 'office-ui-fabric-react/lib/Utilities'
 import * as strings from 'PortfolioWebPartsStrings'
 import React, { useContext } from 'react'
 import { PortfolioAggregationContext } from '../context'
-import { COLUMN_HEADER_CONTEXT_MENU, SET_GROUP_BY, SET_SORT } from '../reducer'
+import { COLUMN_HEADER_CONTEXT_MENU, MOVE_COLUMN, SET_GROUP_BY, SET_SORT, TOGGLE_COLUMN_FORM_PANEL } from '../reducer'
 
 export const ColumnContextMenu = () => {
-    const { state, dispatch } = useContext(PortfolioAggregationContext)
+    const { props, state, dispatch } = useContext(PortfolioAggregationContext)
     if (!state.columnContextMenu) return null
     const { column, target } = state.columnContextMenu
-    const items = [
+    const items: IContextualMenuItem[] = [
         {
             key: 'SortDesc',
             name: strings.SortDescLabel,
@@ -29,20 +30,48 @@ export const ColumnContextMenu = () => {
             )
         },
         {
-            key: 'Divider',
+            key: 'Divider1',
             itemType: ContextualMenuItemType.Divider
         },
         {
             key: 'GroupBy',
-            name: format(strings.GroupByColumnLabel, ''),
+            name: format(strings.GroupByColumnLabel, column.name),
             canCheck: true,
             checked: state.groupBy?.fieldName === column.fieldName,
             disabled: !column.data?.isGroupable,
             onClick: () => dispatch(
                 SET_GROUP_BY({ column })
             )
+        },
+        props.displayMode === DisplayMode.Edit && {
+            key: 'Divider2',
+            itemType: ContextualMenuItemType.Divider
+        },
+        props.displayMode === DisplayMode.Edit && {
+            key: 'MoveLeft',
+            name: strings.MoveLeftLabel,
+            iconProps: { iconName: 'ChevronLeftMed' },
+            onClick: () => dispatch(
+                MOVE_COLUMN({ column, move: -1 })
+            )
+        },
+        props.displayMode === DisplayMode.Edit && {
+            key: 'MoveRight',
+            name: strings.MoveRightLabel,
+            iconProps: { iconName: 'ChevronRightMed' },
+            onClick: () => dispatch(
+                MOVE_COLUMN({ column, move: 1 })
+            )
+        },
+        props.displayMode === DisplayMode.Edit && {
+            key: 'Edit',
+            name: strings.EditColumnLabel,
+            iconProps: { iconName: 'SingleColumnEdit' },
+            onClick: () => dispatch(
+                TOGGLE_COLUMN_FORM_PANEL({ isOpen: true, column })
+            )
         }
-    ]
+    ].filter(i => i)
     return (
         <ContextualMenu
             target={target}
