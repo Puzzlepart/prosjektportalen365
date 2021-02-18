1f59598ffca2696865be765b0676a8079a4f13e8
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-18 15:30:58 +0100

    Clean up

--- Templates/Portfolio/test.ps1
+++
@@ -1,6 +0,0 @@
-$items = Get-ChildItem ./Objects/ClientSidePages/*.xml -Exclude "*@.xml"
-
-$items | ForEach-Object {
-    [xml]$xml = (Get-Content $_.FullName | Out-String)
-    $xml.ClientSidePage.Sections.Section.Controls.CanvasControl.JsonControlData | Out-File "$($_.BaseName).json"
-}
