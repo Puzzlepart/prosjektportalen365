9fcef013cbc7f4fd4e4b8168c320bc7f8d25cb76
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-18 12:21:09 +0100

    Linting

--- SharePointFramework/ProjectWebParts/src/components/ProjectInformation/StatusReports/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformation/StatusReports/index.tsx
@@ -1,15 +1,15 @@
 import { DisplayMode } from '@microsoft/sp-core-library'
 import { WebPartTitle } from '@pnp/spfx-controls-react/lib/WebPartTitle'
 import { ActionButton } from 'office-ui-fabric-react/lib/Button'
-import * as React from 'react'
 import { formatDate } from 'pp365-shared/lib/helpers/formatDate'
-import { IStatusReportsProps } from './types'
+import React, { Component, ReactElement } from 'react'
 import styles from './StatusReports.module.scss'
+import { IStatusReportsProps } from './types'
 
-export class StatusReports extends React.Component<IStatusReportsProps, {}> {
+export class StatusReports extends Component<IStatusReportsProps> {
   public static defaultProps: Partial<IStatusReportsProps> = { iconName: 'PageCheckedin' }
 
-  public render(): React.ReactElement<IStatusReportsProps> {
+  public render(): ReactElement<IStatusReportsProps> {
     return (
       <div className={styles.statusReports} hidden={this.props.hidden}>
         <WebPartTitle
