Puzzlepart/prosjektportalen365

commit 0f47512a4f16c35935b6422a6a8960d71a668d6a

Added innerHTML to enable newline for status report comments (#383)

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/StatusElement/index.tsx
@@ -20,7 +20,7 @@ export const StatusElement = ({
         <div className={styles.statusContent}>
           <div className={styles.statusElementLabel}>{label}</div>
           <div className={styles.statusElementValue}>{value}</div>
-          <div className={styles.statusElementComment}>{comment}</div>
+          <div className={styles.statusElementComment} dangerouslySetInnerHTML={{ __html: comment.replace(/\n/g, '<br />') }}></div>
         </div>
       </div>
     </div>

commit 25a96295a987560080ebc97d4b3c8f23024cfbf2

Fixed issue with HTML support in Project status introduced in #383 (#412)

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/BaseSection/types.ts
@@ -1,6 +1,6 @@
 import { StatusReport, SectionModel } from 'pp365-shared/lib/models'
 import { IProjectStatusData } from '../..'
-import { IStatusElementProps } from '../../StatusElement/IStatusElementProps'
+import { IStatusElementProps } from '../../StatusElement/types'
 
 export interface IBaseSectionProps {
   /**

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/ListSection/index.tsx
@@ -1,4 +1,4 @@
-import { sp } from '@pnp/sp'
+import { Web } from '@pnp/sp'
 import {
   DetailsList,
   DetailsListLayoutMode,
@@ -6,18 +6,18 @@ import {
   SelectionMode
 } from 'office-ui-fabric-react/lib/DetailsList'
 import { MessageBar, MessageBarType } from 'office-ui-fabric-react/lib/MessageBar'
+import { getObjectValue } from 'pp365-shared/lib/helpers'
 import * as strings from 'ProjectWebPartsStrings'
 import * as React from 'react'
-import { getObjectValue } from 'pp365-shared/lib/helpers'
-import { BaseSection } from '../BaseSection/index'
 import { StatusElement } from '../../StatusElement'
-import { IListSectionProps, IListSectionState, IListSectionData } from './types'
+import { BaseSection } from '../BaseSection/index'
 import styles from './ListSection.module.scss'
+import { IListSectionData, IListSectionProps, IListSectionState } from './types'
 
 export class ListSection extends BaseSection<
   IListSectionProps,
   IListSectionState<IListSectionData>
-> {
+  > {
   constructor(props: IListSectionProps) {
     super(props)
     this.state = { loading: true }
@@ -25,7 +25,7 @@ export class ListSection extends BaseSection<
 
   public async componentDidMount() {
     try {
-      const data = await this._fetchData()
+      const data = await this._fetchListData()
       this.setState({ data, loading: false })
     } catch (error) {
       this.setState({ error, loading: false })
@@ -75,11 +75,16 @@ export class ListSection extends BaseSection<
   }
 
   /**
-   * Fetch data
+   * Fetch list data
    */
-  private async _fetchData(): Promise<IListSectionData> {
-    const { listTitle, viewQuery, viewFields, rowLimit } = this.props.model
-    const list = sp.web.lists.getByTitle(listTitle)
+  private async _fetchListData(): Promise<IListSectionData> {
+    const {
+      listTitle,
+      viewQuery,
+      viewFields,
+      rowLimit
+    } = this.props.model
+    const list = new Web(this.props.webUrl).lists.getByTitle(listTitle)
     try {
       const viewXml = `<View><Query>${viewQuery}</Query><RowLimit>${rowLimit}</RowLimit></View>`
       const [items, fields] = await Promise.all([
@@ -99,7 +104,12 @@ export class ListSection extends BaseSection<
             fieldName: field.InternalName,
             name: field.Title,
             minWidth: 100,
-            maxWidth: { Text: 250, Note: 250, Choice: 150, Number: 100 }[field.TypeAsString] || 150,
+            maxWidth: {
+              Text: 250,
+              Note: 250,
+              Choice: 150,
+              Number: 100
+            }[field.TypeAsString] || 150,
             isResizable: true
           } as IColumn
         })

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/RiskSection/index.tsx
@@ -1,4 +1,4 @@
-import { sp } from '@pnp/sp'
+import { Web } from '@pnp/sp'
 import {
   DetailsList,
   DetailsListLayoutMode,
@@ -23,7 +23,7 @@ export class RiskSection extends BaseSection<IRiskSectionProps, IRiskSectionStat
 
   public async componentDidMount() {
     try {
-      const data = await this._fetchData()
+      const data = await this._fetchListData()
       this.setState({ data, loading: false })
     } catch (error) {
       this.setState({ error, loading: false })
@@ -77,9 +77,9 @@ export class RiskSection extends BaseSection<IRiskSectionProps, IRiskSectionStat
   /**
    * Fetch data
    */
-  private async _fetchData(): Promise<IRiskSectionData> {
+  private async _fetchListData(): Promise<IRiskSectionData> {
     const { listTitle, viewQuery, viewFields, rowLimit } = this.props.model
-    const list = sp.web.lists.getByTitle(listTitle)
+    const list = new Web(this.props.webUrl).lists.getByTitle(listTitle)
     const viewXml = `<View><Query>${viewQuery}</Query><RowLimit>${rowLimit}</RowLimit></View>`
     try {
       const [items, fields] = await Promise.all([

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/SummarySection/index.tsx
@@ -2,7 +2,7 @@ import { Web } from '@pnp/sp'
 import { ProjectInformation } from 'components/ProjectInformation'
 import * as React from 'react'
 import { StatusElement } from '../../StatusElement'
-import { IStatusElementProps } from '../../StatusElement/IStatusElementProps'
+import { IStatusElementProps } from '../../StatusElement/types'
 import { BaseSection } from '../BaseSection'
 import styles from './SummarySection.module.scss'
 import { ISummarySectionProps, ISummarySectionState } from './types'

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/StatusElement/StatusElement.module.scss
@@ -1,6 +1,6 @@
 @import "~@microsoft/sp-office-ui-fabric-core/dist/sass/SPFabricCore.scss";
 
-.statusElement {
+.root {
   @include ms-Grid;
   margin: 20px 0 0 0;
   .container {

SharePointFramework/ProjectWebParts/src/components/ProjectStatus/StatusElement/index.tsx
@@ -1,6 +1,6 @@
 import { Icon } from 'office-ui-fabric-react/lib/Icon'
 import * as React from 'react'
-import { IStatusElementProps } from './IStatusElementProps'
+import { IStatusElementProps } from './types'
 import styles from './StatusElement.module.scss'
 
 export const StatusElement = ({
@@ -12,7 +12,7 @@ export const StatusElement = ({
   iconColor
 }: IStatusElementProps) => {
   return (
-    <div className={styles.statusElement}>
+    <div className={styles.root}>
       <div className={styles.container}>
         <div className={styles.statusIcon} style={{ fontSize: iconSize, color: iconColor }}>
           <Icon iconName={iconName} />
@@ -20,9 +20,11 @@ export const StatusElement = ({
         <div className={styles.statusContent}>
           <div className={styles.statusElementLabel}>{label}</div>
           <div className={styles.statusElementValue}>{value}</div>
-          <div
-            className={styles.statusElementComment}
-            dangerouslySetInnerHTML={{ __html: comment.replace(/\n/g, '<br />') }}></div>
+          {comment && (
+            <div
+              className={styles.statusElementComment}
+              dangerouslySetInnerHTML={{ __html: comment.replace(/\n/g, '<br />') }}></div>
+          )}
         </div>
       </div>
     </div>

SharePointFramework/ProjectWebParts/src/webparts/projectStatus/index.ts
@@ -15,11 +15,7 @@ export default class ProjectStatusWebPart extends BaseProjectWebPart<IProjectSta
   }
 
   public render(): void {
-    this.renderComponent(ProjectStatus, {
-      riskMatrixWidth: this.properties.riskMatrixWidth,
-      riskMatrixHeight: this.properties.riskMatrixHeight,
-      riskMatrixCalloutTemplate: this.properties.riskMatrixCalloutTemplate
-    })
+    this.renderComponent<IProjectStatusProps>(ProjectStatus)
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {