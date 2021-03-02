0c7b2ca0ca640e315efefe92d907453212627819
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-02 11:12:17 +0100

    $ROOT_PATH

--- Install/Build-Release.ps1
+++ Install/Build-Release.ps1
@@ -122,3 +122,5 @@ Add-Type -Assembly "System.IO.Compression.FileSystem"
 [IO.Compression.ZipFile]::CreateFromDirectory($RELEASE_PATH, "$($RELEASE_PATH).zip")  
 Write-Host "Done building release [v$($PACKAGE_FILE.version)] in [$($sw.Elapsed)]" -ForegroundColor Cyan
 
+
+Get-ChildItem "$($ROOT_PATH)*.zip"
