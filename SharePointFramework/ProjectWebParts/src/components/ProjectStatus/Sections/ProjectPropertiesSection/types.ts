656947493ba6e3dbb8e3237abbed48f67e7c8390
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-24 09:36:21 +0100

    Project properties sync improvements (#444)
    See https://github.com/Puzzlepart/prosjektportalen365/wiki/Egne-prosjektegenskaper

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/ProjectPropertiesSection/types.ts
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/ProjectPropertiesSection/types.ts
@@ -1,12 +1,15 @@
 import { TypedHash } from '@pnp/common'
 import { SPField } from 'pp365-shared/lib/models'
+import { IEntityField } from 'sp-entityportal-service'
 import { IBaseSectionProps } from '../BaseSection'
+
+type Field = SPField | IEntityField
 
 export interface IProjectPropertiesSectionProps extends IBaseSectionProps {
   /**
    * Fields
    */
-  fields: SPField[]
+  fields: Field[]
 
   /**
    * Field values
