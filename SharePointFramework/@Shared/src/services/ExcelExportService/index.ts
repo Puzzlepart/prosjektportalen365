6daac8e2bb589c94f5a7828bfe2c52f89ed649d1
Petter <petter.braten@puzzlepart.com>
2021-04-28 08:58:20 +0200

    Fix for excel export
    Added XLSX as npm package

--- SharePointFramework/@Shared/src/services/ExcelExportService/index.ts
+++ SharePointFramework/@Shared/src/services/ExcelExportService/index.ts
@@ -2,20 +2,20 @@
 import { IColumn } from 'office-ui-fabric-react/lib/DetailsList'
 import { format } from 'office-ui-fabric-react/lib/Utilities'
 import * as $script from 'scriptjs'
+import * as XLSX from 'xlsx'
 import { getObjectValue } from '../../helpers/getObjectValue'
 import { stringToArrayBuffer } from '../../util'
 import { ExcelExportServiceDefaultConfiguration } from './ExcelExportServiceDefaultConfiguration'
 import { IExcelExportServiceConfiguration } from './IExcelExportServiceConfiguration'
 
 export default new (class ExcelExportService {
-  private _configuration: IExcelExportServiceConfiguration
-  private _deps: string[]
+  public _configuration: IExcelExportServiceConfiguration
+  public _deps: string[]
 
   public configure(configuration: IExcelExportServiceConfiguration) {
     this._configuration = { ...ExcelExportServiceDefaultConfiguration, ...configuration }
     this._deps = [
-      `FileSaver.js/${this._configuration.fileSaverVersion}/FileSaver.min.js`,
-      `xlsx/${this._configuration.xlsxVersion}/xlsx.full.min.js`
+      `FileSaver.js/${this._configuration.fileSaverVersion}/FileSaver.min.js`
     ]
     $script.path('https://cdnjs.cloudflare.com/ajax/libs/')
   }
@@ -25,13 +25,12 @@ export default new (class ExcelExportService {
    *
    * @param {string[]} deps Deps
    */
-  protected loadDeps(): Promise<void> {
+
+
+  public loadDeps(): Promise<void> {
     return new Promise<void>((resolve) => {
-      const _define = ((<any>window).define(window as any).define = undefined)
       $script(this._deps, 'deps')
       $script.ready('deps', () => {
-        // eslint-disable-next-line @typescript-eslint/no-extra-semi
-        ;(window as any).define = _define
         resolve()
       })
     })
@@ -57,12 +56,12 @@ export default new (class ExcelExportService {
           )
         ]
       })
-      const workBook = (<any>window).XLSX.utils.book_new()
+      const workBook = XLSX.utils.book_new()
       sheets.forEach((s, index) => {
-        const sheet = (<any>window).XLSX.utils.aoa_to_sheet(s.data)
-        ;(<any>window).XLSX.utils.book_append_sheet(workBook, sheet, s.name || `Sheet${index + 1}`)
+        const sheet = XLSX.utils.aoa_to_sheet(s.data)
+        ;XLSX.utils.book_append_sheet(workBook, sheet, s.name || `Sheet${index + 1}`)
       })
-      const wbout = (<any>window).XLSX.write(workBook, this._configuration.options)
+      const wbout = XLSX.write(workBook, this._configuration.options)
       ;(<any>window).saveAs(
         new Blob([stringToArrayBuffer(wbout)], { type: 'application/octet-stream' }),
         format('{0}-{1}.xlsx', this._configuration.name, new Date().toISOString())
