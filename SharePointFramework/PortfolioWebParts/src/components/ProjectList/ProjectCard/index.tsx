4ece8d80aed17c89c5b9878b12eb91d47557f81f
Tin Hoang Vu <tin.hoang@puzzlepart.com>
2021-09-02 12:10:42 +0200

    Removed alert message and added tooltip and cursor to ReadOnly projects

--- SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/index.tsx
@@ -6,18 +6,22 @@ import { ProjectCardFooter } from './ProjectCardFooter'
 import { ProjectCardHeader } from './ProjectCardHeader'
 import { IProjectCardProps } from './types'
 import * as strings from 'PortfolioWebPartsStrings'
+import { TooltipHost } from 'office-ui-fabric-react'
 
 export const ProjectCard: FunctionComponent<IProjectCardProps> = (props: IProjectCardProps) => {
   return (
+    <TooltipHost
+    content = {props.project.readOnly && (strings.NoAccessMessage)} 
+    >
     <DocumentCard
-      className={styles.projectCard}
-      onClickHref={props.project.readOnly ? '#' : props.project.url}
-      onClick={() => props.project.readOnly && alert(strings.NoAccessMessage)}
-      style={props.project.readOnly && { opacity: "20%", cursor: "not-allowed" }}
-      >
+    className={styles.projectCard}
+    onClickHref={props.project.readOnly ? '#' : props.project.url}
+    style={props.project.readOnly ? { opacity: "20%", cursor: "default" } : { opacity: "100%"}}
+    >
       <ProjectCardHeader {...props} />
       <ProjectCardContent {...props} />
       <ProjectCardFooter {...props} />
     </DocumentCard>
+      </TooltipHost>
   )
 }
