5b44085fde3847ca838c19affd0267365acc574f
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-09-02 15:06:27 +0200

    Using title html attribute

--- SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/index.tsx
@@ -1,27 +1,23 @@
 import { DocumentCard } from 'office-ui-fabric-react/lib/DocumentCard'
+import * as strings from 'PortfolioWebPartsStrings'
 import React, { FunctionComponent } from 'react'
 import styles from './ProjectCard.module.scss'
 import { ProjectCardContent } from './ProjectCardContent'
 import { ProjectCardFooter } from './ProjectCardFooter'
 import { ProjectCardHeader } from './ProjectCardHeader'
 import { IProjectCardProps } from './types'
-import * as strings from 'PortfolioWebPartsStrings'
-import { TooltipHost } from 'office-ui-fabric-react'
 
 export const ProjectCard: FunctionComponent<IProjectCardProps> = (props: IProjectCardProps) => {
   return (
-    <TooltipHost
-    content = {props.project.readOnly && (strings.NoAccessMessage)} 
-    >
     <DocumentCard
-    className={styles.projectCard}
-    onClickHref={props.project.readOnly ? '#' : props.project.url}
-    style={props.project.readOnly ? { opacity: "20%", cursor: "default" } : { opacity: "100%"}}
+      title={strings.NoAccessMessage}
+      className={styles.projectCard}
+      onClickHref={props.project.readOnly ? '#' : props.project.url}
+      style={props.project.readOnly ? { opacity: '20%', cursor: 'default' } : {}}
     >
       <ProjectCardHeader {...props} />
       <ProjectCardContent {...props} />
       <ProjectCardFooter {...props} />
     </DocumentCard>
-      </TooltipHost>
   )
 }
