17f6ce5ff8e4c40a3229bdbb24fd79661f6fa1d7
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2021-09-21 13:01:34 +0200

    More webparts props + list sorting + fixing issues adding new item

--- SharePointFramework/ProjectWebParts/src/webparts/projectTimeline/index.ts
+++ SharePointFramework/ProjectWebParts/src/webparts/projectTimeline/index.ts
@@ -1,14 +1,16 @@
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField,
+  PropertyPaneToggle
 } from '@microsoft/sp-property-pane'
 import '@pnp/polyfill-ie11'
 import { IProjectTimelineProps, ProjectTimeline } from 'components/ProjectTimeline'
 import 'office-ui-fabric-react/dist/css/fabric.min.css'
 import { BaseProjectWebPart } from 'webparts/@baseProjectWebPart'
+
 import * as strings from 'ProjectWebPartsStrings'
 
-export default class ProjectTimelineWebPart extends BaseProjectWebPart<IProjectTimelineProps> {
+export default class ProjectTimelineWebPart extends BaseProjectWebPart<IProjectTimelineProps>  {
   public async onInit() {
     await super.onInit()
   }
@@ -28,6 +30,26 @@ export default class ProjectTimelineWebPart extends BaseProjectWebPart<IProjectT
                 PropertyPaneTextField('listName', {
                   label: strings.ListNameFieldLabel,
                   value: 'Tidslinjeinnhold'
+                }),
+                PropertyPaneToggle('showFilterButton', {
+                  label: 'Vis filterknapp',
+                  checked: true
+                }),
+                PropertyPaneToggle('showTimeline', {
+                  label: 'Vis tidslinje',
+                  checked: true
+                }),
+                PropertyPaneToggle('showInfoMessage', {
+                  label: 'Vis infomelding',
+                  checked: true
+                }),
+                PropertyPaneToggle('showCmdTimelineList', {
+                  label: 'Vis kommandolinje for liste',
+                  checked: true
+                }),
+                PropertyPaneToggle('showTimelineList', {
+                  label: 'Vis liste',
+                  checked: true
                 })
               ]
             }
