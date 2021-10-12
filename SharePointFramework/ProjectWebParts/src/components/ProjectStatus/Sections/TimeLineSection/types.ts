5d532369427abe718126c503233dbf13e4d84e70
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-10-12 14:40:59 +0200

    Temp removed TimelineSection

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/TimeLineSection/types.ts
+++
@@ -1,9 +0,0 @@
-import { IListSectionProps, IListSectionState, IListSectionData } from '../ListSection'
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ITimelineSectionProps extends IListSectionProps { }
-
-export type ITimelineSectionState = IListSectionState<ITimelineData>
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ITimelineData extends IListSectionData { }
