bd9ac89767a03251e507ef43f3954310db5dfcae
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-10-18 15:34:11 +0200

    Improved render for multi user in portfolio overview

--- SharePointFramework/PortfolioWebParts/src/components/PortfolioOverview/RenderItemColumn/UserColumn/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/PortfolioOverview/RenderItemColumn/UserColumn/index.tsx
@@ -1,35 +1,49 @@
 import { Persona, PersonaPresence, PersonaSize } from 'office-ui-fabric-react/lib/Persona'
-import React, { Component, ReactElement } from 'react'
+import React, { FunctionComponent } from 'react'
 import { SearchValueType } from 'types'
 import { IRenderItemColumnProps } from '../IRenderItemColumnProps'
 
-export class UserColumn extends Component<IRenderItemColumnProps> {
-  public render(): ReactElement<IRenderItemColumnProps> {
-    if (this.props.column.searchType === SearchValueType.OWSUSER) {
-      const [email, text] = this.props.columnValue.split(' | ')
-      return (
-        <span>
-          <Persona
-            text={text}
-            onRenderPrimaryText={(props) => (
-              <div>
-                <a href={`mailto:${email}`}>{props.text}</a>
-              </div>
-            )}
-            size={PersonaSize.size24}
-            presence={PersonaPresence.none}
-          />
-        </span>
-      )
-    }
+export const UserColumn: FunctionComponent<IRenderItemColumnProps> = ({ columnValue, column }) => {
+  if (column.searchType === SearchValueType.OWSUSER) {
+    const [email, text] = columnValue.split(' | ')
     return (
       <span>
         <Persona
-          text={this.props.columnValue}
+          text={text}
+          onRenderPrimaryText={(props) => (
+            <div>
+              <a href={`mailto:${email}`}>{props.text}</a>
+            </div>
+          )}
           size={PersonaSize.size24}
           presence={PersonaPresence.none}
         />
       </span>
     )
   }
+  if (columnValue?.indexOf(';') !== -1) {
+    return (
+      <span>
+        {columnValue.split(';').map((value, key) =>
+          <span key={key}>
+            <Persona
+              styles={{ root: { marginTop: 4 } }}
+              text={value}
+              size={PersonaSize.size24}
+              presence={PersonaPresence.none}
+            />
+          </span>
+        )}
+      </span>
+    )
+  }
+  return (
+    <span>
+      <Persona
+        text={columnValue}
+        size={PersonaSize.size24}
+        presence={PersonaPresence.none}
+      />
+    </span>
+  )
 }
