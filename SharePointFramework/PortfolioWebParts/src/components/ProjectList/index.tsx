074d7b1714eab372cb9e1500fc9e6b34b72b8d74
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-10-20 15:00:26 +0200

    Fixed an issue with sorting for Project List

--- SharePointFramework/PortfolioWebParts/src/components/ProjectList/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/ProjectList/index.tsx
@@ -11,150 +11,48 @@ import { Toggle } from 'office-ui-fabric-react/lib/Toggle'
 import * as strings from 'PortfolioWebPartsStrings'
 import { ProjectInformationModal } from 'pp365-projectwebparts/lib/components/ProjectInformation'
 import { getObjectValue, sortAlphabetically } from 'pp365-shared/lib/helpers'
-import React, { Component } from 'react'
-import * as _ from 'underscore'
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import _ from 'underscore'
 import { ProjectCard } from './ProjectCard'
 import styles from './ProjectList.module.scss'
 import { PROJECTLIST_COLUMNS } from './ProjectListColumns'
 import { IProjectListProps, IProjectListState } from './types'
 
-/**
- * @component ProjectList
- * @extends Component
- */
-export class ProjectList extends Component<IProjectListProps, IProjectListState> {
-  public static defaultProps: Partial<IProjectListProps> = {
-    columns: PROJECTLIST_COLUMNS,
-    sortBy: 'Title'
-  }
+export const ProjectList: FunctionComponent<IProjectListProps> = (props) => {
+  const [state, setState] = useState<IProjectListState>({
+    loading: true,
+    searchTerm: '',
+    showAsTiles: props.showAsTiles,
+    selectedView: 'my_projects'
+  })
 
-  /**
-   * Constructor
-   *
-   * @param props - Props
-   */
-  constructor(props: IProjectListProps) {
-    super(props)
-    this.state = {
-      loading: true,
-      searchTerm: '',
-      showAsTiles: props.showAsTiles,
-      selectedView: 'my_projects'
-    }
-  }
 
-  public async componentDidMount() {
-    try {
-      // eslint-disable-next-line prefer-const
-      let [projects, isUserInPortfolioManagerGroup] = await Promise.all([
-        this.props.dataAdapter.fetchEncrichedProjects(),
-        this.props.dataAdapter.isUserInGroup(strings.PortfolioManagerGroupName)
-      ])
-      const columns = this.props.columns.map((col) => {
-        if (col.fieldName === this.props.sortBy) {
-          col.isSorted = true
-          col.isSortedDescending = true
-        }
-        return col
-      })
-      this.setState({
-        projects,
-        listView: { projects, columns },
-        loading: false,
-        isUserInPortfolioManagerGroup
-      })
-      if (this.props.showProjectLogo) {
-        this._getProjectLogos(20)
-      }
-    } catch (error) {
-      this.setState({ error, loading: false })
-    }
-  }
-
-  /**
-   * Renders the <ProjectList /> component
-   */
-  public render(): React.ReactElement<IProjectListProps> {
-    if (this.state.loading) {
-      return (
-        <div className={styles.root}>
-          <Spinner label={this.props.loadingText} size={SpinnerSize.large} />
-        </div>
-      )
-    }
-    if (this.state.error) {
-      return (
-        <div className={styles.root}>
-          <MessageBar messageBarType={MessageBarType.error}>{strings.ErrorText}</MessageBar>
-        </div>
-      )
-    }
-    const projects = this._filterProjets(this.state.projects)
-    return (
-      <div className={styles.root}>
-        <div className={styles.container}>
-          <div className={styles.searchBox} hidden={!this.props.showSearchBox}>
-            <SearchBox
-              placeholder={this.props.searchBoxPlaceholderText}
-              onChanged={this._onSearch.bind(this)}
-            />
-          </div>
-          {this.state.isUserInPortfolioManagerGroup && (
-            <div className={styles.projectDisplaySelect}>
-              <Pivot
-                onLinkClick={({ props }) => this.setState({ selectedView: props.itemKey })}
-                selectedKey={this.state.selectedView}>
-                <PivotItem headerText={strings.MyProjectsLabel} itemKey='my_projects' />
-                <PivotItem headerText={strings.AllProjectsLabel} itemKey='all_projects' />
-              </Pivot>
-            </div>
-          )}
-          <div className={styles.viewToggle} hidden={!this.props.showViewSelector}>
-            <Toggle
-              offText={strings.ShowAsListText}
-              onText={strings.ShowAsTilesText}
-              defaultChecked={this.state.showAsTiles}
-              inlineLabel={true}
-              onChanged={(showAsTiles) => this.setState({ showAsTiles })}
-            />
-          </div>
-          <div className={styles.emptyMessage} hidden={projects.length > 0}>
-            <MessageBar>{strings.NoSearchResults}</MessageBar>
-          </div>
-          <div className={styles.projects} hidden={projects.length === 0}>
-            {this._renderProjects(projects)}
-          </div>
-        </div>
-        {this._renderProjectInformation()}
-      </div>
-    )
-  }
 
   /**
    * Render projects
    *
    * @param projects - Projects
    */
-  private _renderProjects(projects: ProjectListModel[]) {
-    if (this.state.showAsTiles) {
+  function renderProjects(projects: ProjectListModel[]) {
+    if (state.showAsTiles) {
       return projects.map((project, idx) => (
         <ProjectCard
           key={idx}
           project={project}
           shouldTruncateTitle={true}
-          showProjectLogo={this.props.showProjectLogo}
-          showProjectOwner={this.props.showProjectOwner}
-          showProjectManager={this.props.showProjectManager}
-          actions={this._getCardActions(project)}
+          showProjectLogo={props.showProjectLogo}
+          showProjectOwner={props.showProjectOwner}
+          showProjectManager={props.showProjectManager}
+          actions={getCardActions(project)}
         />
       ))
     } else {
       return (
         <DetailsList
-          items={projects}
-          columns={this.state.listView.columns}
-          onRenderItemColumn={this._onRenderItemColumn.bind(this)}
-          onColumnHeaderClick={this._onListSort.bind(this)}
+          items={filterProjets(state.listView.projects)}
+          columns={state.listView.columns}
+          onRenderItemColumn={onRenderItemColumn}
+          onColumnHeaderClick={onListSort}
           selectionMode={SelectionMode.none}
         />
       )
@@ -168,7 +66,7 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
    * @param _index - Index
    * @param column - Column
    */
-  private _onRenderItemColumn(project: ProjectListModel, _index: number, column: IColumn) {
+  function onRenderItemColumn(project: ProjectListModel, _index: number, column: IColumn) {
     const colValue = getObjectValue(project, column.fieldName, null)
     if (column.fieldName === 'title') {
       if (project.userIsMember) return <a href={project.url}>{colValue}</a>
@@ -183,8 +81,8 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
    * @param _evt - Event
    * @param column - Column
    */
-  private _onListSort(_evt: React.MouseEvent<any>, column: IColumn): void {
-    const { listView } = { ...this.state } as IProjectListState
+  function onListSort(_evt: React.MouseEvent<any>, column: IColumn): void {
+    const { listView } = { ...state } as IProjectListState
     let isSortedDescending = column.isSortedDescending
     if (column.isSorted) {
       isSortedDescending = !isSortedDescending
@@ -201,24 +99,24 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
       }
       return col
     })
-    this.setState({ listView })
+    setState({ ...state, listView })
   }
 
   /**
    * Render <ProjectInformationModal />
    */
-  private _renderProjectInformation() {
-    if (this.state.showProjectInfo) {
+  function renderProjectInformation() {
+    if (state.showProjectInfo) {
       return (
         <ProjectInformationModal
-          modalProps={{ isOpen: true, onDismiss: () => this.setState({ showProjectInfo: null }) }}
-          title={this.state.showProjectInfo.title}
-          webUrl={this.props.pageContext.site.absoluteUrl}
+          modalProps={{ isOpen: true, onDismiss: () => setState({ ...state, showProjectInfo: null }) }}
+          title={state.showProjectInfo.title}
+          webUrl={props.pageContext.site.absoluteUrl}
           hubSite={{
-            web: new Web(this.props.pageContext.site.absoluteUrl),
-            url: this.props.pageContext.site.absoluteUrl
+            web: new Web(props.pageContext.site.absoluteUrl),
+            url: props.pageContext.site.absoluteUrl
           }}
-          siteId={this.state.showProjectInfo.siteId}
+          siteId={state.showProjectInfo.siteId}
           hideActions={true}
           page='Portfolio'
         />
@@ -232,12 +130,12 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
    *
    * @param project - Project
    */
-  private _getCardActions(project: ProjectListModel): IButtonProps[] {
+  function getCardActions(project: ProjectListModel): IButtonProps[] {
     return [
       {
         id: 'ON_SELECT_PROJECT',
         iconProps: { iconName: 'OpenInNewWindow' },
-        onClick: (event: React.MouseEvent<any>) => this._onExecuteCardAction(event, project)
+        onClick: (event: React.MouseEvent<any>) => onExecuteCardAction(event, project)
       }
     ]
   }
@@ -248,13 +146,13 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
    * @param event - Event
    * @param project - Project
    */
-  private _onExecuteCardAction(event: React.MouseEvent<any>, project: ProjectListModel) {
+  function onExecuteCardAction(event: React.MouseEvent<any>, project: ProjectListModel) {
     event.preventDefault()
     event.stopPropagation()
     switch (event.currentTarget.id) {
       case 'ON_SELECT_PROJECT':
         {
-          this.setState({ showProjectInfo: project })
+          setState({ ...state, showProjectInfo: project })
         }
         break
     }
@@ -265,10 +163,10 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
    *
    * @param projects - Projects
    */
-  private _filterProjets(projects: ProjectListModel[]) {
+  function filterProjets(projects: ProjectListModel[]) {
     return projects
       .filter((project) => {
-        if (this.state.selectedView === 'my_projects') return project.userIsMember
+        if (state.selectedView === 'my_projects') return project.userIsMember
         return true
       })
       .filter((p) => {
@@ -277,7 +175,7 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
           return (
             value &&
             typeof value === 'string' &&
-            value.toLowerCase().indexOf(this.state.searchTerm) !== -1
+            value.toLowerCase().indexOf(state.searchTerm) !== -1
           )
         }).length
         return matches > 0
@@ -289,8 +187,8 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
    *
    * @param searchTerm - Search term
    */
-  private _onSearch(searchTerm: string) {
-    this.setState({ searchTerm: searchTerm.toLowerCase() })
+  function onSearch(searchTerm: string) {
+    setState({ ...state, searchTerm: searchTerm.toLowerCase() })
   }
 
   /**
@@ -298,15 +196,15 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
    *
    * @param batchSize - Batch size (defaults to 20)
    */
-  private async _getProjectLogos(batchSize: number = 20) {
-    const requests = this.state.projects.map((p) => ({
+  async function getProjectLogos(batchSize: number = 20) {
+    const requests = state.projects.map((p) => ({
       id: p.groupId,
       method: 'GET',
       url: `groups/${p.groupId}/photo/$value`
     }))
     while (requests.length > 0) {
       const { responses } = await MSGraph.Batch(requests.splice(0, batchSize))
-      this.setState((prevState: IProjectListState) => {
+      setState((prevState: IProjectListState) => {
         const projects = prevState.projects.map((p) => {
           const response = _.find(responses, (r) => r.id === p.groupId && r.status === 200)
           if (response) {
@@ -314,10 +212,99 @@ export class ProjectList extends Component<IProjectListProps, IProjectListState>
           }
           return p
         })
-        return { projects }
+        return { ...state, projects }
       })
     }
   }
+
+  useEffect(() => {
+    Promise.all([
+      props.dataAdapter.fetchEncrichedProjects(),
+      props.dataAdapter.isUserInGroup(strings.PortfolioManagerGroupName)
+    ])
+      .then(([projects, isUserInPortfolioManagerGroup]) => {
+        const columns = props.columns.map((col) => {
+          if (col.fieldName === props.sortBy) {
+            col.isSorted = true
+            col.isSortedDescending = true
+          }
+          return col
+        })
+        setState({
+          ...state,
+          projects,
+          listView: { projects, columns },
+          loading: false,
+          isUserInPortfolioManagerGroup
+        })
+        if (props.showProjectLogo) {
+          getProjectLogos(20)
+        }
+      })
+  }, [])
+
+
+
+
+  if (state.loading) {
+    return (
+      <div className={styles.root}>
+        <Spinner label={props.loadingText} size={SpinnerSize.large} />
+      </div>
+    )
+  }
+  if (state.error) {
+    return (
+      <div className={styles.root}>
+        <MessageBar messageBarType={MessageBarType.error}>{strings.ErrorText}</MessageBar>
+      </div>
+    )
+  }
+
+  const projects = filterProjets(state.projects)
+  return (
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <div className={styles.searchBox} hidden={!props.showSearchBox}>
+          <SearchBox
+            placeholder={props.searchBoxPlaceholderText}
+            onChanged={onSearch.bind(this)}
+          />
+        </div>
+        {state.isUserInPortfolioManagerGroup && (
+          <div className={styles.projectDisplaySelect}>
+            <Pivot
+              onLinkClick={({ props }) => setState({ ...state, selectedView: props.itemKey })}
+              selectedKey={state.selectedView}>
+              <PivotItem headerText={strings.MyProjectsLabel} itemKey='my_projects' />
+              <PivotItem headerText={strings.AllProjectsLabel} itemKey='all_projects' />
+            </Pivot>
+          </div>
+        )}
+        <div className={styles.viewToggle} hidden={!props.showViewSelector}>
+          <Toggle
+            offText={strings.ShowAsListText}
+            onText={strings.ShowAsTilesText}
+            defaultChecked={state.showAsTiles}
+            inlineLabel={true}
+            onChanged={(showAsTiles) => setState({ ...state, showAsTiles })}
+          />
+        </div>
+        <div className={styles.emptyMessage} hidden={projects.length > 0}>
+          <MessageBar>{strings.NoSearchResults}</MessageBar>
+        </div>
+        <div className={styles.projects} hidden={projects.length === 0}>
+          {renderProjects(projects)}
+        </div>
+      </div>
+      {renderProjectInformation()}
+    </div>
+  )
 }
 
-export { IProjectListProps }
+ProjectList.defaultProps = {
+  columns: PROJECTLIST_COLUMNS,
+  sortBy: 'Title'
+}
+
+export * from './types'
