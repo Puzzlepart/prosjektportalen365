ef644cf32ae25c390cb08fefde4a568560058f3f
Martin Ruud <martin.ruud@puzzlepart.com>
2021-10-21 16:08:21 +0200

    Added local portfoliowebparts to node_modules

--- SharePointFramework/ProgramWebParts/src/components/ProgramAddProject/ProgramAddProject.tsx
+++ SharePointFramework/ProgramWebParts/src/components/ProgramAddProject/ProgramAddProject.tsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
 import styles from './ProgramAddProject.module.scss';
 import { IProgramAddProjectProps } from './IProgramAddProjectProps';
-import { ProjectTimeline } from '../../../../PortfolioWebParts/lib/components/ProjectTimeline'
-
+import { Timeline } from 'pp365-portfoliowebparts/lib/components/ProjectTimeline/Timeline'
+import moment from 'moment'
 export default class ProgramAddProject extends React.Component<IProgramAddProjectProps, {}> {
   public render(): React.ReactElement<IProgramAddProjectProps> {
     return (
       <>
-        <ProjectTimeline dataSource="Tedst" />
+        <Timeline items={items} groups={groups} _onItemClick={() => console.log()} defaultTimeStart={[-1, 'months']}
+          defaultTimeEnd={[1, 'years']} />
       </>
     );
   }
 }
+
+const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }]
+
+const items = [
+  {
+    id: 1,
+    group: 1,
+    title: 'item 1',
+    start_time: moment(),
+    end_time: moment().add(1, 'hour')
+  },
+  {
+    id: 2,
+    group: 2,
+    title: 'item 2',
+    start_time: moment().add(-0.5, 'hour'),
+    end_time: moment().add(0.5, 'hour')
+  },
+  {
+    id: 3,
+    group: 1,
+    title: 'item 3',
+    start_time: moment().add(2, 'hour'),
+    end_time: moment().add(3, 'hour')
+  }
+]
