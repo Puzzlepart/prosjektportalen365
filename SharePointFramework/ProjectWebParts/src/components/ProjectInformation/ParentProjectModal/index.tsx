6d98c5728c0a1e99db5bbcac3154174d7d24bd10
Rundez <m.ruud93@gmail.com>
2021-10-28 14:10:26 +0200

    Project setup template selection can now be skipped.

--- SharePointFramework/ProjectWebParts/src/components/ProjectInformation/ParentProjectModal/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformation/ParentProjectModal/index.tsx
@@ -1,28 +1,23 @@
 import React, { FunctionComponent, useState } from 'react'
 import { Dialog, DialogType, DialogFooter, PrimaryButton, DefaultButton, Spinner, SpinnerSize } from 'office-ui-fabric-react'
 import { sp } from '@pnp/sp'
-
-interface ParentModalProps {
-    isOpen: boolean;
-    onDismiss: () => void
-}
+import { userCustomAction, ParentModalProps } from './types'
 
 export const CreateParentModal: FunctionComponent<ParentModalProps> = ({ isOpen, onDismiss }) => {
     const [isLoading, setLoading] = useState(false)
 
-    async function applySiteDesign() {
+    async function applyCustomAction() {
         setLoading(true)
 
-        await sp.siteDesigns.applySiteDesign("bf3dfdb8-abc7-4f27-97f5-5ae9b26477bd", window.location.href)
+        await sp.web.userCustomActions.add(userCustomAction);
         location.reload()
-
     }
 
     return (
         <>
             <Dialog hidden={!isOpen} onDismiss={onDismiss} dialogContentProps={dialogContentProps}>
                 {!isLoading && <DialogFooter>
-                    <PrimaryButton text="Gjør om" onClick={() => applySiteDesign()} />
+                    <PrimaryButton text="Gjør om" onClick={() => applyCustomAction()} />
                     <DefaultButton text="Avbryt" onClick={() => onDismiss()} />
                 </DialogFooter>}
                 {isLoading && <Spinner size={SpinnerSize.medium} />}
