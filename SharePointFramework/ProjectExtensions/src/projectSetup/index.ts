25c14509a25c12c1bd366403ddcdda0d91e79d26
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-10-29 12:11:48 +0200

    ProjectSetup: Should not throw AlreadySetup error if property forceTemplate is set

--- SharePointFramework/ProjectExtensions/src/projectSetup/index.ts
+++ SharePointFramework/ProjectExtensions/src/projectSetup/index.ts
@@ -76,11 +76,13 @@ export default class ProjectSetup extends BaseApplicationCustomizer<IProjectSetu
           )
         }
         case ProjectSetupValidation.AlreadySetup: {
-          throw new ProjectSetupError(
-            'AlreadySetup',
-            strings.ProjectAlreadySetupMessage,
-            strings.ProjectAlreadySetupStack
-          )
+          if (!stringIsNullOrEmpty(this.properties.forceTemplate)) {
+            throw new ProjectSetupError(
+              'AlreadySetup',
+              strings.ProjectAlreadySetupMessage,
+              strings.ProjectAlreadySetupStack
+            )
+          }
         }
       }
 
@@ -373,14 +375,14 @@ export default class ProjectSetup extends BaseApplicationCustomizer<IProjectSetu
         ),
         this.properties.extensionsLibrary
           ? this._portal.getItems(
-              this.properties.extensionsLibrary,
-              ProjectExtension,
-              {
-                ViewXml:
-                  '<View Scope="RecursiveAll"><Query><Where><Eq><FieldRef Name="FSObjType" /><Value Type="Integer">0</Value></Eq></Where></Query></View>'
-              },
-              ['File', 'FieldValuesAsText']
-            )
+            this.properties.extensionsLibrary,
+            ProjectExtension,
+            {
+              ViewXml:
+                '<View Scope="RecursiveAll"><Query><Where><Eq><FieldRef Name="FSObjType" /><Value Type="Integer">0</Value></Eq></Where></Query></View>'
+            },
+            ['File', 'FieldValuesAsText']
+          )
           : Promise.resolve([]),
         this.properties.contentConfigList
           ? this._portal.getItems(this.properties.contentConfigList, ListContentConfig)
