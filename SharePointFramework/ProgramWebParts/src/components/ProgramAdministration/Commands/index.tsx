aca1cbecfc415198ea40147a7278cef46161e28f
Martin Ruud <martin.ruud@puzzlepart.com>
2021-11-22 13:16:52 +0100

    Added spinner to loading state in commandbar

--- SharePointFramework/ProgramWebParts/src/components/ProgramAdministration/Commands/index.tsx
+++ SharePointFramework/ProgramWebParts/src/components/ProgramAdministration/Commands/index.tsx
@@ -3,7 +3,8 @@ import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/Com
 import { useStore } from '../store'
 import { SPRest } from '@pnp/sp'
 import * as strings from 'ProgramWebPartsStrings'
-import { Spinner } from 'office-ui-fabric-react'
+import { Spinner, ProgressIndicator } from 'office-ui-fabric-react'
+
 
 
 interface commandBarProps {
@@ -16,7 +17,16 @@ export const Commandbar: FunctionComponent<commandBarProps> = ({ _sp }) => {
     const selectedProjectsToDelete = useStore(state => state.selectedProjectsToDelete)
     const isLoading = useStore(state => state.isLoading)
 
-    const _items: ICommandBarItemProps[] = isLoading ? [
+
+    const getLoadingBar = () => {
+        const commandBarButtonAs = () => <div style={{ width: '120px', display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}><Spinner /></div>
+        if (isLoading) {
+            return commandBarButtonAs
+        }
+    }
+
+
+    const _items: ICommandBarItemProps[] = [
         {
             key: 'newItem',
             text: strings.ProgramAddProjectButtonText,
@@ -29,31 +39,11 @@ export const Commandbar: FunctionComponent<commandBarProps> = ({ _sp }) => {
             text: strings.ProgramRemoveChildButtonText,
             iconProps: { iconName: 'Delete' },
             buttonStyles: { root: { border: 'none' } },
-            disabled: selectedProjectsToDelete == null,
+            disabled: selectedProjectsToDelete?.length > 0 ? false : true,
             onClick: (): any => {
                 deleteChildProjects(selectedProjectsToDelete, _sp)
             },
-            commandBarButtonAs: () => <Spinner />
-        
-        },
-    ] : [
-        {
-            key: 'newItem',
-            text: strings.ProgramAddProjectButtonText,
-            iconProps: { iconName: 'Add' },
-            buttonStyles: { root: { border: 'none' } },
-            onClick: () => toggleProjectDialog()
-        },
-        {
-            key: 'delete',
-            text: strings.ProgramRemoveChildButtonText,
-            iconProps: { iconName: 'Delete' },
-            buttonStyles: { root: { border: 'none' } },
-            disabled: selectedProjectsToDelete == null,
-            onClick: (): any => {
-                deleteChildProjects(selectedProjectsToDelete, _sp)
-            }
-        
+            commandBarButtonAs: getLoadingBar()
         }
     ]
 
