98666d7f296650d1c28ac7757079144fc0b60084
Rundez <m.ruud93@gmail.com>
2021-12-28 14:59:31 +0100

    Risk matrix can now be customized

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/index.tsx
@@ -324,7 +324,8 @@ export class ProjectStatus extends React.Component<IProjectStatusProps, IProject
                 riskMatrix={{
                   width: riskMatrixWidth,
                   height: riskMatrixHeight,
-                  calloutTemplate: riskMatrixCalloutTemplate
+                  calloutTemplate: riskMatrixCalloutTemplate,
+                  pageContext: this.props.pageContext
                 }}
               />
             )
--- SharePointFramework/ProjectWebParts/src/components/RiskMatrix/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/RiskMatrix/index.tsx
@@ -1,22 +1,44 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect } from 'react'
 import { IRiskMatrixProps } from './types'
 import { MatrixRows } from './MatrixRow'
 import styles from './RiskMatrix.module.scss'
+import { Web, } from '@pnp/sp/'
+import { PageContext } from '@microsoft/sp-page-context'
 
 export const RiskMatrix: FunctionComponent<IRiskMatrixProps> = ({
   items = [],
   width = 400,
   height = 300,
-  calloutTemplate
+  calloutTemplate,
+  pageContext,
 }: IRiskMatrixProps) => {
+  const [jsonConfig, setJsonConfig] = React.useState([])
+
+  useEffect(() => {
+    async function fetchJson() {
+      await fetchJsonConfiguration(pageContext)
+    }
+    pageContext && fetchJson()
+  }, [])
+
+  async function fetchJsonConfiguration(pageContext: PageContext) {
+    const web: Web = new Web(pageContext.web.absoluteUrl)
+    const { ServerRelativeUrl } = await web.get()
+    const json = await web.getFileByServerRelativeUrl(ServerRelativeUrl + '/SiteAssets/custom-cells.txt').getJSON()
+    setJsonConfig(json)
+  }
+  
+
   return (
     <div className={styles.riskMatrix} style={{ width, height }}>
       <table className={styles.table}>
         <tbody>
-          <MatrixRows items={items} calloutTemplate={calloutTemplate} />
+          <MatrixRows items={items} calloutTemplate={calloutTemplate} customCells={jsonConfig}/>
         </tbody>
       </table>
     </div>
   )
 }
+
+
 export * from './types'
