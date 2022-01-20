6f274a9a2bac959d1d4caf7010eba094bfb4eef5
Sindre <sindre.furulund@puzzlepart.com>
2022-01-20 17:35:53 +0100

    Added new Card component & scss-file

---
+++ SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/Card/Card.tsx
@@ -0,0 +1,53 @@
+import React, { FunctionComponent, useState } from 'react'
+import styles from './Card.module.scss'
+import { IProjectCardProps } from '../types'
+import { placeholderImage } from '../../types'
+import { DocumentCardTitle } from 'office-ui-fabric-react/lib/DocumentCard'
+import { Facepile, IFacepilePersona, PersonaSize } from 'office-ui-fabric-react'
+import { Icon } from 'office-ui-fabric-react/lib/Icon'
+import strings from 'PortfolioWebPartsStrings'
+
+export const Card: FunctionComponent<IProjectCardProps> = ({
+  project,
+  shouldTruncateTitle
+}: IProjectCardProps) => {
+  const ownerPersona = {
+    personaName: project.owner
+      ? `${project.owner.text} | ${strings.ProjectOwner}`
+      : `Prosjekteier ikke satt`,
+    imageUrl: project.owner ? project.owner.imageUrl : null,
+    title: strings.ProjectOwner
+  }
+  const managerPersona = {
+    personaName: project.manager
+      ? `${project.manager.text} | ${strings.ProjectManager}`
+      : `Prosjektleder ikke satt`,
+    imageUrl: project.manager ? project.manager.imageUrl : null,
+    title: strings.ProjectManager
+  }
+
+  const personas: IFacepilePersona[] = []
+  personas.push(ownerPersona)
+  personas.push(managerPersona)
+
+  return (
+    <div className={styles.card}>
+      <img className={styles.logo} src={project.logo ?? placeholderImage} />
+      <DocumentCardTitle
+        className={styles.title}
+        title={project.title}
+        shouldTruncate={shouldTruncateTitle}
+      />
+      <hr />
+      <div className={styles.content}>
+        <div className={styles.phase}>
+          <Icon className={styles.icon} iconName='AlarmClock' />
+          <p className={styles.text}>{project.phase ? project.phase : 'Ikke satt'}</p>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <Facepile personaSize={PersonaSize.size32} personas={personas} />
+      </div>
+    </div>
+  )
+}
--- SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/index.tsx
@@ -1,23 +1,8 @@
-import { DocumentCard } from 'office-ui-fabric-react/lib/DocumentCard'
-import * as strings from 'PortfolioWebPartsStrings'
 import React, { FunctionComponent } from 'react'
-import styles from './ProjectCard.module.scss'
-import { ProjectCardContent } from './ProjectCardContent'
-import { ProjectCardFooter } from './ProjectCardFooter'
-import { ProjectCardHeader } from './ProjectCardHeader'
+import { Card } from './Card/Card'
 import { IProjectCardProps } from './types'
 
 export const ProjectCard: FunctionComponent<IProjectCardProps> = (props) => {
   const { project } = props
-  return (
-    <DocumentCard
-      title={!project.userIsMember ? strings.NoAccessMessage : ''}
-      className={styles.projectCard}
-      onClickHref={project.userIsMember ? project.url : '#'}
-      style={!project.userIsMember ? { opacity: '20%', cursor: 'default' } : {}}>
-      <ProjectCardHeader {...props} />
-      <ProjectCardContent {...props} />
-      <ProjectCardFooter {...props} />
-    </DocumentCard>
-  )
+  return <Card {...props} />
 }
