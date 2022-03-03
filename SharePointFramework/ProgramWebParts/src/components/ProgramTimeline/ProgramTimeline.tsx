54db184e2f3301eb01deffb4ff54ff06d5db501b
Petter <petter.braten@puzzlepart.com>
2022-03-03 10:22:46 +0100

    Minor adjustments to columns and title

--- SharePointFramework/ProgramWebParts/src/components/ProgramTimeline/ProgramTimeline.tsx
+++ SharePointFramework/ProgramWebParts/src/components/ProgramTimeline/ProgramTimeline.tsx
@@ -1,14 +1,13 @@
 import React, { FunctionComponent } from 'react'
 import { IProgramTimelineProps } from 'webparts/programTimeline/ProgramTimelineWebPart'
 import { ProjectTimeline } from 'pp365-portfoliowebparts/lib/components/ProjectTimeline/index'
-import * as strings from 'ProgramWebPartsStrings'
 
 export const ProgramTimeline: FunctionComponent<IProgramTimelineProps> = (props) => {
 
   return (
     <>
       <ProjectTimeline
-        title={strings.ProgramTimelineHeader}
+        title={props.webPartTitle}
         dataSource={props.dataSource}
         dataAdapter={props.dataAdapter}
         pageContext={props.context.pageContext as any}
