d0ca4bf829b00638b68abad76268772d36afe947
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-03-07 15:42:09 +0100

    Update StatusReport.ts

--- SharePointFramework/@Shared/src/models/StatusReport.ts
+++ SharePointFramework/@Shared/src/models/StatusReport.ts
@@ -11,7 +11,7 @@ export class StatusReport {
   public created: Date
   public defaultEditFormUrl: string
   public modified: Date
-  public gtLastReportDate: Date
+  public publishedDate: Date
 
   /**
    * Creates a new instance of StatusReport
@@ -23,7 +23,7 @@ export class StatusReport {
     this.id = _item.Id
     this.created = new Date(_item.Created)
     this.modified = new Date(_item.Modified)
-    this.gtLastReportDate = new Date(_item.GtLastReportDate)
+    this.publishedDate = new Date(_item.GtLastReportDate)
   }
 
   public setDefaultEditFormUrl(defaultEditFormUrl: string) {
