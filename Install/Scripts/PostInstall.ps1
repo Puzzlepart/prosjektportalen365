05acbe6ebce32767330d910ee874a90139abcba1
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-04-01 18:56:19 +0200

    1.5.0 prep (#687) [noci]

--- Install/Scripts/PostInstall.ps1
+++ Install/Scripts/PostInstall.ps1
@@ -1,31 +1,24 @@
-Set-PnPList -Identity Prosjektkolonnekonfigurasjon -EnableContentTypes:$false 
-Set-PnPList -Identity Fasesjekkliste -EnableContentTypes:$false 
-Set-PnPList -Identity Konfigurasjon -EnableContentTypes:$false 
-Set-PnPList -Identity Porteføljevisninger -EnableContentTypes:$false 
-Set-PnPList -Identity Prosjektkolonner -EnableContentTypes:$false 
-Set-PnPList -Identity Ressursallokering -EnableContentTypes:$false 
-Set-PnPList -Identity Planneroppgaver -EnableContentTypes:$false 
+Connect-PnPOnline -Url "https://puzzlepart.sharepoint.com/sites/pp365" -UseWebLogin
 
-Write-Host "[INFO] Post-install action: Ensuring default project templates"
 $TemplateSetups = Get-PnPListItem -List "Maloppsett"
 $TemplateFiles = Get-PnPListItem -List "Prosjektmaler"
 
-$TemplatesAndPages = @{
+$TemplatesMap = @{
     "Standardmal" = "Standardmal.txt";
     "Programmal" = "Programmal.txt";
     "Overordnet mal" = "Overordnet.txt";
 }
 
-$TemplatesAndPages.Keys | ForEach-Object {
-    $TemplateSetup = $TemplateSetups | Where-Object { $_["Title"] -eq $_ }
-    $TemplateFile = $TemplateFiles | Where-Object { $_["FileLeafRef"] -eq $TemplatesAndPages[$_] }
-    if ($null -ne $TemplateFile -and $null -ne $TemplateSetup) {
-        $TemplateSetup["GtProjectTemplate"] = $TemplateFile.ID
-        $TemplateSetup.Update()
+foreach ($tmpl in $TemplatesMap.GetEnumerator()) {
+    $TemplateSetup = $TemplateSetups | Where-Object { $_["Title"] -eq $tmpl.Name }
+    $TemplateFileId = $TemplateFiles | Where-Object { $_["FileLeafRef"] -eq $tmpl.Value } | Select-Object -ExpandProperty Id
+    if ($null -ne $TemplateFileId -and $null -ne $TemplateSetup) {
+        $TemplateSetup["GtProjectTemplate"] = $TemplateFileId
+        $TemplateSetup.SystemUpdate()
         $TemplateSetup.Context.ExecuteQuery()
     }
     else {
-        Write-Host "[WARNING] Cannot find template $_ or template file $($TemplatesAndPages[$_]). Ensure template associations manually in Lists/Maloppsett" -ForegroundColor Yellow
+        Write-Host "[WARNING] Cannot find template $($tmpl.Name) or template file $($tmpl.Value). Ensure template associations manually in Lists/Maloppsett" -ForegroundColor Yellow
     }
 }
 
