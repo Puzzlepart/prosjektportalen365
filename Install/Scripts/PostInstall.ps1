26eb45b163d820ee3fdb8332bb2ed8de97f3eebe
Tarjei E. Ormestøyl <ormestoyl@gmail.com>
2022-04-01 15:11:10 +0200

    Improved template ensure code

--- Install/Scripts/PostInstall.ps1
+++ Install/Scripts/PostInstall.ps1
@@ -10,36 +10,23 @@ Write-Host "[INFO] Post-install action: Ensuring default project templates"
 $TemplateSetups = Get-PnPListItem -List "Maloppsett"
 $TemplateFiles = Get-PnPListItem -List "Prosjektmaler"
 
-$TemplateSetup = $TemplateSetups | Where-Object { $_["Title"] -eq "Standardmal" }
-$TemplateFile = $TemplateFiles | Where-Object { $_["FileLeafRef"] -eq "Standardmal.txt" }
-if ($null -ne $TemplateFile -and $null -ne $TemplateSetup) {
-    $TemplateSetup["GtProjectTemplate"] = $TemplateFile.ID
-    $TemplateSetup.Update()
-    $TemplateSetup.Context.ExecuteQuery()
-}
-else {
-    Write-Host "[WARNING] Cannot find template Standardmal or template file Standardmal.txt. Ensure template associations manually in Lists/Maloppsett" -ForegroundColor Yellow
+$TemplatesAndPages = @{
+    "Standardmal" = "Standardmal.txt";
+    "Programmal" = "Programmal.txt";
+    "Overordnet mal" = "Overordnet.txt";
 }
 
-$TemplateSetup = $TemplateSetups | Where-Object { $_["Title"] -eq "Programmal" }
-$TemplateFile = $TemplateFiles | Where-Object { $_["FileLeafRef"] -eq "Programmal.txt" }
-if ($null -ne $TemplateFile -and $null -ne $TemplateSetup) {
-    $TemplateSetup["GtProjectTemplate"] = $TemplateFile.ID
-    $TemplateSetup.Update()
-    $TemplateSetup.Context.ExecuteQuery()
-}
-else {
-    Write-Host "[WARNING] Cannot find template Programmal or template file Programmal.txt. Ensure template associations manually in Lists/Maloppsett" -ForegroundColor Yellow
+$TemplatesAndPages.Keys | ForEach-Object {
+    $TemplateSetup = $TemplateSetups | Where-Object { $_["Title"] -eq $_ }
+    $TemplateFile = $TemplateFiles | Where-Object { $_["FileLeafRef"] -eq $TemplatesAndPages[$_] }
+    if ($null -ne $TemplateFile -and $null -ne $TemplateSetup) {
+        $TemplateSetup["GtProjectTemplate"] = $TemplateFile.ID
+        $TemplateSetup.Update()
+        $TemplateSetup.Context.ExecuteQuery()
+    }
+    else {
+        Write-Host "[WARNING] Cannot find template $_ or template file $($TemplatesAndPages[$_]). Ensure template associations manually in Lists/Maloppsett" -ForegroundColor Yellow
+    }
 }
 
-$TemplateSetup = $TemplateSetups | Where-Object { $_["Title"] -eq "Overordnet mal" }
-$TemplateFile = $TemplateFiles | Where-Object { $_["FileLeafRef"] -eq "Overordnet.txt" }
-if ($null -ne $TemplateFile -and $null -ne $TemplateSetup) {
-    $TemplateSetup["GtProjectTemplate"] = $TemplateFile.ID
-    $TemplateSetup.Update()
-    $TemplateSetup.Context.ExecuteQuery()
-}
-else {
-    Write-Host "[WARNING] Cannot find template Overordnet mal or template file Overordnet.txt. Ensure template associations manually in Lists/Maloppsett" -ForegroundColor Yellow
-}
 Write-Host "[SUCCESS] Post-install action: Ensured default project templates"
