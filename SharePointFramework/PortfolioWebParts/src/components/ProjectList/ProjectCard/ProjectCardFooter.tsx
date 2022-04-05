40b9f2d5bfde0870ecc12c0a552b4123cba96013
Sindre <sindrefuru@hotmail.no>
2022-04-05 09:42:03 +0200

    Removed unused components

--- SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/ProjectCardFooter.tsx
+++
@@ -1,9 +0,0 @@
-import { DocumentCardActions } from 'office-ui-fabric-react/lib/DocumentCard'
-import React, { FunctionComponent } from 'react'
-import { IProjectCardProps } from './types'
-
-export const ProjectCardFooter: FunctionComponent<IProjectCardProps> = ({
-  actions
-}: IProjectCardProps): JSX.Element => {
-  return <DocumentCardActions actions={actions} />
-}
