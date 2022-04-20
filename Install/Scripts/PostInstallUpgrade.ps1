62b44fbf8111b806861aade602916191ce9c49b9
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2022-04-20 13:16:48 +0200

    Add merging of timeline content data to PostInstallUpgrade script

--- Install/Scripts/PostInstallUpgrade.ps1
+++ Install/Scripts/PostInstallUpgrade.ps1
@@ -7,9 +7,33 @@ if ($null -ne $LastInstall) {
         Write-Host "[INFO] In version v1.2.7 we added 'Prosjekttidslinje' to the top navigation. Adding this navigation item now as part of the upgrade" 
         Add-PnPNavigationNode -Location TopNavigationBar -Title "Prosjekttidslinje" -Url "$($Uri.LocalPath)/SitePages/Prosjekttidslinje.aspx"
     }
+    if ($PreviousVersion -lt "1.5.5") {
+        Write-Host "[INFO] In version v1.5.5 we added Project timeline configuration and reworked the TimelineContent list. Merging data now as part of the upgrade"
+
+        $Items = Get-PnPListItem -List "Tidslinjeinnhold"
+        foreach ($Item in $Items) {
+            $OldSiteId = $Item.FieldValues["SiteIdLookup"].LookupId
+            $OldType = $Item.FieldValues["TimelineType"]
+
+            $Item["GtSiteIdLookup"] = $OldSiteId
+
+            Switch ($OldType)
+            {
+                "Prosjekt" { $Item["GtTimelineTypeLookup"] = 1 }
+                "Fase" { $Item["GtTimelineTypeLookup"] = 2 }
+                "Delfase" { $Item["GtTimelineTypeLookup"] = 3 }
+                "Milepæl" { $Item["GtTimelineTypeLookup"] = 4 }
+            }
+
+            
+            $Item.Update()
+            Invoke-PnPQuery
+        }
+    }
     if ($PreviousVersion -lt "1.5.0") {
         Write-Host "[INFO] Applying PnP template [1.5.0] to [$Url]"
         Apply-PnPProvisioningTemplate "$BasePath\1.5.0.pnp" -ErrorAction Stop
         Write-Host "[SUCCESS] Successfully applied PnP template [1.5.0] to [$Url]" -ForegroundColor Green
     }
+    
 }
