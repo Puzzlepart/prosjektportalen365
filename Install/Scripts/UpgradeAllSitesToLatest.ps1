0e25297f293b8d2a6eab19e92e7ae4186ae698a8
tarjeieo <ormestoyl@gmail.com>
2022-05-13 00:32:23 +0200

    Handling upgrade of existing sites #610

--- Install/Scripts/UpgradeAllSitesToLatest.ps1
+++ Install/Scripts/UpgradeAllSitesToLatest.ps1
@@ -14,7 +14,7 @@ function EnsureProjectTimelinePage($Url) {
         Write-Host "`t`tCannot connect to site. Do you have access?" -ForegroundColor Red
     }
     else {
-        $existingNode = $existingNodes | Where-Object { $_.Title -eq "Prosjekttidslinje" } -ErrorAction SilentlyContinue
+        $existingNode = $existingNodes | Where-Object { $_.Title -eq "Prosjekttidslinje" -or $_.Title -eq "Programtidslinje" } -ErrorAction SilentlyContinue
         if ($null -eq $existingNode) {
             Write-Host "`t`tAdding project timeline page"
             $page = Add-PnPClientSidePage -Name "Prosjekttidslinje.aspx" -PromoteAs None -LayoutType SingleWebPartAppPage -CommentsEnabled:$false -Publish
@@ -25,32 +25,85 @@ function EnsureProjectTimelinePage($Url) {
             $node = Add-PnPNavigationNode -Location QuickLaunch -Title "Prosjekttidslinje" -Url "SitePages/Prosjekttidslinje.aspx"
         }
         else {        
-            Write-Host "`t`tThe project already has the project timeline page. " -ForegroundColor Yellow
+            Write-Host "`t`tThe site already has the project timeline page" -ForegroundColor Green
         }
 
     }
 }
 
+function EnsureResourceLoadIsSiteColumn($Url) {
+    Connect-PnPOnline -Url $Url -UseWebLogin
+
+    $ResourceAllocation = Get-PnPList -Identity "Ressursallokering" -ErrorAction SilentlyContinue
+    if ($null -ne $ResourceAllocation) {
+        $ResourceLoadSiteColumn = Get-PnPField -Identity "GtResourceLoad"
+        $ResourceLoadListColumn = Get-PnPField -Identity "GtResourceLoad" -List $ResourceAllocation
+        if ($null -ne $ResourceLoadSiteColumn) {
+            Write-Host "`t`tReplacing GtResourceLoad field"
+            $PreviousValues = Get-PnPListItem -List $ResourceAllocation -Fields "ID","GtResourceLoad" | ForEach-Object {
+                @{Id = $_.Id; GtResourceLoad = $_.FieldValues["GtResourceLoad"]}
+            }
+            
+            if ($PreviousValues.length -gt 0) {
+                Write-Host "`t`t`t" (ConvertTo-Json $PreviousValues -Compress)
+            }
+  
+            $ResourceAllocationContentType = Get-PnPContentType -Identity "Ressursallokering" -List "Ressursallokering" -ErrorAction SilentlyContinue
+            if ($null -ne $ResourceAllocationContentType) {
+                Write-Host "`t`t`tRemoving old field"
+                $RemovedColumn = Remove-PnPField -Identity $ResourceLoadListColumn -List $ResourceAllocation -Force
+
+                
+                Write-Host "`t`t`tAdding the site field"
+                $FieldLink = New-Object Microsoft.SharePoint.Client.FieldLinkCreationInformation
+                $FieldLink.Field = $ResourceLoadSiteColumn
+                $Output = $ResourceAllocationContentType.FieldLinks.Add($FieldLink)
+                $ResourceAllocationContentType.Update($false)
+                $ResourceAllocationContentType.Context.ExecuteQuery()
+
+                Write-Host "`t`t`tAdding the site field to default view"
+                $DefaultView = Get-PnPView -List $ResourceAllocation -Identity "Alle elementer" -ErrorAction SilentlyContinue
+                if ($null -ne $DefaultView) {
+                    $DefaultView.ViewFields.Add("GtResourceLoad")
+                    $DefaultView.Update()
+                    $DefaultView.Context.ExecuteQuery()
+                }
+                
+                Write-Host "`t`t`tRestoring previous values"
+                $PreviousValues | ForEach-Object {
+                    $NewValue = Set-PnPListItem -List $ResourceAllocation -Identity $_.Id -Values @{"GtResourceLoad" = $_.GtResourceLoad} -SystemUpdate
+                } 
+
+                Write-Host "`t`t`tField swap completed" -ForegroundColor Green
+            }
+        } else {
+            Write-Host "`t`tThe site already has the correct GtResourceLoad field" -ForegroundColor Green
+        }
+    }
+}
+
 function UpgradeSite($Url) {
     EnsureProjectTimelinePage -Url $Url
+    EnsureResourceLoadIsSiteColumn -Url $Url
 }
 
 Write-Host "This script will update all existing sites in a Prosjektportalen installation. This requires you to have the SharePoint admin role"
 
+Set-PnPTraceLog -Off
+
 [System.Uri]$Uri = $PortfolioUrl
 $AdminSiteUrl = (@($Uri.Scheme, "://", $Uri.Authority) -join "").Replace(".sharepoint.com", "-admin.sharepoint.com")
 
-Connect-PnPOnline -Url $AdminSiteUrl -UseWebLogin
-
-$PPHubSite = Get-PnPHubSite -Identity $PortfolioUrl
-
-$ProjectsInHub = Get-PP365HubSiteChild -Identity $PPHubSite
+Connect-PnPOnline -Url $AdminSiteUrl -UseWebLogin -WarningAction Ignore
 
 # Get current logged in user
 $ctx = Get-PnPContext
 $ctx.Load($ctx.Web.CurrentUser)
 $ctx.ExecuteQuery()
 $UserName = $ctx.Web.CurrentUser.LoginName
+
+$PPHubSite = Get-PnPHubSite -Identity $PortfolioUrl
+$ProjectsInHub = Get-PP365HubSiteChild -Identity $PPHubSite
 
 Write-Host "The following sites were found to be part of the Project Portal hub:"
 $ProjectsInHub | ForEach-Object { Write-Host "`t$_" }
