2553033eb5f18d5d77680d4bf9a93f681bb04e76
Ole Kristian Mørch-Storstein <ok@okms.me>
2022-08-19 13:09:51 +0200

    Adding script for Planner Plans

---
+++ Install/Scripts/Ensure-PlannerPlans.ps1
@@ -0,0 +1,72 @@
+[CmdletBinding()]
+param (
+    [Parameter( Mandatory = $true, Position = 0 )]
+    [string]
+    $PortfolioUrl,
+    [Parameter( Mandatory = $true, Position = 1 )]
+    [string]
+    $TenantAdminUrl,
+    [Parameter( Mandatory = $true, Position = 2 )]
+    [string]
+    $CurrentUser,
+    [Parameter( Mandatory = $false, Position = 3 )]
+    [switch]
+    $GrantPermissions
+)
+
+$groupsWhereAdded = [System.Collections.ArrayList]@()
+function GrantPermissions ($Url) {
+    Write-Host "`tGranting owner permissions to site collection $Url"
+    Set-PnPTenantSite -Url $Url -Owners $CurrentUser
+    $groupId = (Get-PnPSite -Includes GroupId -ErrorAction Ignore).GroupId.toString()
+    Write-Host "`tGranting owner permissions to group $groupId"
+    Set-PnPMicrosoft365Group -Identity $groupId -Owners $CurrentUser 
+    $groupsWhereAdded.Add($groupId) | out-null
+}
+
+function CleanupPermissions {
+    $groupsWhereAdded | ForEach-Object { Write-Host "`tRemoving $CurrentUser from $_"; Remove-PnPMicrosoft365GroupOwner -Identity $_ -Users $CurrentUser }
+}
+
+
+if(Get-Module | Where-Object{$_.Name -like "*SharePointPnPPowerShellOnline*"}){
+    Remove-Module -Name SharePointPnPPowerShellOnline -Force
+}
+
+if (-not (Test-Path "..\PnP.PowerShell") ) {
+   Expand-Archive "..\PnP.PowerShell.zip" -DestinationPath "..\"
+}
+
+Import-Module "..\PnP.PowerShell\1.11.0\PnP.PowerShell.psd1"
+
+Connect-PnPOnline -TenantAdminUrl $TenantAdminUrl -Url $PortfolioUrl -Interactive
+
+$children = Get-PnPHubSiteChild
+
+if($GrantPermissions) {
+    Write-Host "Granting permissions to $CurrentUser" -ForegroundColor Green
+    $children | ForEach-Object { GrantPermissions $_ }
+    Write-Host "[x] Done" -ForegroundColor Green
+}
+
+$children | ForEach-Object {
+    $childSiteUrl = $_
+
+    Connect-PnPOnline -Url $childSiteUrl  -TenantAdminUrl $TenantAdminUrl -Interactive;
+    $groupId = (Get-PnPSite -Includes GroupId -ErrorAction Ignore).GroupId.toString()
+  
+    $plannerPlan = Get-PnPPlannerPlan -Group $groupId
+    
+    if ($null -eq $plannerPlan) { 
+         Write-Host "[ ] Gruppen $childSiteUrl ($groupId) har IKKE plannerPlann" -ForegroundColor Yellow
+         New-PnPPlannerPlan -Group $groupId -Title (Get-PnPWeb).Title | out-null
+         Write-Host "`t[x] Planner plan opprettet" -ForegroundColor Green
+    } else {
+        Write-Host "[x] Gruppen $childSiteUrl ($groupId) har plannerPlan" -ForegroundColor Green
+    }
+}
+
+# if($cleanUpOnComplete) {
+#     Write-Host "Cleaning up permissions"
+#     CleanupPermissions
+# }
