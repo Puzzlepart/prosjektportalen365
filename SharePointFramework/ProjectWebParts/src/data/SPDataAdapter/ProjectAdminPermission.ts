217a7610a8794d67304271693b43d35069ee266b
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-05 13:14:16 +0200

    String enum for project admin permissions

---
+++ SharePointFramework/ProjectWebParts/src/data/SPDataAdapter/ProjectAdminPermission.ts
@@ -0,0 +1,10 @@
+/**
+ * String enum for project admin permissions.
+ * 
+ * @see Templates/Portfolio/Objects/Lists/Prosjektadministrasjonstilganger.xml
+ */
+export enum ProjectAdminPermission {
+    EditProjectProperties = 'edc568a8-9cfc-4547-9af2-d9d3aeb5aa2a',
+    ChangePhase = '75a08ae0-d69a-41b2-adf4-ae233c6bff9f',
+    ProjectStatusAdmin = 'f6b875ae-fdb4-4ceb-bc75-ed853c2a2b0e'
+}
