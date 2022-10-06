4cf2078d054d84296e12165b230dde62e7dad09b
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-06 13:00:18 +0200

    pp365-projectwebparts@1.6.1-725.4

--- SharePointFramework/ProjectWebParts/src/components/ProjectInformationPanel/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformationPanel/index.tsx
@@ -1,17 +1,24 @@
 import { Panel, PanelType } from 'office-ui-fabric-react'
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState } from 'react'
 import { ProjectInformation } from '../ProjectInformation'
 import { IProjectInformationPanelProps } from './types'
 
 export const ProjectInformationPanel: FunctionComponent<IProjectInformationPanelProps> = (
   props
 ) => {
+  const [showPanel, setShowPanel] = useState(false)
   return (
-    <Panel
-      type={PanelType.medium}
-      {...props.panelProps}>
-      <ProjectInformation {...props} />
-    </Panel>
+    <>
+      {props.onRenderToggleElement(() => setShowPanel(!showPanel))}
+      <Panel
+        isOpen={showPanel}
+        type={PanelType.medium}
+        onDismiss={() => setShowPanel(false)}
+        isLightDismiss={true}
+        {...props.panelProps}>
+        <ProjectInformation {...props} />
+      </Panel>
+    </>
   )
 }
 
--- SharePointFramework/ProjectWebParts/src/components/ProjectInformationPanel/types.ts
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformationPanel/types.ts
@@ -3,6 +3,11 @@ import { IProjectInformationProps } from '../ProjectInformation'
 
 export interface IProjectInformationPanelProps extends IProjectInformationProps {
   /**
+   * On render function for the element that should toggle the panel visibility
+   */
+  onRenderToggleElement: (onToggle: () => void) => JSX.Element
+
+  /**
    * Props for the `<Panel />`
    */
   panelProps?: IPanelProps
