fa71bc6c2676a6e26c81ee3ce18dd6116dff5f48
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-07 14:23:49 +0200

    ParentProjectsList

--- SharePointFramework/ProjectWebParts/src/components/ProjectInformation/ParentProjectsList/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformation/ParentProjectsList/index.tsx
@@ -1,23 +1,27 @@
 import { Icon, Link } from 'office-ui-fabric-react'
 import strings from 'ProjectWebPartsStrings'
 import React, { FunctionComponent, useContext } from 'react'
+import { isEmpty } from 'underscore'
 import { ProjectInformationContext } from '../context'
-import styles from './ParentProjects.module.scss'
+import styles from './ParentProjectsList.module.scss'
 
-export const ParentProjects: FunctionComponent = () => {
+export const ParentProjectsList: FunctionComponent = () => {
   const context = useContext(ProjectInformationContext)
-  const projects = (context.state?.data?.parentProjects || [])
+  const projects = context.state?.data?.parentProjects || []
+  if (context.props.hideParentProjects || isEmpty(projects)) return null
   return (
-    <div
-      hidden={projects.length === 0}
-      className={styles.root}>
+    <div className={styles.root}>
       <div className={styles.header}>
-        <span role='heading' aria-level={3}>{strings.ParentProjectsHeaderText}</span>
+        <span role='heading' aria-level={3}>
+          {strings.ParentProjectsHeaderText}
+        </span>
       </div>
       {projects.map((p, index) => (
         <div key={index} className={styles.projectItem}>
           <Icon iconName={p.iconName} className={styles.icon} />
-          <Link href={p.url} className={styles.link}>{p.title}</Link>
+          <Link href={p.url} className={styles.link}>
+            {p.title}
+          </Link>
         </div>
       ))}
     </div>
