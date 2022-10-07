7f47c18547c4f85ee68d8bad304c77dc78491f8b
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-07 16:52:03 +0200

    Commands

--- SharePointFramework/ProgramWebParts/src/components/ProgramAdministration/Commands/index.tsx
+++ SharePointFramework/ProgramWebParts/src/components/ProgramAdministration/Commands/index.tsx
@@ -1,16 +1,11 @@
-import React, { FunctionComponent } from 'react'
-import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar'
-import { useStore } from '../store'
-import * as strings from 'ProgramWebPartsStrings'
 import { Spinner } from 'office-ui-fabric-react'
-import { ICommandsProps } from './types'
+import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar'
+import * as strings from 'ProgramWebPartsStrings'
+import React, { FunctionComponent, useContext } from 'react'
+import { ProgramAdministrationContext } from '../context'
 
-export const Commands: FunctionComponent<ICommandsProps> = ({ _sp, isSiteAdmin }) => {
-  const toggleProjectDialog = useStore((state) => state.toggleProjectDialog)
-  const deleteChildProjects = useStore((state) => state.deleteChildProjects)
-  const selectedProjectsToDelete = useStore((state) => state.selectedProjectsToDelete)
-  const isLoading = useStore((state) => state.isLoading)
-
+export const Commands: FunctionComponent = () => {
+  const context = useContext(ProgramAdministrationContext)
   const getLoadingBar = () => {
     const commandBarButtonAs = () => (
       <div
@@ -23,7 +18,7 @@ export const Commands: FunctionComponent<ICommandsProps> = ({ _sp, isSiteAdmin }
         <Spinner />
       </div>
     )
-    if (isLoading) {
+    if (context.state.loading.ProgramAdministration) {
       return commandBarButtonAs
     }
   }
@@ -34,17 +29,17 @@ export const Commands: FunctionComponent<ICommandsProps> = ({ _sp, isSiteAdmin }
       text: strings.ProgramAddProjectButtonText,
       iconProps: { iconName: 'Add' },
       buttonStyles: { root: { border: 'none' } },
-      onClick: () => toggleProjectDialog(),
-      disabled: !isSiteAdmin
+      // onClick: () => toggleProjectDialog(),
+      // disabled: !isSiteAdmin
     },
     {
       key: 'delete',
       text: strings.ProgramRemoveChildButtonText,
       iconProps: { iconName: 'Delete' },
       buttonStyles: { root: { border: 'none' } },
-      disabled: selectedProjectsToDelete?.length > 0 ? false : true || !isSiteAdmin,
+      // disabled: selectedProjectsToDelete?.length > 0 ? false : true || !isSiteAdmin,
       onClick: (): any => {
-        deleteChildProjects(selectedProjectsToDelete, _sp)
+        // deleteChildProjects(selectedProjectsToDelete, _sp)
       },
       commandBarButtonAs: getLoadingBar()
     }
