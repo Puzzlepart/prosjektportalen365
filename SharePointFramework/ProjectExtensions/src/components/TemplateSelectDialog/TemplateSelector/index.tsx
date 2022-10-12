2249c8cebeaeea5d75eebe770346245ede174535
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-12 13:54:00 +0200

    Fix for #811 (#812)

--- SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/TemplateSelector/index.tsx
+++ SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/TemplateSelector/index.tsx
@@ -1,5 +1,6 @@
-import { ChoiceGroup, IChoiceGroupOptionStyles } from 'office-ui-fabric-react'
+import { ChoiceGroup, IChoiceGroupOption, IChoiceGroupOptionStyles } from 'office-ui-fabric-react'
 import React, { FunctionComponent } from 'react'
+import { find } from 'underscore'
 import { ProjectTemplate } from '../../../models'
 import { ProjectTemplateTooltip } from './ProjectTemplateTooltip'
 import styles from './TemplateSelector.module.scss'
@@ -11,11 +12,23 @@ export const TemplateSelector: FunctionComponent<ITemplateSelectorProps> = ({
   onChange
 }) => {
   const choiceGroupOptionStyles: IChoiceGroupOptionStyles = {
-    root: { width: 180, height: 140 },
-    choiceFieldWrapper: { width: 180, height: 140 },
-    labelWrapper: { maxWidth: '180px !important', width: 180 },
-    field: { padding: 0, width: 180, height: 140 }
+    root: { width: 180, height: 120 },
+    choiceFieldWrapper: { width: 180, height: 120 },
+    labelWrapper: { maxWidth: '180px !important', width: 120 },
+    field: { padding: 0, width: 180, height: 120 }
   }
+
+  const options: IChoiceGroupOption[] = templates.map((t) => ({
+    ...t,
+    styles: choiceGroupOptionStyles,
+    onRenderLabel: (opt: ProjectTemplate) => {
+      return (
+        <ProjectTemplateTooltip template={opt}>
+          <span>{opt.text}</span>
+        </ProjectTemplateTooltip>
+      )
+    }
+  }))
 
   return (
     <div className={styles.root}>
@@ -23,18 +36,11 @@ export const TemplateSelector: FunctionComponent<ITemplateSelectorProps> = ({
         className={styles.choiceGroup}
         disabled={templates?.length <= 1}
         defaultSelectedKey={selectedTemplate.key}
-        onChanged={(opt: ProjectTemplate) => onChange(opt)}
-        options={templates.map((t) => ({
-          ...t,
-          styles: choiceGroupOptionStyles,
-          onRenderLabel: (opt: ProjectTemplate) => {
-            return (
-              <ProjectTemplateTooltip template={opt}>
-                <span>{opt.text}</span>
-              </ProjectTemplateTooltip>
-            )
-          }
-        }))}
+        onChange={(_evt, opt: IChoiceGroupOption) => {
+          const template = find(templates, (t) => t.id === opt.id)
+          onChange(template)
+        }}
+        options={options}
       />
     </div>
   )
