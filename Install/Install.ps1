bdaca7ff7ad7f6b0e08473085d2816ae3dfdd68b
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-24 17:29:20 +0200

    Install.ps1 logging

--- Install/Install.ps1
+++ Install/Install.ps1
@@ -59,6 +59,7 @@ $LanguageCode = $LanguageCodes[$Language]
 
 $ErrorActionPreference = "Stop"
 $sw = [Diagnostics.Stopwatch]::StartNew()
+$global:sw_action = $null
 $InstallStartTime = (Get-Date -Format o)
 if ($Upgrade.IsPresent) {
     Write-Host "[INFO] Upgrading Prosjektportalen 365] to v{VERSION_PLACEHOLDER}"
@@ -101,6 +102,17 @@ function Connect-SharePoint {
         Write-Host "[INFO] Failed to connect to $($Url): $($_.Exception.Message)"
         throw $_.Exception.Message
     }
+}
+
+function StartAction($Action) {
+    $global:sw_action = [Diagnostics.Stopwatch]::StartNew()
+    Write-Host "[INFO] $Action...  " -NoNewline
+}
+
+function EndAction() {
+    $global:sw_action.Stop()
+    $ElapsedSeconds = [math]::Round(($global:sw_action.ElapsedMilliseconds)/1000, 2)
+    Write-Host "Completed in $($ElapsedSeconds)s" -ForegroundColor Green
 }
 
 function LoadBundle() {
@@ -153,13 +165,14 @@ if (-not $SkipSiteCreation.IsPresent -and -not $Upgrade.IsPresent) {
         Connect-SharePoint -Url $AdminSiteUrl -ErrorAction Stop
         $PortfolioSite = Get-PnPTenantSite -Url $Url -ErrorAction SilentlyContinue
         if ($null -eq $PortfolioSite) {
-            Write-Host "[INFO] Creating portfolio site at $Url"
+            StartAction("Creating portfolio site at $Url")
             New-PnPSite -Type TeamSite -Title $Title -Alias $Alias -IsPublic:$true -ErrorAction Stop -Lcid $LanguageId >$null 2>&1
-            Write-Host "[SUCCESS] Portfolio site created at $Url" -ForegroundColor Green
+            EndAction
         }
-        Register-PnPHubSite -Site $Url -ErrorAction SilentlyContinue
-        Write-Host "[SUCCESS] Portfolio site $Url promoted to hub site" -ForegroundColor Green
+        StartAction("Promoting $Url to hubsite")
+        Register-PnPHubSite -Site $Url -ErrorAction SilentlyContinue >$null 2>&1
         Disconnect-PnPOnline
+        EndAction
     }
     Catch {
         Write-Host "[ERROR] Failed to create site and promote to hub site: $($_.Exception.Message)" -ForegroundColor Red
@@ -171,7 +184,7 @@ if (-not $SkipSiteCreation.IsPresent -and -not $Upgrade.IsPresent) {
 #region Setting permissons
 if (-not $Upgrade.IsPresent) {
     Try {
-        Write-Host "[INFO] Setting permissions for associated member group"
+        StartAction("Setting permissions for associated member group")
         Connect-SharePoint -Url $Url -ErrorAction Stop
         # Must use english names to avoid errors, even on non 1033 sites
         # Where-Object doesn't work directly on Get-PnPRoleDefinition, so need to clone it first (https://github.com/Puzzlepart/prosjektportalen365/issues/35)
@@ -180,7 +193,7 @@ if (-not $Upgrade.IsPresent) {
         Set-PnPGroupPermissions -Identity (Get-PnPGroup -AssociatedMemberGroup) -RemoveRole ($RoleDefinitions | Where-Object { $_.RoleTypeKind -eq "Editor" }) -ErrorAction SilentlyContinue
         Set-PnPGroupPermissions -Identity (Get-PnPGroup -AssociatedMemberGroup) -AddRole ($RoleDefinitions | Where-Object { $_.RoleTypeKind -eq "Reader" }) -ErrorAction SilentlyContinue    
         Disconnect-PnPOnline
-        Write-Host "[SUCCESS] Successfully set permissions for associated member group" -ForegroundColor Green
+        EndAction
     }
     Catch {
         Write-Host "[ERROR] Failed to set permissions for associated member group: $($_.Exception.Message)" -ForegroundColor Red
@@ -197,7 +210,7 @@ if (-not $SkipSiteDesign.IsPresent) {
     $SiteScriptIds = @()
 
     Try {
-        Write-Host "[INFO] Creating/updating site scripts"        
+        StartAction("Creating/updating site scripts")  
         Connect-SharePoint -Url $AdminSiteUrl -ErrorAction Stop
         $SiteScripts = Get-PnPSiteScript
         $SiteScriptSrc = Get-ChildItem "$PSScriptRoot/SiteScripts/*.txt"
@@ -214,7 +227,7 @@ if (-not $SkipSiteDesign.IsPresent) {
             $SiteScriptIds += $SiteScript.Id.Guid
         }
         Disconnect-PnPOnline
-        Write-Host "[SUCCESS] Successfully created/updated site scripts" -ForegroundColor Green
+        EndAction
     }
     Catch {
         Write-Host "[ERROR] Failed to create/update site scripts: $($_.Exception.Message)" -ForegroundColor Red
@@ -222,7 +235,7 @@ if (-not $SkipSiteDesign.IsPresent) {
     }
 
     Try {
-        Write-Host "[INFO] Creating/updating site design $SiteDesignName"   
+        StartAction("Creating/updating site design $SiteDesignName")
         Connect-SharePoint -Url $AdminSiteUrl -ErrorAction Stop
     
         $SiteDesign = Get-PnPSiteDesign -Identity $SiteDesignName 
@@ -242,7 +255,7 @@ if (-not $SkipSiteDesign.IsPresent) {
         }
 
         Disconnect-PnPOnline
-        Write-Host "[SUCCESS] Successfully created/updated site design $SiteDesignName" -ForegroundColor Green
+        EndAction
     }
     Catch {
         Write-Host "[ERROR] Failed to create/update site design: $($_.Exception.Message)" -ForegroundColor Red
@@ -250,24 +263,25 @@ if (-not $SkipSiteDesign.IsPresent) {
     }
 }
 if (-not $SkipDefaultSiteDesignAssociation.IsPresent) {
+    StartAction("Setting default site design for hub $Url to $SiteDesignName")
     Connect-SharePoint -Url $AdminSiteUrl -ErrorAction Stop
     $SiteDesign = Get-PnPSiteDesign -Identity $SiteDesignName 
-    Write-Host "[INFO] Setting default site design for hub $Url to $SiteDesignName"
     Set-PnPHubSite -Identity $Url -SiteDesignId $SiteDesign.Id.Guid
     Disconnect-PnPOnline
+    EndAction
 }
 #endregion
 
 #region Pre install
 if ($Upgrade.IsPresent) {
-    Write-Host "[INFO] Running pre-install upgrade steps" 
+    StartAction("Running pre-install upgrade steps")
     try {
         Connect-SharePoint -Url $Url -ErrorAction Stop
         ."$PSScriptRoot\Scripts\PreInstallUpgrade.ps1"
         Disconnect-PnPOnline
-        Write-Host "[SUCCESS] Successfully ran pre-install upgrade steps" -ForegroundColor Green
     }
     Catch {}
+    EndAction
 }
 #endregion
 
@@ -286,17 +300,14 @@ if (-not $SkipAppPackages.IsPresent) {
         exit 0 
     }
     Try {
-        Write-Host "[INFO] Installing SharePoint Framework app packages to $TenantAppCatalogUrl"
+        StartAction("[INFO] Installing SharePoint Framework app packages to $TenantAppCatalogUrl")
         foreach ($AppPkg in (Get-ChildItem "$PSScriptRoot\Apps" -ErrorAction SilentlyContinue)) {
-            Write-Host "[INFO] Installing $($AppPkg.BaseName)..."  -NoNewline
             Add-PnPApp -Path $AppPkg.FullName -Scope Tenant -Publish -Overwrite -SkipFeatureDeployment -ErrorAction Stop >$null 2>&1
-            Write-Host " DONE" -ForegroundColor Green
         }
         Disconnect-PnPOnline
-        Write-Host "[SUCCESS] SharePoint Framework app packages successfully installed to $TenantAppCatalogUrl" -ForegroundColor Green
+        EndAction
     }
     Catch {
-        Write-Host "Error" -ForegroundColor Red
         Write-Host "[ERROR] Failed to install app packages to $($TenantAppCatalogUrl): $($_.Exception.Message)" -ForegroundColor Red
         exit 0
     }
@@ -330,31 +341,31 @@ if (-not $SkipTemplate.IsPresent) {
             throw "Wrong connection identified - you are not connected to the correct site"
         }
         if (-not $SkipTaxonomy.IsPresent -and -not $Upgrade.IsPresent) {
-            Write-Host "[INFO] Applying PnP template Taxonomy to $Url"
+            StartAction("Applying PnP template Taxonomy to $Url")
             Invoke-PnPSiteTemplate "$TemplatesBasePath/Taxonomy.pnp" -ErrorAction Stop
-            Write-Host "[SUCCESS] Successfully applied PnP template Taxonomy to $Url" -ForegroundColor Green
+            EndAction
         }
 
         if ($Upgrade.IsPresent) {
-            Write-Host "[INFO] Applying PnP template Portfolio to $Url"
+            StartAction("Applying PnP template Portfolio to $Url")
             Invoke-PnPSiteTemplate "$TemplatesBasePath/Portfolio.pnp" -ExcludeHandlers Navigation, SupportedUILanguages -ErrorAction Stop
-            Write-Host "[SUCCESS] Successfully applied PnP template Portfolio to $Url" -ForegroundColor Green
+            EndAction
 
-            Write-Host "[INFO] Applying PnP content template with Handlers:Files to $Url"
+            StartAction("Applying PnP content template to $Url")
             Invoke-PnPSiteTemplate "$TemplatesBasePath/Portfolio_content.$LanguageCode.pnp" -Handlers Files -ErrorAction Stop
-            Write-Host "[SUCCESS] Successfully applied PnP content template to $Url" -ForegroundColor Green
+            EndAction
         }
         else {
-            Write-Host "[INFO] Applying PnP template Portfolio to $Url"
+            StartAction("Applying PnP template Portfolio to $Url")
             $Instance = Read-PnPSiteTemplate "$TemplatesBasePath/Portfolio.pnp"
             $Instance.SupportedUILanguages[0].LCID = $LanguageId
             Invoke-PnPSiteTemplate -InputInstance $Instance -Handlers SupportedUILanguages
             Invoke-PnPSiteTemplate "$TemplatesBasePath/Portfolio.pnp" -ExcludeHandlers SupportedUILanguages -ErrorAction Stop
-            Write-Host "[SUCCESS] Successfully applied PnP template Portfolio to $Url" -ForegroundColor Green
+            EndAction
 
-            Write-Host "[INFO] Applying PnP content template to $Url"
+            StartAction("Applying PnP content template to $Url")
             Invoke-PnPSiteTemplate "$TemplatesBasePath/Portfolio_content.$LanguageCode.pnp" -ErrorAction Stop
-            Write-Host "[SUCCESS] Successfully applied PnP content template to $Url" -ForegroundColor Green
+            EndAction
         }
         
         Disconnect-PnPOnline
@@ -373,10 +384,10 @@ if (-not $SkipTemplate.IsPresent) {
 #region QuickLaunch 
 Try {
     Connect-SharePoint -Url $Url -ErrorAction Stop
-    Write-Host "[INFO] Clearing QuickLaunch"    
+    StartAction("Clearing QuickLaunch")
     Get-PnPNavigationNode -Location QuickLaunch | Remove-PnPNavigationNode -Force
     Disconnect-PnPOnline
-    Write-Host "[SUCCESS] Successfully cleared QuickLaunch" -ForegroundColor Green
+    EndAction
 }
 Catch {
     Write-Host "[WARNING] Failed to clear QuickLaunch: $($_.Exception.Message)" -ForegroundColor Yellow
@@ -387,10 +398,10 @@ Catch {
 if (-not $SkipSearchConfiguration.IsPresent) {
     Try {
         Connect-SharePoint -Url $AdminSiteUrl -ErrorAction Stop
-        Write-Host "[INFO] Importing Search Configuration"    
+        StartAction("Importing Search Configuration")
         Set-PnPSearchConfiguration -Scope Subscription -Path "$PSScriptRoot/SearchConfiguration.xml" -ErrorAction SilentlyContinue   
         Disconnect-PnPOnline
-        Write-Host "[SUCCESS] Successfully imported Search Configuration" -ForegroundColor Green
+        EndAction
     }
     Catch {
         Write-Host "[WARNING] Failed to import Search Configuration: $($_.Exception.Message)" -ForegroundColor Yellow
