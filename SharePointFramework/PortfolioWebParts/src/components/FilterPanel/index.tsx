9ddd9e0deb51d98fd9ff244e116bb7136f559890
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-25 10:58:43 +0200

    Fix for FilterPanel in PortfolioWebParts

--- SharePointFramework/PortfolioWebParts/src/components/FilterPanel/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/FilterPanel/index.tsx
@@ -1,24 +1,23 @@
-import { Customizer, Panel, PanelType } from '@fluentui/react'
+import { Panel, PanelType } from '@fluentui/react'
 import React, { FunctionComponent } from 'react'
 import { Filter } from './Filter'
 import { IFilterPanelProps } from './types'
 
 export const FilterPanel: FunctionComponent<IFilterPanelProps> = (props) => {
   return (
-    <Customizer scopedSettings={{ Layer: { hostId: props.layerHostId } }}>
-      <Panel {...props} type={PanelType.smallFixedFar}>
-        <div>
-          {props.filters
-            .filter((f) => f.items.length > 1)
-            .map((f, idx) => (
-              <Filter {...f} key={idx} onFilterChange={props.onFilterChange} />
-            ))}
-        </div>
-      </Panel>
-    </Customizer>
+    <Panel {...props} type={PanelType.smallFixedFar}>
+      <div>
+        {props.filters
+          .filter((f) => f.items.length > 1)
+          .map((f, idx) => (
+            <Filter {...f} key={idx} onFilterChange={props.onFilterChange} />
+          ))}
+      </div>
+    </Panel>
   )
 }
 
-export * from './FilterItem/types'
 export * from './Filter/types'
+export * from './FilterItem/types'
 export * from './types'
+
