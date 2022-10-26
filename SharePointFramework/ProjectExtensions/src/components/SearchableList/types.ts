4973876aef797673f2b58b0765f5e7dcc95aea5d
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-26 18:44:12 +0200

    Removed SearchableList as it didn't work to reuse

--- SharePointFramework/ProjectExtensions/src/components/SearchableList/types.ts
+++
@@ -1,7 +0,0 @@
-import { IDetailsListProps, ISearchBoxProps } from '@fluentui/react'
-
-export interface ISearchableListProps extends Pick<IDetailsListProps, 'columns' | 'items'> {
-  selectedKeys?: any
-  onSelectionChanged(selectedItems: any[]): void
-  searchBox?: ISearchBoxProps
-}
