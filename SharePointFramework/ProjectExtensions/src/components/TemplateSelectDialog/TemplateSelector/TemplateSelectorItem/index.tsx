dc884be18c003d3ec218103ea1f798e306914b56
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-26 12:20:11 +0200

    Adjustments to TemplateSelectorItem

--- SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/TemplateSelector/TemplateSelectorItem/index.tsx
+++ SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/TemplateSelector/TemplateSelectorItem/index.tsx
@@ -3,18 +3,18 @@ import React, { FunctionComponent } from 'react'
 import { ITemplateSelectorItemProps } from './types'
 import styles from './TemplateSelectorItem.module.scss'
 
-export const TemplateSelectorItem: FunctionComponent<ITemplateSelectorItemProps> = (props) => {
+export const TemplateSelectorItem: FunctionComponent<ITemplateSelectorItemProps> = ({
+  template,
+  isHighlighted
+}) => {
   return (
-    <div
-      className={[styles.root, props.isHighlighted ?? styles.isHighlighted]
-        .filter(Boolean)
-        .join(' ')}>
+    <div className={[styles.root, isHighlighted && styles.isHighlighted].filter(Boolean).join(' ')}>
       <div className={styles.icon}>
-        <Icon iconName={props.template.iconName} />
+        <Icon iconName={template.iconName} />
       </div>
       <div className={styles.body}>
-        <div className={styles.text}>{props.template.text}</div>
-        <div className={styles.subText}>{props.template.subText}</div>
+        <div className={styles.text}>{template.text}</div>
+        <div className={styles.subText}>{template.subText}</div>
       </div>
     </div>
   )
--- SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/TemplateSelector/index.tsx
+++ SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/TemplateSelector/index.tsx
@@ -1,14 +1,13 @@
-import { SearchBox } from '@fluentui/react'
+import { ISearchBoxProps, SearchBox } from '@fluentui/react'
 import { ProjectTemplate } from 'models'
 import React, { FunctionComponent, useState } from 'react'
 import Autocomplete from 'react-autocomplete'
-import { omit } from 'underscore'
 import styles from './TemplateSelector.module.scss'
 import { TemplateSelectorItem } from './TemplateSelectorItem'
 import { ITemplateSelectorProps } from './types'
 
 export const TemplateSelector: FunctionComponent<ITemplateSelectorProps> = (props) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState(props.selectedTemplate?.text)
   return (
     <div className={styles.root}>
       <div className={styles.container}>
@@ -19,11 +18,22 @@ export const TemplateSelector: FunctionComponent<ITemplateSelectorProps> = (prop
             template.text.toLowerCase().indexOf(value.toLowerCase()) > -1
           }
           renderItem={(template: ProjectTemplate, isHighlighted) => (
-            <TemplateSelectorItem template={template} isHighlighted={isHighlighted} />
+            <div key={template.id}>
+              <TemplateSelectorItem template={template} isHighlighted={isHighlighted} />
+            </div>
           )}
           inputProps={{ className: styles.searchBox }}
           renderInput={(inputProps) => (
-            <SearchBox {...omit(inputProps, 'value')} placeholder={inputProps.value} />
+            <SearchBox
+              {...(inputProps as ISearchBoxProps)}
+              onClear={(event) => {
+                // eslint-disable-next-line no-console
+                console.log(event.stopPropagation)
+                // eslint-disable-next-line no-console
+                console.log(event.preventDefault)
+                setValue('')
+              }}
+            />
           )}
           value={value}
           onChange={(_, value_) => setValue(value_)}
