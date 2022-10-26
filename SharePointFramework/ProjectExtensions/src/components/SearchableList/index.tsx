947b81185ae1fc203cd048c6468bf6c8409dd249
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-26 17:25:49 +0200

    SearchableList component adjustments

--- SharePointFramework/ProjectExtensions/src/components/SearchableList/index.tsx
+++ SharePointFramework/ProjectExtensions/src/components/SearchableList/index.tsx
@@ -1,7 +1,7 @@
-import { DetailsList, ScrollablePane, SearchBox, SelectionMode } from '@fluentui/react'
+import { DetailsList, SearchBox, SelectionMode } from '@fluentui/react'
 import React, { FunctionComponent } from 'react'
-import { ISearchableListProps } from './types'
 import styles from './SearchableList.module.scss'
+import { ISearchableListProps } from './types'
 import { useSearchableList } from './useSearchableList'
 
 export const SearchableList: FunctionComponent<ISearchableListProps> = (props) => {
@@ -9,24 +9,33 @@ export const SearchableList: FunctionComponent<ISearchableListProps> = (props) =
 
   return (
     <div className={styles.root}>
-      <SearchBox
-        {...props.searchBox}
-        onChange={(_, newValue) => onSearch(newValue)}
-        onSearch={(newValue) => onSearch(newValue)}
+      <DetailsList
+        styles={{ root: { height: 450 } }}
+        setKey='set'
+        selection={selection}
+        selectionMode={SelectionMode.multiple}
+        selectionPreservedOnEmptyClick={true}
+        onRenderRow={onRenderRow}
+        onRenderDetailsHeader={(detailsHeaderProps, defaultRender) => (
+          <div>
+            <div style={{ marginTop: 10 }}>
+              <SearchBox
+                {...props.searchBox}
+                onChange={(_, newValue) => onSearch(newValue)}
+                onSearch={(newValue) => onSearch(newValue)}
+              />
+            </div>
+            {defaultRender(detailsHeaderProps)}
+          </div>
+        )}
+        items={props.items}
+        columns={props.columns}
       />
-      <div style={{ height: 300 }}>
-        <ScrollablePane>
-          <DetailsList
-            setKey='set'
-            selection={selection}
-            selectionMode={SelectionMode.multiple}
-            selectionPreservedOnEmptyClick={true}
-            onRenderRow={onRenderRow}
-            items={props.items}
-            columns={props.columns}
-          />
-        </ScrollablePane>
-      </div>
     </div>
   )
+}
+
+SearchableList.defaultProps = {
+  searchBox: {},
+  selectedKeys: []
 }
--- SharePointFramework/ProjectExtensions/src/components/SearchableList/types.ts
+++ SharePointFramework/ProjectExtensions/src/components/SearchableList/types.ts
@@ -1,7 +1,7 @@
 import { IDetailsListProps, ISearchBoxProps } from '@fluentui/react'
 
 export interface ISearchableListProps extends Pick<IDetailsListProps, 'columns' | 'items'> {
-  selectedKeys: any
+  selectedKeys?: any
   onSelectionChanged(selectedItems: any[]): void
-  searchBox: ISearchBoxProps
+  searchBox?: ISearchBoxProps
 }
