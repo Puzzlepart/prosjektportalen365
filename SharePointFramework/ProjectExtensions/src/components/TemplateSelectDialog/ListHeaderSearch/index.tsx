2173bb6f82b98b5e6368afc6857ef619b0d765b1
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-27 13:12:36 +0200

    Added component ListHeaderSearch

---
+++ SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/ListHeaderSearch/index.tsx
@@ -0,0 +1,32 @@
+import { SearchBox, SelectAllVisibility, Sticky, StickyPositionType } from '@fluentui/react'
+import React, { FC } from 'react'
+import styles from './ListHeaderSearch.module.scss'
+import { IListHeaderSearchProps } from './types'
+
+export const ListHeaderSearch: FC<IListHeaderSearchProps> = (props) => {
+  return (
+    <Sticky stickyPosition={StickyPositionType.Header}>
+      <div className={styles.searchBox} hidden={props.search?.hidden}>
+        <SearchBox
+          placeholder={props.search?.placeholder}
+          onChange={(_, newValue) => props.search?.onSearch(newValue)}
+          onSearch={(newValue) => props.search?.onSearch(newValue)}
+        />
+      </div>
+      {props.defaultRender({
+        ...props.detailsHeaderProps,
+        selectAllVisibility: props.selectAllVisibility
+      })}
+    </Sticky>
+  )
+}
+
+ListHeaderSearch.defaultProps = {
+  search: {
+    hidden: true,
+    placeholder: '',
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onSearch: () => {}
+  },
+  selectAllVisibility: SelectAllVisibility.hidden
+}
