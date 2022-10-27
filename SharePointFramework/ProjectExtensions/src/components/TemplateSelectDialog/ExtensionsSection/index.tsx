0896e8a436e18c77fe64c5b35a10d9242e745714
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-27 11:34:50 +0200

    ExtensionsSection using hook useSelectionList

--- SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/ExtensionsSection/index.tsx
+++ SharePointFramework/ProjectExtensions/src/components/TemplateSelectDialog/ExtensionsSection/index.tsx
@@ -1,5 +1,6 @@
 import {
   DetailsList,
+  IDetailsRowProps,
   ScrollablePane,
   SearchBox,
   SelectAllVisibility,
@@ -7,17 +8,23 @@ import {
   Sticky,
   StickyPositionType
 } from '@fluentui/react'
+import { ProjectExtension } from 'models'
 import strings from 'ProjectExtensionsStrings'
 import React, { useContext } from 'react'
 import { TemplateSelectDialogContext } from '../context'
 import { TemplateListContentConfigMessage } from '../TemplateListContentConfigMessage'
 import { TemplateSelectDialogSectionComponent } from '../types'
+import { useSelectionList } from '../useSelectionList'
 import styles from './ExtensionsSection.module.scss'
-import { useExtensionsSection } from './useExtensionsSection'
+import { onRenderCheckLocked } from './onRenderCheckLocked'
+import { useColumns } from './useColumns'
 
 export const ExtensionsSection: TemplateSelectDialogSectionComponent = (props) => {
   const context = useContext(TemplateSelectDialogContext)
-  const { selection, items, columns, onSearch, onRenderRow } = useExtensionsSection()
+  const selectedKeys = context.state.selectedExtensions.map((lc) => lc.key)
+  const { selection, onSearch, searchTerm } = useSelectionList(selectedKeys)
+  const items = context.props.data.extensions.filter((ext) => !ext.hidden)
+  const columns = useColumns()
 
   return (
     <div className={styles.root} style={props.style}>
@@ -27,7 +34,20 @@ export const ExtensionsSection: TemplateSelectDialogSectionComponent = (props) =
           selection={selection}
           selectionMode={SelectionMode.multiple}
           selectionPreservedOnEmptyClick={true}
-          onRenderRow={onRenderRow}
+          onRenderRow={(
+            detailsRowProps: IDetailsRowProps,
+            defaultRender: (props?: IDetailsRowProps) => JSX.Element
+          ) => {
+            const ext = detailsRowProps.item as ProjectExtension
+            detailsRowProps.disabled = ext.isLocked(context.state.selectedTemplate)
+            if (detailsRowProps.disabled) detailsRowProps.onRenderCheck = onRenderCheckLocked
+            if (
+              ext.text.toLowerCase().indexOf(searchTerm.toLowerCase()) === -1 &&
+              !selectedKeys.includes(ext.key)
+            )
+              return null
+            return defaultRender(detailsRowProps)
+          }}
           onRenderDetailsHeader={(detailsHeaderProps, defaultRender) => (
             <Sticky stickyPosition={StickyPositionType.Header}>
               <div className={styles.searchBox} hidden={items.length < 5}>
