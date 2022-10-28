b770add77a6ca0a957c473a6e43812f9ce5591a2
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-28 12:16:48 +0200

    ListContentConfig => ContentConfig

--- SharePointFramework/ProjectExtensions/src/models/ContentConfig.ts
+++ SharePointFramework/ProjectExtensions/src/models/ContentConfig.ts
@@ -3,7 +3,7 @@ import { List, sp, Web } from '@pnp/sp'
 import { IListProperties } from './IListProperties'
 import { UserSelectableObject } from './UserSelectableObject'
 
-export interface IListContentConfigSPItem {
+export interface IContentConfigSPItem {
   ContentTypeId: string
   Id: number
   Title: string
@@ -16,21 +16,21 @@ export interface IListContentConfigSPItem {
   GtLccLocked: boolean
 }
 
-export enum ListContentConfigType {
+export enum ContentConfigType {
   List,
   Planner
 }
 
 /**
- * @model ListContentConfig
+ * @model ContentConfig
  */
-export class ListContentConfig extends UserSelectableObject {
+export class ContentConfig extends UserSelectableObject {
   public sourceListProps: IListProperties = {}
   public destListProps: IListProperties = {}
   private _sourceList: string
   private _destinationList: string
 
-  constructor(private _spItem: IListContentConfigSPItem, public web: Web) {
+  constructor(private _spItem: IContentConfigSPItem, public web: Web) {
     super(
       _spItem.Id,
       _spItem.Title,
@@ -43,10 +43,10 @@ export class ListContentConfig extends UserSelectableObject {
     this._destinationList = _spItem.GtLccDestinationList
   }
 
-  public get type(): ListContentConfigType {
+  public get type(): ContentConfigType {
     if (this._spItem.ContentTypeId.indexOf('0x0100B8B4EE61A547B247B49CFC21B67D5B7D01') !== -1)
-      return ListContentConfigType.Planner
-    return ListContentConfigType.List
+      return ContentConfigType.Planner
+    return ContentConfigType.List
   }
   public get fields() {
     return !stringIsNullOrEmpty(this._spItem.GtLccFields) ? this._spItem.GtLccFields.split(',') : []
@@ -65,7 +65,7 @@ export class ListContentConfig extends UserSelectableObject {
       .select('Title', 'ListItemEntityTypeFullName', 'ItemCount', 'BaseTemplate')
       .expand('RootFolder')
       .get<IListProperties>()
-    if (this.type === ListContentConfigType.List) {
+    if (this.type === ContentConfigType.List) {
       this.destListProps = await this.destList
         .select('Title', 'ListItemEntityTypeFullName', 'ItemCount', 'BaseTemplate')
         .expand('RootFolder')
