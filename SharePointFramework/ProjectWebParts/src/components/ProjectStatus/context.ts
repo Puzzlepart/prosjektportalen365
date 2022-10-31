4d32ac786dc1172c367a5184bd9870a55a038ef0
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-31 14:53:50 +0100

    ProjectStatusContext

---
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/context.ts
@@ -0,0 +1,10 @@
+import { createContext } from 'react'
+import { IProjectStatusProps, IProjectStatusState } from './types'
+
+export interface IProjectStatusContext {
+  props: IProjectStatusProps
+  state: IProjectStatusState
+  setState: (newState: Partial<IProjectStatusState>) => void
+}
+
+export const ProjectStatusContext = createContext<IProjectStatusContext>(null)
