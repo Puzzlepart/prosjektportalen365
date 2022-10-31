44078e046cf54a6545ffb780c1ed5c77aa3e5a67
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-10-31 14:23:11 +0100

    useProjectStatus.tsx

---
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/useProjectStatus.ts
@@ -0,0 +1,44 @@
+import { parseUrlHash, getUrlParam } from 'pp365-shared/lib/util';
+import { useEffect, useState } from 'react';
+import { find } from 'underscore';
+import { fetchData } from './fetchData';
+import { IProjectStatusHashState, IProjectStatusProps, IProjectStatusState } from './types';
+
+export function useProjectStatus(props: IProjectStatusProps) {
+    const [state, $setState] = useState<IProjectStatusState>();
+    const setState = (newState: Partial<IProjectStatusState>) => {
+        $setState((_state) => ({ ..._state, ...newState }));
+    };
+
+    useEffect(() => {
+        fetchData(props).then(data => {
+            let [selectedReport] = data.reports;
+            const hashState = parseUrlHash<IProjectStatusHashState>();
+            const selectedReportUrlParam = getUrlParam('selectedReport');
+            const sourceUrlParam = getUrlParam('Source');
+            if (hashState.selectedReport) {
+                selectedReport = find(
+                    data.reports,
+                    (report) => report.id === parseInt(hashState.selectedReport, 10)
+                );
+            } else if (selectedReportUrlParam) {
+                selectedReport = find(
+                    data.reports,
+                    (report) => report.id === parseInt(selectedReportUrlParam, 10)
+                );
+            }
+            const newestReportId = data.reports.length > 0 ? data.reports[0].id : 0;
+
+            setState({
+                data,
+                selectedReport,
+                sourceUrl: decodeURIComponent(sourceUrlParam || ''),
+                loading: false,
+                newestReportId,
+                userHasAdminPermission: data.userHasAdminPermission
+            });
+        });
+    }, []);
+
+    return { state } as const;
+}
