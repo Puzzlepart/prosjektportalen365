fbebf472ac94cd9af06c82350320913589d8185a
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-01 14:28:34 +0100

    TimelineList - removed props

--- SharePointFramework/ProjectWebParts/src/components/ProjectTimeline/TimelineList/ITimelineListProps.tsx
+++
@@ -1,6 +0,0 @@
-import { IBaseWebPartComponentProps } from 'components/BaseWebPartComponent';
-
-
-export interface ITimelineListProps extends IBaseWebPartComponentProps {
-  isSelectionModeNone?: boolean;
-}
--- SharePointFramework/ProjectWebParts/src/components/ProjectTimeline/TimelineList/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectTimeline/TimelineList/index.tsx
@@ -6,11 +6,10 @@ import {
 } from '@fluentui/react'
 import React, { FC, useContext } from 'react'
 import { ProjectTimelineContext } from '../context'
-import { ITimelineListProps } from './ITimelineListProps'
 import styles from './TimelineList.module.scss'
 import { useTimelineList } from './useTimelineList'
 
-export const TimelineList: FC<ITimelineListProps> = (props) => {
+export const TimelineList: FC = () => {
   const context = useContext(ProjectTimelineContext)
   const { getCommandBarProps, onRenderItemColumn, selection, onColumnHeaderClick } = useTimelineList()
   return (
@@ -26,7 +25,7 @@ export const TimelineList: FC<ITimelineListProps> = (props) => {
           items={context.state.data.timelineListItems}
           onRenderItemColumn={onRenderItemColumn}
           selection={selection}
-          selectionMode={props.isSelectionModeNone ? SelectionMode.none : SelectionMode.single}
+          selectionMode={SelectionMode.single}
           layoutMode={DetailsListLayoutMode.justified}
           onColumnHeaderClick={onColumnHeaderClick}
         />
