9167f38900d8d7a24d78db7cb4e237eb183133a2
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-01 10:34:09 +0100

    ProjectCardHeader

--- SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/ProjectCardHeader/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/ProjectList/ProjectCard/ProjectCardHeader/index.tsx
@@ -1,11 +1,19 @@
 import { DocumentCardTitle } from '@fluentui/react/lib/DocumentCard'
+import strings from 'PortfolioWebPartsStrings'
 import React, { FC, useContext } from 'react'
 import { ProjectCardContext } from '../context'
 import styles from './ProjectCardHeader.module.scss'
 import { IProjectCardHeaderProps } from './types'
 
+function useProjectCardHeader() {
+  return {
+    getPhaseStyle: () => ({ backgroundColor: '#808080', opacity: 0.9, color: 'white' })
+  }
+}
+
 export const ProjectCardHeader: FC<IProjectCardHeaderProps> = (props) => {
   const context = useContext(ProjectCardContext)
+  const {getPhaseStyle} = useProjectCardHeader()
   return (
     <div className={styles.root}>
       <div className={styles.logo} hidden={!context.showProjectLogo}>
@@ -14,7 +22,15 @@ export const ProjectCardHeader: FC<IProjectCardHeaderProps> = (props) => {
           onLoad={props.onImageLoad}
         />
       </div>
-      <DocumentCardTitle title={context.project.title} shouldTruncate={true} />
+      <div
+        title={context.project.phase}
+        style={getPhaseStyle()}
+        className={styles.phase}>
+        <span className={styles.title}>
+          {context.project?.phase ?? strings.NotSet}
+        </span>
+      </div>
+      <DocumentCardTitle className={styles.title} title={context.project.title} shouldTruncate={true} />
     </div>
   )
 }
