1ba745f7d3a7d4c3f07ec9d675adefa7c041eb52
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-01 14:07:06 +0100

    TimelineList using logic hook

---
+++ SharePointFramework/ProjectWebParts/src/components/ProjectTimeline/TimelineList/ITimelineListProps.tsx
@@ -0,0 +1,6 @@
+import { IBaseWebPartComponentProps } from 'components/BaseWebPartComponent';
+
+
+export interface ITimelineListProps extends IBaseWebPartComponentProps {
+  isSelectionModeNone?: boolean;
+}
