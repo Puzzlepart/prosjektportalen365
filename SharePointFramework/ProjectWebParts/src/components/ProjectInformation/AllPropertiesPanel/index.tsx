a2e63a40c21127d61dde37838e3a24c05ec5d490
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-02 11:53:59 +0100

    Added component AllPropertiesPanel

---
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformation/AllPropertiesPanel/index.tsx
@@ -0,0 +1,20 @@
+import { Panel, PanelType } from '@fluentui/react'
+import strings from 'ProjectWebPartsStrings'
+import React, { FC, useContext } from 'react'
+import { ProjectInformationContext } from '../context'
+import { ProjectProperties } from '../ProjectProperties'
+
+export const AllPropertiesPanel: FC = () => {
+  const context = useContext(ProjectInformationContext)
+  return (
+    <Panel
+      type={PanelType.medium}
+      headerText={strings.ProjectPropertiesListName}
+      isOpen={context.state.showAllPropertiesPanel}
+      onDismiss={() => context.setState({ showAllPropertiesPanel: false })}
+      isLightDismiss
+      closeButtonAriaLabel={strings.CloseText}>
+      <ProjectProperties properties={context.state.allProperties} />
+    </Panel>
+  )
+}
--- SharePointFramework/ProjectWebParts/src/components/ProjectInformation/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformation/index.tsx
@@ -1,16 +1,16 @@
-import { Panel, PanelType } from '@fluentui/react/lib/Panel'
 import { Shimmer } from '@fluentui/react/lib/Shimmer'
 import { UserMessage } from 'pp365-shared/lib/components/UserMessage'
-import * as strings from 'ProjectWebPartsStrings'
 import { ConfirmDialog } from 'pzl-spfx-components/lib/components/ConfirmDialog'
 import React, { FC } from 'react'
 import { ProgressDialog } from '../ProgressDialog'
 import { Actions } from './Actions'
+import { AllPropertiesPanel } from './AllPropertiesPanel'
 import { ProjectInformationContext } from './context'
 import { CreateParentModal } from './CreateParentModal'
 import { ParentProjectsList } from './ParentProjectsList'
 import styles from './ProjectInformation.module.scss'
 import { ProjectProperties } from './ProjectProperties'
+import { ProjectStatusReport } from './ProjectStatusReport'
 import { SyncProjectModal } from './SyncProjectModal'
 import { IProjectInformationProps } from './types'
 import { useProjectInformation } from './useProjectInformation'
@@ -32,17 +32,10 @@ export const ProjectInformation: FC<IProjectInformationProps> = (props) => {
             <ProjectProperties properties={state.properties} />
             {!props.hideAllActions && state.message && <UserMessage {...state.message} />}
             <ParentProjectsList />
+            <ProjectStatusReport />
             <Actions />
             <ProgressDialog {...state.progress} />
-            <Panel
-              type={PanelType.medium}
-              headerText={strings.ProjectPropertiesListName}
-              isOpen={state.showAllPropertiesPanel}
-              onDismiss={() => setState({ showAllPropertiesPanel: false })}
-              isLightDismiss
-              closeButtonAriaLabel={strings.CloseText}>
-              <ProjectProperties properties={state.allProperties} />
-            </Panel>
+            <AllPropertiesPanel />
             {state.confirmActionProps && <ConfirmDialog {...state.confirmActionProps} />}
             {state.displayCreateParentModal && <CreateParentModal />}
             {state.displaySyncProjectModal && <SyncProjectModal />}
@@ -65,3 +58,4 @@ ProjectInformation.defaultProps = {
 
 export { ProjectInformationModal } from '../ProjectInformationModal'
 export * from './types'
+
