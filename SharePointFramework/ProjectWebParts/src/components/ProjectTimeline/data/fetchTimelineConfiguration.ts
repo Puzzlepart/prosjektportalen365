3cd9d892b9e47f8f7d5c2c0028ed968850b921db
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-02 13:54:58 +0100

    Linting?

--- SharePointFramework/ProjectWebParts/src/components/ProjectTimeline/data/fetchTimelineConfiguration.ts
+++ SharePointFramework/ProjectWebParts/src/components/ProjectTimeline/data/fetchTimelineConfiguration.ts
@@ -4,6 +4,7 @@ import {
 } from 'pp365-portfoliowebparts/lib/models'
 import strings from 'ProjectWebPartsStrings'
 import { IProjectTimelineProps } from '../types'
+
 /**
  * Fetch timeline configuration
  *
