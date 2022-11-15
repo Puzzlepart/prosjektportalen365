f665416cdb9d01776149858ff991be53ac411034
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-15 10:09:29 +0100

    Opportunity matrix in status report #499

---
+++ SharePointFramework/ProjectWebParts/src/components/OpportunityMatrix/useOpportunityMatrixConfiguration.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import { DynamicMatrixConfiguration } from '../DynamicMatrix'
+import { generateMatrixConfiguration } from '../DynamicMatrix/generateMatrixConfiguration'
+import { IOpportunityMatrixProps, RiskMatrixHeaders } from './types'
+
+/**
+ * Configuration hook for `OpportunityMatrix`
+ *
+ * @param props Props
+ */
+export function useOpportunityMatrixConfiguration(props: IOpportunityMatrixProps) {
+  const [configuration, setConfiguration] = useState<DynamicMatrixConfiguration>([])
+
+  useEffect(() => {
+    if (props.size) {
+      setConfiguration(
+        generateMatrixConfiguration(parseInt(props.size, 10), RiskMatrixHeaders(props))
+      )
+    }
+  }, [props])
+
+  return configuration
+}
