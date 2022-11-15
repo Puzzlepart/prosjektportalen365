28614e187335944d4c6e4b025c2b503fbfe985bc
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-15 12:33:36 +0100

    Added field GtSecView

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/UncertaintySection/useFetchListData.ts
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/UncertaintySection/useFetchListData.ts
@@ -4,11 +4,17 @@ import { ProjectStatusContext } from '../../context'
 import { useContext } from 'react'
 import { SectionContext } from '../context'
 import { UncertaintyElementModel } from 'types'
+import { IUncertaintySectionData } from './types'
+import { SPField } from 'pp365-shared/lib/models'
+
+const columnMaxWidth: Record<string, number> = { Text: 250, Note: 250, Choice: 150, Number: 100 }
 
 /**
- * Fetch list data
+ * Fetch list data hook. 
+ * 
+ * @returns A function used to fetch data for `UncertaintySection`.
  */
-export function useFetchListData() {
+export function useFetchListData(): () => Promise<IUncertaintySectionData> {
   const context = useContext(ProjectStatusContext)
   const { section } = useContext(SectionContext)
   return async () => {
@@ -19,25 +25,25 @@ export function useFetchListData() {
         list.getItemsByCAMLQuery({ ViewXml: viewXml }, 'FieldValuesAsText') as Promise<any[]>,
         list.fields
           .select('Title', 'InternalName', 'TypeAsString')
-          .get<{ Title: string; InternalName: string; TypeAsString: string }[]>()
+          .get<SPField[]>()
       ])
       if (items.length === 0) return null
       const itemValues = items.map((i) => i.FieldValuesAsText)
       const matrixElements = itemValues.map((i) => new UncertaintyElementModel(i))
       const columns = section.viewFields
-        .filter((vf) => fields.filter((fld) => fld.InternalName === vf).length === 1)
-        .map<IColumn>((vf) => {
-          const [field] = fields.filter((fld) => fld.InternalName === vf)
-          return {
-            key: field.InternalName,
-            fieldName: field.InternalName,
-            name: field.Title,
-            minWidth: 100,
-            maxWidth: { Text: 250, Note: 250, Choice: 150, Number: 100 }[field.TypeAsString] || 150,
-            isResizable: true,
-            isMultiline: true
-          } 
-        })
+        .map<SPField>((viewField) => fields.find((fld) => fld.InternalName === viewField))
+        .filter(Boolean)
+        .map<IColumn>((field) => ({
+          key: field.InternalName,
+          fieldName: field.InternalName,
+          name: field.Title,
+          minWidth: 100,
+          maxWidth: columnMaxWidth[field.TypeAsString] ?? 150,
+          isResizable: true,
+          isMultiline: true
+        }))
+        // eslint-disable-next-line no-console
+        console.log(itemValues[0])
       return { items: itemValues, columns, matrixElements }
     } catch (error) {
       throw error
