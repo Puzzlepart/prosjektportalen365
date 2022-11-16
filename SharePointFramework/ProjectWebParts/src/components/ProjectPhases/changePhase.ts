5823d0b4c5e06d7205e42b679d24e1540ca5c6f4
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-11-16 13:17:13 +0100

    changePhase logs to list on error

--- SharePointFramework/ProjectWebParts/src/components/ProjectPhases/changePhase.ts
+++ SharePointFramework/ProjectWebParts/src/components/ProjectPhases/changePhase.ts
@@ -4,14 +4,18 @@ import { changeWelcomePage } from './changeWelcomePage'
 import { runPhaseHook } from './runPhaseHook'
 import { modifyCurrentPhaseView } from './modifyCurrentPhaseView'
 import { IPhaseSitePageModel, IProjectPhasesProps } from './types'
+import { ListLogger } from 'pp365-shared/lib/logging'
+import { ProjectPhases } from '.'
 
 /**
- * Change phase
+ * Change phase for the current project. Runs `SPDataAdapter.project.updatePhase`, runs phase
+ * hooks if specified, and changes the welcome page if `useDynamicHomepage` is enabled. Also modified
+ * current phase view for documents library.
  *
  * @param phase Phase
- * @param phaseTextField Phase TextField
- * @param props IProjectPhasesProps props
- * @param phaseSitePages Phase SitePages
+ * @param phaseTextField Phase text field
+ * @param props Component props for `ProjectPhases`
+ * @param phaseSitePages Phase site pages
  */
 export const changePhase = async (
   phase: ProjectPhaseModel,
@@ -31,6 +35,12 @@ export const changePhase = async (
     await modifyCurrentPhaseView(phase.name, props.currentPhaseViewName)
     sessionStorage.clear()
   } catch (error) {
+    ListLogger.log({
+      message: error.message,
+      level: 'Error',
+      functionName: 'changePhase',
+      component: ProjectPhases.displayName
+    })
     throw error
   }
 }
