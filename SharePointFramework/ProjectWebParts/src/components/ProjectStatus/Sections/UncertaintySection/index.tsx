dd9dfe04f7455e8b0711fa1913af3867ddaa9c68
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-02-22 10:20:01 +0100

    #999

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/UncertaintySection/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/UncertaintySection/index.tsx
@@ -21,7 +21,8 @@ export const UncertaintySection: FC = () => {
   const { state, matrixElements, items, columns, shouldRenderContent } = useUncertaintySection()
 
   /**
-   * Render content
+   * Render content for the Uncertainty section. Handles potential errors and renders OpportunityMatrix 
+   * or RiskMatrix based on the content type of the first item in the list.
    */
   function renderContent() {
     if (state.error)
