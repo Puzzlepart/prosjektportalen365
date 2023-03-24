69e4d4148b64e45c0b57e3699e88e2753720b179
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-03-24 14:31:52 +0100

    Fixed issue in Templates/.tasks/generateJsonTemplates.js

--- Templates/.tasks/generateJsonTemplates.js
+++ Templates/.tasks/generateJsonTemplates.js
@@ -8,28 +8,53 @@ const pkg = require('../../package.json')
 const JsonTokenReplace = require('@ptkdev/json-token-replace')
 const jsonTokenReplace = new JsonTokenReplace()
 
+
+/**
+ * Get file content for the given file path in JSON format. If the file 
+ * does not exist or the JSON is invalid, the fallback value is returned.
+ * 
+ * By default the function goes up one folder from the current folder.
+ * 
+ * @param {*} file File path 
+ * @param {*} fallbackValue Fallback value if file does not exist
+ * 
+ * @returns File contents as JSON
+ */
+function getFileContent(file, fallbackValue = {}) {
+    try {
+        const fileContent = fs.readFileSync(path.resolve(__dirname, '..', file), 'UTF-8')
+        const fileContentJson = JSON.parse(fileContent)
+        return fileContentJson
+    } catch (error) {
+        return fallbackValue
+    }
+}
+
 // Template names for the different languages
 const templateNames = {
-    "nb-NO": {
-        "Project": "Standardmal",
-        "Program": "Programmal",
-        "Parent": "Overordnet"
+    'nb-NO': {
+        'Project': 'Standardmal',
+        'Program': 'Programmal',
+        'Parent': 'Overordnet'
     },
-    "en-US": {
-        "Project": "DefaultTemplate",
-        "Program": "ProgramTemplate",
-        "Parent": "ParentTemplate"
+    'en-US': {
+        'Project': 'DefaultTemplate',
+        'Program': 'ProgramTemplate',
+        'Parent': 'ParentTemplate'
     }
 }
 
 // Resources in JSON format
-const resourcesJson = require('../Resources.json')
+const resourcesJson = getFileContent('Resources.json')
 
 // JSON templates
-const jsonTemplates = fs.readdirSync(path.resolve(__dirname, '../JsonTemplates'))
+const jsonMasterTemplates = fs.readdirSync(path.resolve(__dirname, '../JsonTemplates'))
 
-// Get the current channel config
-const currentChannelConfig = require('../../.current-channel-config.json')
+// Get the main channel config
+const mainChannelConfig = getFileContent('../channels/main.json')
+
+// Get the current channel config with fallback to main channel config
+const currentChannelConfig = getFileContent('../.current-channel-config.json', mainChannelConfig)
 
 // Generate the channel replace values
 const channelReplaceValue = Object.keys(currentChannelConfig.spfx.solutions).reduce((acc, key) => {
@@ -41,9 +66,9 @@ const channelReplaceValue = Object.keys(currentChannelConfig.spfx.solutions).red
 }, {})
 
 // For each JSON template, replace the tokens and write the output to the correct folder.
-jsonTemplates.forEach(templateFile => {
-    const templateJson = require(`../JsonTemplates/${templateFile}`)
-    const templateType = templateFile.substring("_JsonTemplate".length).replace((/\.[^.]+/), '')
+jsonMasterTemplates.forEach(templateFile => {
+    const templateJson = getFileContent(`../JsonTemplates/${templateFile}`)
+    const templateType = templateFile.substring('_JsonTemplate'.length).replace((/\.[^.]+/), '')
     const outputPaths = {
         'en-US': path.resolve(__dirname, `../Content/Portfolio_content.en-US/ProjectTemplates/${templateNames['en-US'][templateType]}.txt`),
         'no-NB': path.resolve(__dirname, `../Content/Portfolio_content.no-NB/ProjectTemplates/${templateNames['nb-NO'][templateType]}.txt`)
