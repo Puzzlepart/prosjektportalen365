ed0ef649a84a232a98f1b4fcbb82186727fca28b
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-03-24 13:31:19 +0100

    Tilgang for å opprette porteføljeoversikt-visninger er nå basert på tilgang til listen (#1028)

--- SharePointFramework/PortfolioWebParts/src/interfaces/IPortfolioConfiguration.ts
+++ SharePointFramework/PortfolioWebParts/src/interfaces/IPortfolioConfiguration.ts
@@ -1,9 +1,33 @@
 import { ProjectColumn, PortfolioOverviewView } from 'pp365-shared/lib/models'
 
 export interface IPortfolioConfiguration {
+  /**
+   * Available columns
+   */
   columns: ProjectColumn[]
+
+  /**
+   * Available refiners
+   */
   refiners: ProjectColumn[]
+
+  /**
+   * Available views
+   */
   views: PortfolioOverviewView[]
+
+  /**
+   * New forms and edit forms urls for views list
+   */
   viewsUrls: { defaultNewFormUrl: string; defaultEditFormUrl: string }
+
+  /**
+   * New form and edit form urls for columns list
+   */
   columnUrls: { defaultNewFormUrl: string; defaultEditFormUrl: string }
+
+  /**
+   * Current user can add views (has `ADD_LIST_ITEMS` permission)
+   */
+  userCanAddViews?: boolean
 }
