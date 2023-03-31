a94a1c4d0630bcb246ceaba7b09fb506968dadd4
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-03-31 14:12:10 +0200

    Component for displaying a Sticky list header.

--- SharePointFramework/PortfolioWebParts/src/components/PortfolioOverview/ListHeader/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/PortfolioOverview/ListHeader/index.tsx
@@ -6,23 +6,28 @@ import { EXECUTE_SEARCH } from '../reducer'
 import styles from './ListHeader.module.scss'
 import { IListHeaderProps } from './types'
 
+/**
+ * Component for displaying a Sticky list header.
+ */
 export const ListHeader: FC<IListHeaderProps> = (props) => {
     const context = useContext(PortfolioOverviewContext)
     return (
         <Sticky
-            stickyClassName={styles.root}
+            stickyClassName={styles.sticky}
             stickyPosition={StickyPositionType.Header}
             isScrollSynced={true}>
-            <div className={styles.header}>
-                <div className={styles.title}>{context.props.title}</div>
+            <div className={styles.root}>
+                <div className={styles.header}>
+                    <div className={styles.title}>{context.props.title}</div>
+                </div>
+                <div className={styles.searchBox} hidden={!context.props.showSearchBox}>
+                    <SearchBox
+                        onChange={(_e, newValue) => context.dispatch(EXECUTE_SEARCH(newValue))}
+                        placeholder={''}
+                    />
+                </div>
+                <div className={styles.headerColumns}>{props.defaultRender(props.headerProps)}</div>
             </div>
-            <div className={styles.searchBox} hidden={!context.props.showSearchBox}>
-                <SearchBox
-                    onChange={(_e, newValue) => context.dispatch(EXECUTE_SEARCH(newValue))}
-                    placeholder={''}
-                />
-            </div>
-            <div className={styles.headerColumns}>{props.defaultRender(props.headerProps)}</div>
         </Sticky>
     )
 }
