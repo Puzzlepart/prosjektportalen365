df421eac1658d1367d2f7c63e298657891d26a89
Bloom <remi.blom-ohlsen@puzzlepart.com>
2023-04-05 11:39:38 +0200

    Sorteringsrekkefølge på Planner oppgaver (#1081)
    * Add GtSortOrder to Planner lists
    
    * Change sort order to descending for planner items
    
    * Overwrite default planner items
    
    * Minor changelog fix
    
    * Update releasenotes for 1.9

--- SharePointFramework/ProjectExtensions/src/models/IPlannerTaskSPItem.ts
+++ SharePointFramework/ProjectExtensions/src/models/IPlannerTaskSPItem.ts
@@ -2,6 +2,7 @@ export interface IPlannerTaskSPItem {
   Title: string
   GtDescription: string
   GtCategory: string
+  GtSortOrder: number
   GtChecklist: string
   GtAttachments: string
   GtPlannerPreviewType: string
