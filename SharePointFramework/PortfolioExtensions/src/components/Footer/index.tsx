317bcb1dc3fab558f55010bcb83eeeedaef72320
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-05-03 11:21:09 +0200

    Footer v1.0 (#1092)
    * Footer v1.0
    
    * Updated ci-channel-test workflow to temporarily trigger on branch issues/1071
    
    * Added Promoted Links to Footer
    
    * Added site settings link to footer for portfolio site only
    
    * Using Promise.all() to wait for all promises to resolve in the footer extension
    
    * Added help content to footer
    
    * Removed help-content-application-customizer from ProjectExtensions
    
    * 002300 - Footer.txt site script
    
    * HelpContent component should be hidden when no help content is available
    
    * All links in footer rendered as action buttons
    
    * Reverted .github/workflows/ci-channel-test.yml
    
    ---------
    
    Co-authored-by: Bloom <remi.blom-ohlsen@puzzlepart.com>

--- SharePointFramework/PortfolioExtensions/src/components/Footer/index.tsx
+++ SharePointFramework/PortfolioExtensions/src/components/Footer/index.tsx
@@ -1,11 +1,13 @@
-import { Link } from '@fluentui/react'
-import strings from 'PortfolioExtensionsStrings'
 import React, { FC } from 'react'
-import { FooterContext } from './context'
+import { ConfigurationLink } from './ConfigurationLink'
 import styles from './Footer.module.scss'
 import { InstallVersion } from './InstallVersion'
+import { PromotedLinks } from './PromotedLinks'
+import { FooterContext } from './context'
 import { IFooterProps } from './types'
 import { useFooter } from './useFooter'
+import { SiteSettingsLink } from './SiteSettingsLink'
+import { HelpContent } from './HelpContent'
 
 export const Footer: FC<IFooterProps> = (props) => {
   const footer = useFooter(props)
@@ -14,15 +16,13 @@ export const Footer: FC<IFooterProps> = (props) => {
       <div className={styles.root}>
         <div className={styles.content}>
           <section className={styles.left}>
-            <Link
-              className={styles.configurationLink}
-              href={`${props.pageContext.web.absoluteUrl}/SitePages/Konfigurasjon.aspx`}
-            >
-              {strings.ConfigurationLinkText}
-            </Link>
+            <SiteSettingsLink />
+            <ConfigurationLink />
+            <PromotedLinks />
           </section>
           <section className={styles.right}>
             <InstallVersion />
+            <HelpContent />
           </section>
         </div>
       </div>
