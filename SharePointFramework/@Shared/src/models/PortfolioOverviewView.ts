fadb225c106399b82f53791c746a55a2ffe4f7e9
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-05-09 12:32:17 +0200

    pp365-shared@1.9.0-1111.11

--- SharePointFramework/@Shared/src/models/PortfolioOverviewView.ts
+++ SharePointFramework/@Shared/src/models/PortfolioOverviewView.ts
@@ -16,7 +16,7 @@ export class SPPortfolioOverviewViewItem {
 }
 
 export class PortfolioOverviewView {
-  public id: number
+  public id: string | number
   public title: string
   public sortOrder: number
   public searchQuery: string
@@ -28,16 +28,21 @@ export class PortfolioOverviewView {
   public groupBy?: ProjectColumn
   public scope?: string
 
-  constructor(private _item: SPPortfolioOverviewViewItem) {
-    this.id = _item.Id
-    this.title = _item.Title
-    this.sortOrder = _item.GtSortOrder
-    this.searchQuery = _item.GtSearchQuery
-    this.isDefaultView = _item.GtPortfolioIsDefaultView
-    this.iconName = _item.GtPortfolioFabricIcon
-    this.isPersonal = _item.GtPortfolioIsPersonalView
+  constructor(private _item?: SPPortfolioOverviewViewItem) {
+    this.id = _item?.Id
+    this.title = _item?.Title
+    this.sortOrder = _item?.GtSortOrder
+    this.searchQuery = _item?.GtSearchQuery
+    this.isDefaultView = _item?.GtPortfolioIsDefaultView
+    this.iconName = _item?.GtPortfolioFabricIcon
+    this.isPersonal = _item?.GtPortfolioIsPersonalView
   }
 
+  /**
+   * Configure the view with columns.
+   * 
+   * @param columns Columns to configure the view with
+   */
   public configure(columns: ProjectColumn[] = []): PortfolioOverviewView {
     this.columns = this._item.GtPortfolioColumnsId.map((id) =>
       _.find(columns, (col) => col.id === id)
@@ -46,6 +51,20 @@ export class PortfolioOverviewView {
       _.find(columns, (col) => col.id === id)
     ).sort((a, b) => a.sortOrder - b.sortOrder)
     this.groupBy = _.find(columns, (col) => col.id === this._item.GtPortfolioGroupById)
+    return this
+  }
+
+  /**
+   * Configure the view from another view, overriding properties.
+   * 
+   * @param view View to configure from≤
+   */
+  public configureFrom(view: PortfolioOverviewView): PortfolioOverviewView {
+    this.columns = view.columns
+    this.refiners = view.refiners
+    this.groupBy = view.groupBy
+    this.scope = view.scope
+    this.searchQuery = view.searchQuery
     return this
   }
 }
