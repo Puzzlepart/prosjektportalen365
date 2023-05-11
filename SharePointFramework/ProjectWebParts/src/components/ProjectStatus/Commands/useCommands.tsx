26f378615e096f10558ab4d26d9e6a34b6133d63
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-05-11 14:27:34 +0200

    Midlertidig fiks for #1049 (#1102)

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Commands/useCommands.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Commands/useCommands.tsx
@@ -1,4 +1,4 @@
-import { getId, IContextualMenuItem } from '@fluentui/react'
+import { getId, IContextualMenuItem, Spinner, SpinnerSize } from '@fluentui/react'
 import { formatDate } from 'pp365-shared/lib/helpers'
 import strings from 'ProjectWebPartsStrings'
 import { useContext } from 'react'
@@ -10,9 +10,21 @@ import { useEditFormUrl } from './useEditFormUrl'
 import { usePublishReport } from './usePublishReport'
 import { useRedirectNewStatusReport } from './useRedirectNewStatusReport'
 import { useReportOptions } from './useReportOptions'
+import React from 'react'
 
 /**
- * Component logic hook for `Commands`
+ * Component logic hook for `Commands`. This hook is used to generate the commands for the `CommandBar` component.
+ *
+ * The following commands are conditionally rendered:
+ * - `NEW_STATUS_REPORT`: Renders a command to create a new status report. This command is disabled if there are any unpublished reports.
+ * - `DELETE_REPORT`: Renders a command to delete the selected report. This command is disabled if the selected report is published or if the component is publishing a report.
+ * - `EDIT_REPORT`: Renders a command to edit the selected report. This command is disabled if the selected report is published or if the component is publishing a report.
+ * - `PUBLISH_REPORT`: Renders a command to publish the selected report. This command is disabled if the selected report is published or if the component is publishing a report.
+ * - `IS_PUBLISHING`: Renders a spinner if the component is publishing a report.
+ * - `NAVIGATE_TO_SOURCE_URL`: Renders a command to navigate to the source URL.
+ * - `GET_SNAPSHOT`: Renders a command to get the snapshot of the selected report. This command is disabled if the selected report does not have any attachments or if the component is publishing a report.
+ * - `REPORT_DROPDOWN`: Renders a dropdown menu of all the reports. This command is disabled if there are no reports.
+ * - `STATUS_ICON`: Renders an icon to indicate the status of the selected report. This command is disabled and only for display purposes.
  */
 export function useCommands() {
   const context = useContext(ProjectStatusContext)
@@ -34,7 +46,7 @@ export function useCommands() {
         key: 'DELETE_REPORT',
         name: strings.DeleteReportButtonText,
         iconProps: { iconName: 'Delete' },
-        disabled: context.state.selectedReport?.published,
+        disabled: context.state.selectedReport?.published || context.state.isPublishing,
         onClick: () => {
           deleteReport()
         }
@@ -45,10 +57,11 @@ export function useCommands() {
         name: strings.EditReportButtonText,
         iconProps: { iconName: 'Edit' },
         href: getEditFormUrl(context.state.selectedReport),
-        disabled: context.state.selectedReport?.published
+        disabled: context.state.selectedReport?.published || context.state.isPublishing
       },
     context.state.selectedReport &&
-      context.state.userHasAdminPermission && {
+      context.state.userHasAdminPermission &&
+      !context.state.isPublishing && {
         key: 'PUBLISH_REPORT',
         name: strings.PublishReportButtonText,
         iconProps: { iconName: 'PublishContent' },
@@ -57,7 +70,19 @@ export function useCommands() {
           context.dispatch(REPORT_PUBLISHING())
           publishReport()
         }
+      },
+    context.state.isPublishing && {
+      key: 'IS_PUBLISHING',
+      onRender: () => {
+        return (
+          <Spinner
+            label={strings.PublishReportSpinnerText}
+            size={SpinnerSize.small}
+            labelPosition='right'
+          />
+        )
       }
+    }
   ].filter(Boolean)
   const farItems: IContextualMenuItem[] = []
   if (context.state.sourceUrl) {
@@ -73,7 +98,7 @@ export function useCommands() {
       key: 'GET_SNAPSHOT',
       name: strings.GetSnapshotButtonText,
       iconProps: { iconName: 'Photo2' },
-      disabled: !context.state.selectedReport?.hasAttachments,
+      disabled: !context.state.selectedReport?.hasAttachments || context.state.isPublishing,
       onClick: () => {
         window.open(first(context.state.selectedReport.attachments).ServerRelativeUrl)
       }
