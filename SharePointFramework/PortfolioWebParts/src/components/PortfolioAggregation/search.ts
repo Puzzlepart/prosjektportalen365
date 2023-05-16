95d5d04f363ae8f2a8e6374d7e33bb491538ef04
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-05-16 09:09:13 +0200

    Søk i aggregerte oversikter søker nå også på prosjektnavn

--- SharePointFramework/PortfolioWebParts/src/components/PortfolioAggregation/search.ts
+++ SharePointFramework/PortfolioWebParts/src/components/PortfolioAggregation/search.ts
@@ -9,9 +9,14 @@ import { getObjectValue as get } from 'pp365-shared/lib/helpers'
  * @param columns Columns
  */
 export const searchItem = (item: any, searchTerm: string, columns: IColumn[]) => {
-  searchTerm = searchTerm.toLowerCase()
-  const searchObj = columns.reduce((obj, col) => {
-    return { ...obj, [col.fieldName]: get(item, col.fieldName, null) }
-  }, {})
-  return JSON.stringify(searchObj).toLowerCase().indexOf(searchTerm) !== -1
+  try {
+    const searchObj = columns.reduce((obj, col, index) => {
+      return { ...obj, [index]: get(item, col.fieldName, null) }
+    }, {
+      [columns.length]: item['SiteTitle']
+    } as Record<string, any>)
+    return JSON.stringify(searchObj).toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+  } catch (error) {
+    return false
+  }
 }
