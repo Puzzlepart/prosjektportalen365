aff28d4a77336b8158a19e39a99ad3343fa110b2
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-05-25 15:50:04 +0200

    Søk i aggregerte oversikter søker nå også på prosjektnavn (#1124) [skip-ci]

--- SharePointFramework/PortfolioWebParts/src/components/PortfolioAggregation/search.ts
+++ SharePointFramework/PortfolioWebParts/src/components/PortfolioAggregation/search.ts
@@ -2,20 +2,27 @@ import { IColumn } from '@fluentui/react/lib/DetailsList'
 import { getObjectValue as get } from 'pp365-shared/lib/helpers'
 
 /**
- * Search item by stringifying the items and searching for the search term.
+ * Search item by search term. Search term is matched against all columns,
+ * aswell as the SiteTitle property (name of the project). Uses `JSON.stringify`
+ * to create a string representation of the item, which is then matched against
+ * the search term.
  *
  * @param item Item
  * @param searchTerm Search term
  * @param columns Columns
  */
 export const searchItem = (item: any, searchTerm: string, columns: IColumn[]) => {
-  searchTerm = searchTerm.toLowerCase()
-  const searchObj = columns.reduce((obj, col) => {
-    return { ...obj, [col.fieldName]: get(item, col.fieldName, null) }
-  }, {})
   try {
-    return JSON.stringify(searchObj).toLowerCase().indexOf(searchTerm) !== -1
+    const searchObj = columns.reduce(
+      (obj, col, index) => {
+        return { ...obj, [index]: get(item, col.fieldName, null) }
+      },
+      {
+        [columns.length]: item['SiteTitle']
+      } as Record<string, any>
+    )
+    return JSON.stringify(searchObj).toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
   } catch (error) {
-    return true
+    return false
   }
 }
