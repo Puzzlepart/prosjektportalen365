9546a5a62c300d740976ba9d2dbcc0f3e3885b03
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-05-25 15:06:23 +0200

    SPDataAdapter.getConfigurations(folderPath: string)

--- SharePointFramework/ProjectWebParts/src/data/SPDataAdapter/index.ts
+++ SharePointFramework/ProjectWebParts/src/data/SPDataAdapter/index.ts
@@ -10,11 +10,12 @@ import * as strings from 'ProjectWebPartsStrings'
 import { IEntityField } from 'sp-entityportal-service/types'
 import { find } from 'underscore'
 import { ISPDataAdapterConfiguration } from './ISPDataAdapterConfiguration'
+import { IConfigurationFile } from 'types'
 
 class SPDataAdapter extends SPDataAdapterBase<ISPDataAdapterConfiguration> {
   public project: ProjectDataService
   private _name = 'SPDataAdapter'
-  
+
   /**
    * Configure the SP data adapter
    *
@@ -297,6 +298,34 @@ class SPDataAdapter extends SPDataAdapterBase<ISPDataAdapterConfiguration> {
    */
   public clearCache() {
     this.project.clearCache()
+  }
+
+  /**
+   * Get configuration files from the specified folder `folderPath` relative to the configuration folder in Site Assets.
+   *
+   * @param folderPath Folder path relative to the configuration folder in Site Assets
+   */
+  public async getConfigurations(folderPath: string): Promise<IConfigurationFile[]> {
+    try {
+      const { ServerRelativeUrl } = await this.portal.web.rootFolder
+        .select('ServerRelativeUrl')
+        .usingCaching()
+        .get<{ ServerRelativeUrl: string }>()
+      const folderRelativeUrl = `${ServerRelativeUrl}/${strings.SiteAssetsConfigurationFolder}/${folderPath}`
+      const folder = this.portal.web.getFolderByServerRelativeUrl(folderRelativeUrl)
+      const files = await folder.files
+        .select('Name', 'ServerRelativeUrl', 'ListItemAllFields/Title')
+        .expand('ListItemAllFields')
+        .usingCaching()
+        .get()
+      return files.map((file) => ({
+        name: file.Name,
+        title: file.ListItemAllFields.Title ?? `${strings.UnknownConfigurationName} ${file.Name}`,
+        url: file.ServerRelativeUrl
+      }))
+    } catch {
+      return []
+    }
   }
 }
 
