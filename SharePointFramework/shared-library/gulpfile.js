0294d6b94547d398509575f5a253e1933acdfd51
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-06-02 12:53:00 +0200

    Pre- and postwatch scripts

--- SharePointFramework/shared-library/gulpfile.js
+++ SharePointFramework/shared-library/gulpfile.js
@@ -1,16 +1,17 @@
-'use strict';
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict'
 
-const build = require('@microsoft/sp-build-web');
+const build = require('@microsoft/sp-build-web')
 
-build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+build.addSuppression('Warning - [sass] The local CSS class \'ms-Grid\' is not camelCase and will not be type-safe.')
 
-var getTasks = build.rig.getTasks;
+var getTasks = build.rig.getTasks
 build.rig.getTasks = function () {
-  var result = getTasks.call(build.rig);
+  var result = getTasks.call(build.rig)
 
-  result.set('serve', result.get('serve-deprecated'));
+  result.set('serve', result.get('serve-deprecated'))
 
-  return result;
-};
+  return result
+}
 
-build.initialize(require('gulp'));
+build.initialize(require('gulp'))
