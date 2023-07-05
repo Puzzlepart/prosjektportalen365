9af338c092137d27f6e0e7b73072eebe566c79b6
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-07-06 00:19:47 +0200

    Forbedret Liste-komponent

---
+++ SharePointFramework/PortfolioWebParts/src/components/List/ListHeader/index.tsx
@@ -0,0 +1,46 @@
+import {
+  IDetailsHeaderProps,
+  IRenderFunction,
+  Sticky,
+  StickyPositionType
+} from '@fluentui/react'
+import { SearchBox } from '@fluentui/react/lib/SearchBox'
+import React, { FC } from 'react'
+import { IListProps } from '../types'
+import styles from './ListHeader.module.scss'
+import { IListHeaderProps } from './types'
+
+/**
+ * Component for displaying a Sticky list header.
+ */
+export const ListHeader: FC<IListHeaderProps> = (props) => {/*  */
+  return (
+    <Sticky
+      stickyClassName={styles.sticky}
+      stickyPosition={StickyPositionType.Header}
+      isScrollSynced={true}
+    >
+      <div className={styles.root}>
+        <div className={styles.header}>
+          <div className={styles.title}>{props.title}</div>
+        </div>
+        <div className={styles.searchBoxContainer} hidden={!props.searchBox}>
+          <SearchBox {...props.searchBox} />
+        </div>
+        {props.defaultRender && (
+          <div className={styles.headerColumns}>{props.defaultRender(props.headerProps)}</div>
+        )}
+      </div>
+    </Sticky>
+  )
+}
+
+/**
+ * A function that returns a render function for the list header.
+ *
+ * @param props List props
+ */
+export const onRenderDetailsHeader = (props: IListProps): IRenderFunction<IDetailsHeaderProps> => (
+  headerProps,
+  defaultRender
+) => <ListHeader {...props} headerProps={headerProps} defaultRender={defaultRender} />
