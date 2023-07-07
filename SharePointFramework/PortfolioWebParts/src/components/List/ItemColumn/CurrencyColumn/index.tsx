6e9be154427ddccca6513cc72e1c9f9d5d4b9bb1
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-07-07 14:18:07 +0200

    CurrencyColumn.getDataTypeProperties

--- SharePointFramework/PortfolioWebParts/src/components/List/ItemColumn/CurrencyColumn/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/List/ItemColumn/CurrencyColumn/index.tsx
@@ -1,9 +1,8 @@
-import { ITextFieldProps, TextField } from '@fluentui/react'
+import { TextField } from '@fluentui/react'
 import strings from 'PortfolioWebPartsStrings'
 import { tryParseCurrency } from 'pp365-shared-library'
 import React from 'react'
-import { IColumnDataTypePropertyField } from '../ColumnDataTypeField'
-import { ColumnRenderComponentRegistry } from '../registry'
+import { ColumnDataTypePropertyField } from '../ColumnDataTypeField'
 import { ColumnRenderComponent } from '../types'
 import { ICurrencyColumnProps } from './types'
 
@@ -28,45 +27,42 @@ CurrencyColumn.key = 'currency'
 CurrencyColumn.id = 'Currency'
 CurrencyColumn.displayName = strings.ColumnRenderOptionCurrency
 CurrencyColumn.iconName = 'Money'
-ColumnRenderComponentRegistry.register(
-  CurrencyColumn,
-  (onChange, dataTypeProperties: Record<string, any>) => [
+CurrencyColumn.getDataTypeProperties = (onChange, dataTypeProperties: Record<string, any>) => [
+  ColumnDataTypePropertyField(
+    TextField,
     {
-      type: TextField,
-      props: {
-        type: 'number',
-        label: strings.ColumnRenderOptionCurrencyMinimumFractionDigitsLabel,
-        placeholder: CurrencyColumn.defaultProps.minimumFractionDigits.toString(),
-        value: dataTypeProperties.minimumFractionDigits,
-        onChange: (_, value) => onChange('minimumFractionDigits', parseInt(value))
-      }
-    } as IColumnDataTypePropertyField<ITextFieldProps>,
+      type: 'number',
+      label: strings.ColumnRenderOptionCurrencyMinimumFractionDigitsLabel,
+      placeholder: CurrencyColumn.defaultProps.minimumFractionDigits.toString(),
+      value: dataTypeProperties.minimumFractionDigits,
+      onChange: (_, value) => onChange('minimumFractionDigits', parseInt(value))
+    }
+  ),
+  ColumnDataTypePropertyField(
+    TextField,
     {
-      type: TextField,
-      props: {
-        type: 'number',
-        label: strings.ColumnRenderOptionCurrencyMaximumFractionDigitsLabel,
-        placeholder: CurrencyColumn.defaultProps.maximumFractionDigits.toString(),
-        value: dataTypeProperties.maximumFractionDigits,
-        onChange: (_, value) => onChange('maximumFractionDigits', parseInt(value))
-      }
-    } as IColumnDataTypePropertyField<ITextFieldProps>,
+      type: 'number',
+      label: strings.ColumnRenderOptionCurrencyMaximumFractionDigitsLabel,
+      placeholder: CurrencyColumn.defaultProps.maximumFractionDigits.toString(),
+      value: dataTypeProperties.maximumFractionDigits,
+      onChange: (_, value) => onChange('maximumFractionDigits', parseInt(value))
+    }
+  ),
+  ColumnDataTypePropertyField(
+    TextField,
     {
-      type: TextField,
-      props: {
-        label: strings.ColumnRenderOptionCurrencyFallbackValueLabel,
-        value: dataTypeProperties.fallbackValue,
-        onChange: (_, value) => onChange('fallbackValue', value)
-      }
-    } as IColumnDataTypePropertyField<ITextFieldProps>,
+      label: strings.ColumnRenderOptionCurrencyFallbackValueLabel,
+      value: dataTypeProperties.fallbackValue,
+      onChange: (_, value) => onChange('fallbackValue', value)
+    }
+  ),
+  ColumnDataTypePropertyField(
+    TextField,
     {
-      type: TextField,
-      props: {
-        label: strings.ColumnRenderOptionCurrencyPrefixLabel,
-        placeholder: CurrencyColumn.defaultProps.currencyPrefix,
-        value: dataTypeProperties.currencyPrefix,
-        onChange: (_, value) => onChange('currencyPrefix', value)
-      }
-    } as IColumnDataTypePropertyField<ITextFieldProps>
-  ]
-)
+      label: strings.ColumnRenderOptionCurrencyPrefixLabel,
+      placeholder: CurrencyColumn.defaultProps.currencyPrefix,
+      value: dataTypeProperties.currencyPrefix,
+      onChange: (_, value) => onChange('currencyPrefix', value)
+    }
+  )
+]
