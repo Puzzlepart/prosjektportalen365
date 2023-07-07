40e45f76175bb2496140c9d832db1b225f88e473
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-07-07 14:23:25 +0200

    DateColumn.getDataTypeProperties

--- SharePointFramework/PortfolioWebParts/src/components/List/ItemColumn/DateColumn/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/List/ItemColumn/DateColumn/index.tsx
@@ -1,11 +1,10 @@
-import { IToggleProps, Toggle } from '@fluentui/react'
+import { Toggle } from '@fluentui/react'
 import strings from 'PortfolioWebPartsStrings'
+import { formatDate } from 'pp365-shared-library'
 import React from 'react'
+import { ColumnDataTypePropertyField } from '../ColumnDataTypeField'
 import { ColumnRenderComponent } from '../types'
 import { IDateColumnProps } from './types'
-import { formatDate } from 'pp365-shared-library'
-import { IColumnDataTypePropertyField } from '../ColumnDataTypeField'
-import { ColumnRenderComponentRegistry } from '../registry'
 
 export const DateColumn: ColumnRenderComponent<IDateColumnProps> = (props) => {
   return <span>{formatDate(props.columnValue, props.includeTime)}</span>
@@ -15,16 +14,13 @@ DateColumn.key = 'date'
 DateColumn.id = 'Date'
 DateColumn.displayName = strings.ColumnRenderOptionDate
 DateColumn.iconName = 'Calendar'
-ColumnRenderComponentRegistry.register(
-  DateColumn,
-  (onChange, dataTypeProperties: Record<string, any>) => [
+DateColumn.getDataTypeProperties = (onChange, dataTypeProperties: Record<string, any>) => [
+  ColumnDataTypePropertyField(
+    Toggle,
     {
-      type: Toggle,
-      props: {
-        label: strings.ColumnRenderOptionDateIncludeTimeLabel,
-        checked: dataTypeProperties.includeTime ?? false,
-        onChange: (_, checked) => onChange('includeTime', checked)
-      }
-    } as IColumnDataTypePropertyField<IToggleProps>
-  ]
-)
+      label: strings.ColumnRenderOptionDateIncludeTimeLabel,
+      checked: dataTypeProperties.includeTime ?? false,
+      onChange: (_, checked) => onChange('includeTime', checked)
+    }
+  )
+]
