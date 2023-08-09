3be6530e8f1ae0a327480c13435958cbe02d2022
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-08-09 11:32:15 +0200

    Added component FieldDescription

---
+++ SharePointFramework/shared-library/src/components/FormFieldContainer/FieldDescription/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react'
+import styles from './FieldDescription.module.scss'
+
+export const FieldDescription: FC<{ description: string }> = ({ description }) => {
+    return description && <div className={styles.root}>{description}</div>
+}
