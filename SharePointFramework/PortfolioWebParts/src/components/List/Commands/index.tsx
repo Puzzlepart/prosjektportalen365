8c38659a814c49ca8fd7c7734841eb59c7d68dac
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-08-29 13:07:39 +0200

    Refaktorering av Commands for PortfolioOverview/PortfolioAggregation

--- SharePointFramework/PortfolioWebParts/src/components/List/Commands/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/List/Commands/index.tsx
@@ -1,14 +1,22 @@
-import { CommandBar } from '@fluentui/react'
+import { Toolbar, ToolbarButton } from '@fluentui/react-components'
+import { ArrowExportUp24Regular, Filter24Regular, ViewDesktop24Regular } from '@fluentui/react-icons'
+import strings from 'PortfolioWebPartsStrings'
+import { FilterPanel } from 'pp365-shared-library'
 import React, { FC } from 'react'
 import { useListContext } from '../context'
-import { FilterPanel } from 'pp365-shared-library'
-import strings from 'PortfolioWebPartsStrings'
 
 export const Commands: FC = () => {
   const context = useListContext()
   return (
     <div>
-      <CommandBar {...context.props.commandBarProps} />
+      <Toolbar aria-label='Default'>
+
+        <ToolbarButton icon={<ArrowExportUp24Regular />} />
+        <ToolbarButton icon={<ViewDesktop24Regular />}>
+          Alle prosjekter
+        </ToolbarButton>
+        <ToolbarButton icon={<Filter24Regular />} />
+      </Toolbar>
       <FilterPanel
         {...context.props.filterPanelProps}
         headerText={strings.FiltersString}
