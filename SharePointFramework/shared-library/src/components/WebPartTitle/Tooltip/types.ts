132338634da8f18783f483422373db2a251ca8cf
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-08-29 10:42:35 +0200

    WebPartTitle supports tooltip

--- SharePointFramework/shared-library/src/components/WebPartTitle/Tooltip/types.ts
+++ SharePointFramework/shared-library/src/components/WebPartTitle/Tooltip/types.ts
@@ -5,13 +5,13 @@ import { HTMLAttributes } from 'react'
  * Props for the Tooltip component.
  */
 export interface ITooltipProps extends HTMLAttributes<HTMLDivElement> {
-    /**
-     * The text to display in the tooltip.
-     */
-    text: string
-    
-    /**
-     * Props for the icon displayed next to the tooltip.
-     */
-    iconProps?: IIconProps
+  /**
+   * The text to display in the tooltip.
+   */
+  text: string
+
+  /**
+   * Props for the icon displayed next to the tooltip.
+   */
+  iconProps?: IIconProps
 }
--- SharePointFramework/shared-library/src/components/WebPartTitle/index.tsx
+++ SharePointFramework/shared-library/src/components/WebPartTitle/index.tsx
@@ -5,17 +5,17 @@ import { Tooltip } from './Tooltip'
 
 /**
  * Renders a web part title with an optional tooltip.
- * 
+ *
  * @param props - The component props.
  * @param props.title - The title to display.
  * @param props.tooltip - The tooltip to display when hovering over the title.
- * 
+ *
  * @returns The rendered component.
  */
 export const WebPartTitle: FC<IWebPartTitleProps> = (props) => {
   return (
-    <div className={styles.header} title={props.title}>
-      <Tooltip  {...props.tooltip}>
+    <div className={styles.root} title={props.title}>
+      <Tooltip {...props.tooltip}>
         <h2 className={styles.heading}>
           <span role='heading' aria-level={2} className={styles.title}>
             {props.title}
