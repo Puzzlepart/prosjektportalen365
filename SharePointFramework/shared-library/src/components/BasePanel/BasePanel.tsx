1c61a0cd4d75b8fd41fc5d27eac57620ae0fec2b
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-10-09 11:42:47 +0200

    BasePanel in shared

---
+++ SharePointFramework/shared-library/src/components/BasePanel/BasePanel.tsx
@@ -0,0 +1,29 @@
+import { IPanelProps, Panel, PanelType } from '@fluentui/react'
+import { FluentProvider, webLightTheme } from '@fluentui/react-components'
+import React, { FC } from 'react'
+import styles from './BasePanel.module.scss'
+import { IBasePanelProps } from './types'
+
+export const BasePanel: FC<IBasePanelProps> = (props) => {
+  return (
+    <Panel
+      {...props}
+      onRenderBody={() => {
+        if (!props.onRenderBody) return null
+        return (
+          <FluentProvider theme={webLightTheme} className={styles.root}>
+            {props.onRenderBody()}
+          </FluentProvider>
+        )
+      }}
+    >
+      {props.children}
+    </Panel>
+  )
+}
+
+BasePanel.defaultProps = {
+  type: PanelType.medium,
+  isLightDismiss: true,
+  closeButtonAriaLabel: 'strings.CloseText'
+}
