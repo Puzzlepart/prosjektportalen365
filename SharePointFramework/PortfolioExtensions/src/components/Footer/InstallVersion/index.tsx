e2580df2fc57277b5d026fb4d080fad4b8c6c19f
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-10-30 16:34:53 +0100

    Add tooltip to version button

--- SharePointFramework/PortfolioExtensions/src/components/Footer/InstallVersion/index.tsx
+++ SharePointFramework/PortfolioExtensions/src/components/Footer/InstallVersion/index.tsx
@@ -1,17 +1,24 @@
 import React, { FC, useContext } from 'react'
 import { FooterContext } from '../context'
 import { InstallVersionTooltipContent } from './InstallVersionTooltipContent'
-import { Button, Popover, PopoverSurface, PopoverTrigger } from '@fluentui/react-components'
+import { Button, Popover, PopoverSurface, PopoverTrigger, Tooltip } from '@fluentui/react-components'
 import { getFluentIcon } from 'pp365-shared-library'
+import strings from 'PortfolioExtensionsStrings'
 
 export const InstallVersion: FC = () => {
   const context = useContext(FooterContext)
   return (
     <Popover withArrow positioning='above-start'>
       <PopoverTrigger disableButtonEnhancement>
-        <Button size='small' appearance='subtle' icon={getFluentIcon('BoxToolbox')}>
-          {context.installedVersion}
-        </Button>
+        <Tooltip
+          relationship='description'
+          withArrow
+          content={strings.LastInstallDescription}
+        >
+          <Button size='small' appearance='subtle' icon={getFluentIcon('BoxToolbox')}>
+            {context.installedVersion}
+          </Button>
+        </Tooltip>
       </PopoverTrigger>
       <PopoverSurface>
         <InstallVersionTooltipContent />
