0f41dd1593a31e6ddff49c1bb2c2e53eb08e0695
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-11-01 14:52:35 +0100

    Fix issues with FluentProvider - Footer

--- SharePointFramework/PortfolioExtensions/src/components/Footer/Footer.tsx
+++ SharePointFramework/PortfolioExtensions/src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ import { IFooterProps } from './types'
 import { useFooter } from './useFooter'
 import { SiteSettings } from './SiteSettings'
 import { HelpContent } from './HelpContent'
-import { FluentProvider, useId, webLightTheme } from '@fluentui/react-components'
+import { FluentProvider, IdPrefixProvider, useId, webLightTheme } from '@fluentui/react-components'
 
 export const Footer: FC<IFooterProps> = (props) => {
   const fluentProviderId = useId('fp-footer')
@@ -16,19 +16,21 @@ export const Footer: FC<IFooterProps> = (props) => {
 
   return (
     <FooterContext.Provider value={{ ...footer, props }}>
-      <FluentProvider id={fluentProviderId} theme={webLightTheme}>
-        <div className={styles.footer}>
-          <section className={styles.left}>
-            <SiteSettings />
-            <Configuration />
-            <PromotedLinks />
-          </section>
-          <section className={styles.right}>
-            <InstallVersion />
-            <HelpContent />
-          </section>
-        </div>
-      </FluentProvider>
+      <IdPrefixProvider value={fluentProviderId}>
+        <FluentProvider theme={webLightTheme}>
+          <div className={styles.footer}>
+            <section className={styles.left}>
+              <SiteSettings />
+              <Configuration />
+              <PromotedLinks />
+            </section>
+            <section className={styles.right}>
+              <InstallVersion />
+              <HelpContent />
+            </section>
+          </div>
+        </FluentProvider>
+      </IdPrefixProvider >
     </FooterContext.Provider>
   )
 }
