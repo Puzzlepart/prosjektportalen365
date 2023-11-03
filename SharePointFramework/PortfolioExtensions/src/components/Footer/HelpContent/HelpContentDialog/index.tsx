bb4edba31d806ab1a07d738908710f953067399b
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-11-03 15:54:21 +0100

    Finish helpContent

--- SharePointFramework/PortfolioExtensions/src/components/Footer/HelpContent/HelpContentDialog/index.tsx
+++ SharePointFramework/PortfolioExtensions/src/components/Footer/HelpContent/HelpContentDialog/index.tsx
@@ -1,11 +1,8 @@
 import { Pivot, PivotItem } from '@fluentui/react'
 import React, { FC, ReactElement, useContext } from 'react'
-import ReactMarkdown from 'react-markdown'
-import rehypeRaw from 'rehype-raw'
 import styles from './HelpContentDialog.module.scss'
 import { FooterContext } from 'components/Footer/context'
 import {
-  Button,
   Dialog,
   DialogBody,
   DialogContent,
@@ -17,6 +14,7 @@ import {
   useId,
   webLightTheme
 } from '@fluentui/react-components'
+import { Content } from './Content'
 
 export const HelpContentDialog: FC<Omit<DialogProps, 'children'>> = (props) => {
   const fluentProviderId = useId('fp-helpDialog')
@@ -27,42 +25,16 @@ export const HelpContentDialog: FC<Omit<DialogProps, 'children'>> = (props) => {
       <FluentProvider theme={webLightTheme} className={styles.helpContentDialog}>
         <Dialog open={props.open}>
           <DialogTrigger disableButtonEnhancement>{props.children as ReactElement}</DialogTrigger>
-          <DialogSurface style={{ maxWidth: '800px !important' }}>
-            <DialogBody className={styles.body}>
+          <DialogSurface>
+            <DialogBody>
               <DialogContent className={styles.content}>
                 <Pivot>
                   {context.props.helpContent.map((content, index) => (
-                    <PivotItem
-                      key={index}
-                      headerText={content.title}
-                      itemIcon={content.iconName}
-                      style={{
-                        overflow: 'auto',
-                        height: 'calc(100vh - 250px)',
-                        width: 'calc(100vw - 250px)'
-                      }}
-                    >
-                      <div className={styles.contentItem} title={content.title}>
-                        <p dangerouslySetInnerHTML={{ __html: content.textContent }}></p>
-                        {content.markdownContent && (
-                          <ReactMarkdown
-                            linkTarget='_blank'
-                            rehypePlugins={[rehypeRaw]}
-                            transformImageUri={null}
-                          >
-                            {content.markdownContent}
-                          </ReactMarkdown>
-                        )}
-                        {content.resourceLink && (
-                          <Button
-                            size='medium'
-                            appearance='primary'
-                            onClick={() => window.open(content.resourceLink.Url, '_blank')}
-                          >
-                            <span>{content.resourceLink.Description}</span>
-                          </Button>
-                        )}
-                      </div>
+                    <PivotItem key={index} headerText={content.title} style={{
+                      overflow: 'auto',
+                      height: 'calc(100vh - 285px)'
+                    }}>
+                      <Content content={content} />
                     </PivotItem>
                   ))}
                 </Pivot>
