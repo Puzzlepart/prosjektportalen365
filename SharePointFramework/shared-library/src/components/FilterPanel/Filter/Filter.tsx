32e411d85b36db177b3deccab722bb5d23d8a874
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-11-06 15:39:21 +0100

    Visual overhaul - FilterPanel

--- SharePointFramework/shared-library/src/components/FilterPanel/Filter/Filter.tsx
+++ SharePointFramework/shared-library/src/components/FilterPanel/Filter/Filter.tsx
@@ -1,22 +1,20 @@
-import { Icon } from '@fluentui/react/lib/Icon'
 import React, { FC } from 'react'
 import styles from './Filter.module.scss'
 import { IFilterProps } from './types'
 import { useFilter } from './useFilter'
+import { ChevronDown16Filled, ChevronUp16Filled } from '@fluentui/react-icons'
 
 export const Filter: FC<IFilterProps> = (props) => {
   const { state, onToggleSectionContent, renderItems } = useFilter(props)
 
   return (
     <div className={styles.filter}>
-      <div className={styles.filterSectionHeader} onClick={onToggleSectionContent}>
-        <span className={styles.titleText}>{props.column.name}</span>
-        <span className={styles.titleIcon}>
-          <Icon iconName={state.isCollapsed ? 'ChevronUp' : 'ChevronDown'} />
-        </span>
+      <div className={styles.sectionHeader} onClick={onToggleSectionContent}>
+        <span>{props.column.name}</span>
+        {state.isCollapsed ? <ChevronUp16Filled /> : <ChevronDown16Filled />}
       </div>
       <div hidden={state.isCollapsed}>
-        <ul className={styles.filterSectionContent}>{renderItems()}</ul>
+        <div className={styles.sectionContent}>{renderItems()}</div>
       </div>
     </div>
   )
