7bc62f97abe19676d93e7b0640e8b944e2bfb61b
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-11-06 08:57:25 +0100

    FilterPanel

--- SharePointFramework/shared-library/src/components/FilterPanel/Filter/Filter.tsx
+++ SharePointFramework/shared-library/src/components/FilterPanel/Filter/Filter.tsx
@@ -1,74 +1,23 @@
-import { Icon } from '@fluentui/react/lib/Icon';
-import * as React from 'react';
-import { Component } from 'react';
-import { FilterItem } from '../FilterItem/FilterItem';
-import { IFilterItemProps } from '../FilterItem/types';
-import styles from './Filter.module.scss';
-import { IFilterProps, IFilterState } from './types';
+import { Icon } from '@fluentui/react/lib/Icon'
+import React, { FC } from 'react'
+import styles from './Filter.module.scss'
+import { IFilterProps } from './types'
+import { useFilter } from './useFilter'
 
+export const Filter: FC<IFilterProps> = (props) => {
+  const { state, onToggleSectionContent, renderItems } = useFilter(props)
 
-export class Filter extends Component<IFilterProps, IFilterState> {
-  constructor(props: IFilterProps) {
-    super(props);
-    this.state = { isCollapsed: props.defaultCollapsed, items: props.items };
-  }
-
-  public render(): React.ReactElement<IFilterProps> {
-    return (
-      <div className={styles.root}>
-        <div className={styles.filterSectionHeader} onClick={this._onToggleSectionContent}>
-          <span className={styles.titleText}>{this.props.column.name}</span>
-          <span className={styles.titleIcon}>
-            <Icon iconName={this.state.isCollapsed ? 'ChevronUp' : 'ChevronDown'} />
-          </span>
-        </div>
-        <div hidden={this.state.isCollapsed}>
-          <ul className={styles.filterSectionContent}>{this._renderItems()}</ul>
-        </div>
+  return (
+    <div className={styles.filter}>
+      <div className={styles.filterSectionHeader} onClick={onToggleSectionContent}>
+        <span className={styles.titleText}>{props.column.name}</span>
+        <span className={styles.titleIcon}>
+          <Icon iconName={state.isCollapsed ? 'ChevronUp' : 'ChevronDown'} />
+        </span>
       </div>
-    );
-  }
-
-  /**
-   * On toggle section content
-   */
-  private _onToggleSectionContent = () => {
-    this.setState((prevState: IFilterState) => ({ isCollapsed: !prevState.isCollapsed }));
-  };
-
-  /**
-   * Render filter items
-   */
-  private _renderItems() {
-    return this.state.items.map((props, idx) => (
-      <FilterItem
-        key={idx}
-        {...props}
-        onChanged={(_event, checked) => this._onChanged(props, checked)} />
-    ));
-  }
-
-  /**
-   * On changed
-   *
-   * @param item Item that was changed
-   * @param checked Item checked
-   */
-  private _onChanged = (item: IFilterItemProps, checked: boolean) => {
-    this.setState(
-      (prevState) => {
-        const items = prevState.items.map((i) => {
-          if (i.value === item.value) {
-            return { ...i, selected: checked };
-          }
-          return i;
-        });
-        return { items };
-      },
-      () => {
-        const selectedItems = this.state.items.filter((i) => i.selected);
-        this.props.onFilterChange(this.props.column, selectedItems);
-      }
-    );
-  };
+      <div hidden={state.isCollapsed}>
+        <ul className={styles.filterSectionContent}>{renderItems()}</ul>
+      </div>
+    </div>
+  )
 }
---
+++ SharePointFramework/shared-library/src/components/FilterPanel/Filter/useFilter.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { FilterItem } from '../FilterItem/FilterItem'
+import { IFilterItemProps } from '../FilterItem/types'
+import { IFilterProps, IFilterState } from './types'
+
+export function useFilter(props: IFilterProps) {
+  const [state, setState] = useState<IFilterState>({ isCollapsed: props.defaultCollapsed, items: props.items })
+
+  /**
+   * On change filter item
+   *
+   * @param item Item that was changed
+   * @param checked Item checked
+   */
+  const onChange = (item: IFilterItemProps, checked: boolean) => {
+    setState(
+      (prevState) => {
+        const items = prevState.items.map((i) => {
+          if (i.value === item.value) {
+            return { ...i, selected: checked }
+          }
+          return i
+        })
+        return { ...prevState, items }
+      }
+    )
+    const selectedItems = state.items.filter(({ selected }) => selected)
+    props.onFilterChange(props.column, selectedItems)
+  }
+
+  /**
+   * On toggle section content
+   */
+  const onToggleSectionContent = () => {
+    setState((prevState) => ({ ...prevState, isCollapsed: !prevState.isCollapsed }))
+  }
+
+  /**
+   * Render filter items
+   */
+  const renderItems = () => {
+    return state.items.map((props, idx) => (
+      <FilterItem
+        key={idx}
+        {...props}
+        onChange={(_event, { checked }) => onChange(props, checked as boolean)} />
+    ))
+  }
+
+  return { state, onToggleSectionContent, renderItems }
+}
