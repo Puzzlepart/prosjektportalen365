2d00c067344d487d3f016905843e632918c1efe7
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2023-11-06 14:20:21 +0100

    ProjectWebParts

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/UserMessages/UserMessages.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/UserMessages/UserMessages.tsx
@@ -1,16 +1,16 @@
-import { Shimmer } from '@fluentui/react'
 import { UserMessage } from 'pp365-shared-library/lib/components/UserMessage'
 import React, { FC } from 'react'
 import { useProjectStatusContext } from '../context'
 import styles from './UserMessages.module.scss'
+import { LoadingSkeleton } from 'pp365-shared-library'
 
 export const UserMessages: FC = () => {
   const context = useProjectStatusContext()
-  return (
-    <Shimmer isDataLoaded={context.state.isDataLoaded}>
-      <div className={styles.userMessages}>
-        {context.state.userMessage && <UserMessage {...context.state.userMessage} />}
-      </div>
-    </Shimmer>
+  return context.state.isDataLoaded ? (
+    <div className={styles.userMessages}>
+      {context.state.userMessage && <UserMessage {...context.state.userMessage} />}
+    </div>
+  ) : (
+    <LoadingSkeleton />
   )
 }
