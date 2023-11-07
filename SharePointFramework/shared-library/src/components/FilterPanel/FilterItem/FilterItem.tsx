b8eb156ff05d995855fde0166d0891c2372dcb37
Bloom <remi.blom-ohlsen@puzzlepart.com>
2023-11-07 08:44:13 +0100

    Visuelt løft - Filterpanel (#1299)
    * Visual overhaul - FilterPanel
    
    * Minor change
    
    * Use Icon component from shared

--- SharePointFramework/shared-library/src/components/FilterPanel/FilterItem/FilterItem.tsx
+++ SharePointFramework/shared-library/src/components/FilterPanel/FilterItem/FilterItem.tsx
@@ -5,10 +5,11 @@ import { Checkbox } from '@fluentui/react-components'
 
 export const FilterItem: FC<IFilterItemProps> = (props) => {
   return (
-    <li>
-      <div className={styles.filterItem}>
-        <Checkbox label={props.name} checked={props.selected} onChange={props.onChange} />
-      </div>
-    </li>
+    <Checkbox
+      className={styles.filterItem}
+      label={props.name}
+      checked={props.selected}
+      onChange={props.onChange}
+    />
   )
 }
--- SharePointFramework/shared-library/src/components/FilterPanel/FilterPanel.tsx
+++ SharePointFramework/shared-library/src/components/FilterPanel/FilterPanel.tsx
@@ -2,17 +2,25 @@ import { Panel, PanelType } from '@fluentui/react'
 import React, { FC } from 'react'
 import { Filter } from './Filter/Filter'
 import { IFilterPanelProps } from './types'
+import styles from './FilterPanel.module.scss'
+import { useId, IdPrefixProvider, FluentProvider, webLightTheme } from '@fluentui/react-components'
 
 export const FilterPanel: FC<IFilterPanelProps> = (props) => {
+  const fluentProviderId = useId('fp-filterPanel')
+
   return (
     <Panel {...props} type={PanelType.smallFixedFar}>
-      <div>
-        {props.filters
-          .filter((f) => f.items.length > 1)
-          .map((f, idx) => (
-            <Filter {...f} key={idx} onFilterChange={props.onFilterChange} />
-          ))}
-      </div>
+      <IdPrefixProvider value={fluentProviderId}>
+        <FluentProvider theme={webLightTheme}>
+          <div className={styles.filterPanel}>
+            {props.filters
+              .filter((f) => f.items.length > 1)
+              .map((f, idx) => (
+                <Filter {...f} key={idx} onFilterChange={props.onFilterChange} />
+              ))}
+          </div>
+        </FluentProvider>
+      </IdPrefixProvider>
     </Panel>
   )
 }
