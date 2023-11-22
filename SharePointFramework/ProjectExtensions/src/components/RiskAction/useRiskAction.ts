7d670d8bf1badc2f204d70047dd1e2a4dcfb5d53
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-11-22 14:28:24 +0100

    useRiskAction

--- SharePointFramework/ProjectExtensions/src/components/RiskAction/useRiskAction.ts
+++ SharePointFramework/ProjectExtensions/src/components/RiskAction/useRiskAction.ts
@@ -2,6 +2,11 @@ import { useId } from '@fluentui/react-components'
 import { useMemo, useState } from 'react'
 import { useRiskActionFieldCustomizerContext } from '../../riskAction/context'
 
+/**
+ * Custom hook for managing risk action functionality.
+ * 
+ * @returns An object containing the fluentProviderId and contextValue.
+ */
 export function useRiskAction() {
   const fluentProviderId = useId('risk-action-fluent-provider')
   const context = useRiskActionFieldCustomizerContext()
