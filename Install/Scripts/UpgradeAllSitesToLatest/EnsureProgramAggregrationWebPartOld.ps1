b294eeef472e51285c24334023d6060156498f22
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2024-01-17 09:15:07 +0100

    CI [test]

--- Install/Scripts/UpgradeAllSitesToLatest/EnsureProgramAggregrationWebPartOld.ps1
+++ Install/Scripts/UpgradeAllSitesToLatest/EnsureProgramAggregrationWebPartOld.ps1
@@ -7,7 +7,7 @@ if ($global:__InstalledVersion -lt $TargetVersion) {
     foreach ($Page in $Pages.PSObject.Properties.GetEnumerator()) {
         $DeprecatedComponent = Get-PnPPageComponent -Page "$($Page.Name).aspx" -ErrorAction SilentlyContinue | Where-Object { $_.WebPartId -eq $Page.Value } | Select-Object -First 1
         if ($null -ne $DeprecatedComponent) {
-            Write-Host "`t`tReplacing deprecated component $($Page.Value) for $($Page.Name).aspx"
+            Write-Host "`t`tReplacing older deprecated component $($Page.Value) for $($Page.Name).aspx"
             $JsonControlData = Get-Content "$BaseDir/JsonControlData_$($Page.Name).json" -Raw -Encoding UTF8
             $Title = $JsonControlData | ConvertFrom-Json | Select-Object -ExpandProperty title
             Invoke-PnPSiteTemplate -Path "$BaseDir/Template_ProgramAggregationWebPart.xml" -Parameters @{"JsonControlData" = $JsonControlData; "PageName" = "$($Page.Name).aspx"; "Title" = $Title }
