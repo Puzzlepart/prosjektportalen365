eb1972248b0f934882ee28f6a2f436ec69dd7bb9
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2024-02-07 14:43:13 +0100

    Adjustments to UpgradeAllSitesToLatest version check

--- Install/Scripts/UpgradeAllSitesToLatest/EnsureRiskActionPlanner.ps1
+++ Install/Scripts/UpgradeAllSitesToLatest/EnsureRiskActionPlanner.ps1
@@ -1,6 +1,6 @@
 $TargetVersion = "1.9.0"
 
-if ($global:__InstalledVersion -lt $TargetVersion) {
+if ($global:__PreviousVersion -lt $TargetVersion) {
     [System.Guid]$ClientSideComponentId = "1dd9fdb3-df0f-4248-a869-ca6f512e3d0f"
     if ($null -ne $global:__CurrentChannelConfig) {
         $ClientSideComponentId = $global:__CurrentChannelConfig.spfx.solutions.ProjectExtensions.components.RiskActionPlanner
