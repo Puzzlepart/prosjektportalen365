1ee9b977473915e87564b9915f5a258310f62756
Stian Grepperud <stian.grepperud@puzzlepart.com>
2024-04-23 16:09:12 +0200

    Workaround to safely map ModernTaxonomyPicker initial values without affecting other components

--- SharePointFramework/shared-library/src/components/CustomEditPanel/CustomEditPanelBody/FieldElements/TaxonomyFieldTypeMulti.tsx
+++ SharePointFramework/shared-library/src/components/CustomEditPanel/CustomEditPanelBody/FieldElements/TaxonomyFieldTypeMulti.tsx
@@ -1,11 +1,20 @@
-import { ModernTaxonomyPicker } from "@pnp/spfx-controls-react/lib/ModernTaxonomyPicker";
+import { ModernTaxonomyPicker } from '@pnp/spfx-controls-react/lib/ModernTaxonomyPicker'
 import React from 'react'
 import { FieldContainer } from '../../../FieldContainer'
 import { useCustomEditPanelContext } from '../../context'
-import { FieldElementComponent } from './types'
+import { FieldElementComponent, Term } from './types'
+
+const mapInitialValues = (term: Term) => {
+  if ('id' in term) {
+    return {labels: term.labels, id: term.id}
+  } else {
+    return {labels: [{name: term.name, isDefault: true, languageTag: 'nb-NO'}], id: term.key}
+  }
+}
 
 export const TaxonomyFieldTypeMulti: FieldElementComponent = ({ field }) => {
   const context = useCustomEditPanelContext()
+  const terms = context.model.get<Term[]>(field)
 
   return (
     <FieldContainer
@@ -17,6 +26,7 @@ export const TaxonomyFieldTypeMulti: FieldElementComponent = ({ field }) => {
       <ModernTaxonomyPicker
         context={context.props.dataAdapter.spfxContext as any} // Newest version of the control requires this cast for now, as context type is incompatibale with other types of SPFxContext
         panelTitle={field.description || field.displayName}
+        initialValues={terms?.map(mapInitialValues)}
         allowMultipleSelections
         label=''
         termSetId={field.getProperty('TermSetId')}
--- SharePointFramework/shared-library/src/components/CustomEditPanel/CustomEditPanelBody/FieldElements/types.ts
+++ SharePointFramework/shared-library/src/components/CustomEditPanel/CustomEditPanelBody/FieldElements/types.ts
@@ -5,4 +5,22 @@ interface IFieldElementProps {
   field: EditableSPField
 }
 
+interface ITermLabel {
+  name: string;
+  isDefault: boolean;
+  languageTag: string;
+}
+
+interface ITagTerm {
+  key: string;
+  name: string;
+}
+
+interface IModernTaxonomyTerm {
+  id: string;
+  labels: ITermLabel[];
+}
+
+export type Term = ITagTerm | IModernTaxonomyTerm;
+
 export type FieldElementComponent = FC<IFieldElementProps>
