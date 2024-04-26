a1075956c949e4ee3ef6ccd8ca748f5f56bffa7f
Stian Grepperud <stian.grepperud@puzzlepart.com>
2024-04-26 11:50:53 +0200

    Link to program timeline of project is program

--- SharePointFramework/shared-library/src/components/ProjectTimeline/Timeline/useGroupRenderer.tsx
+++ SharePointFramework/shared-library/src/components/ProjectTimeline/Timeline/useGroupRenderer.tsx
@@ -10,13 +10,14 @@ import strings from 'SharedLibraryStrings'
 export function useGroupRenderer() {
   return ({ group }: ReactCalendarGroupRendererProps<ITimelineGroup>) => {
     const style: React.CSSProperties = { display: 'block', width: '100%' }
+    const page = group.isProgram ? 'Programtidslinje' : 'Prosjekttidslinje'
 
     return (
       <div style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
         {group.type === TimelineGroupType.Project ? (
           <Tooltip content={strings.TimelineGroupDescription} relationship='description' withArrow>
             <Link
-              href={`${group.path}/SitePages/Prosjekttidslinje.aspx`}
+              href={`${group.path}/SitePages/${page}.aspx`}
               target='_blank'
               title={group.title}
             >
