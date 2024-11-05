6593946310666c28e158b25fc8705a456f343027
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2024-11-05 17:14:33 +0100

    Fix sorting columns for Program Aggregation webparts [packages-only]

--- SharePointFramework/ProgramWebParts/src/webparts/programAggregation/index.tsx
+++ SharePointFramework/ProgramWebParts/src/webparts/programAggregation/index.tsx
@@ -4,46 +4,69 @@ import {
   PropertyPaneDropdown,
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane'
-import strings from 'ProgramWebPartsStrings'
+import { DisplayMode } from '@microsoft/sp-core-library'
+import { ISPHttpClientOptions, SPHttpClient } from '@microsoft/sp-http'
+import * as strings from 'ProgramWebPartsStrings'
 import _ from 'lodash'
 import {
   IPortfolioAggregationConfiguration,
   IPortfolioAggregationProps,
   PortfolioAggregation
 } from 'pp365-portfoliowebparts/lib/components/PortfolioAggregation'
-import { BaseProgramWebPart } from '../baseProgramWebPart'
 import { IProgramAggregationWebPartProps } from './types'
+import { BaseProgramWebPart } from '../baseProgramWebPart'
 
 export default class ProgramAggregationWebPart extends BaseProgramWebPart<IProgramAggregationWebPartProps> {
   private _configuration: IPortfolioAggregationConfiguration
 
   public render(): void {
     this.renderComponent<IPortfolioAggregationProps>(PortfolioAggregation, {
-      onUpdateProperty: this._onUpdateProperty.bind(this),
       configuration: this._configuration,
+      onUpdateProperty: this._onUpdateProperty.bind(this),
       isParentProject: true,
       spfxContext: this.context
     })
   }
 
   /**
-   * On update property. Sets `key` to `value` and refreshes the property pane
-   * using `this.context.propertyPane.refresh()`
+   * On update property. For `DisplayMode.Edit` it refreshes the property pane,
+   * for `DisplayMode.Read` it updates the page using the `_api/SitePages/Pages/UpdateAppPage`
+   * endpoint.
    *
    * @param key Key
    * @param value Value
    */
-  private _onUpdateProperty(key: string, value: any) {
+  private async _onUpdateProperty(key: string, value: any) {
     this.properties[key] = value
-    this.context.propertyPane.refresh()
+    switch (this.displayMode) {
+      case DisplayMode.Edit:
+        {
+          this.context.propertyPane.refresh()
+        }
+        break
+      case DisplayMode.Read: {
+        const options: ISPHttpClientOptions = {
+          body: JSON.stringify({
+            includeInNavigation: false,
+            pageId: this.context.pageContext.listItem.id,
+            title: this.properties.title,
+            webPartDataAsJson: JSON.stringify({
+              id: this.context.manifest.id,
+              instanceId: this.context.instanceId,
+              title: this.properties.title,
+              properties: this.properties
+            })
+          })
+        }
+        await this.context.spHttpClient.post(
+          `${this.context.pageContext.web.absoluteUrl}/_api/SitePages/Pages/UpdateAppPage`,
+          SPHttpClient.configurations.v1,
+          options
+        )
+      }
+    }
   }
 
-  /**
-   * Initializes the web part. Overrides the base `onInit` method to retrieve the configuration
-   * for the aggregated list from the data adapter.
-   *
-   * @returns A promise that resolves when the initialization is complete.
-   */
   public async onInit(): Promise<void> {
     await super.onInit()
     this._configuration = await this._dataAdapter.getAggregatedListConfig(
@@ -53,7 +76,7 @@ export default class ProgramAggregationWebPart extends BaseProgramWebPart<IProgr
   }
 
   /**
-   * Get view options for the property pane dropdown 'defaultViewId'.
+   * Get view options for the property pane dropdown field `defaultViewId`.
    *
    * @param configuration Configuration
    */
