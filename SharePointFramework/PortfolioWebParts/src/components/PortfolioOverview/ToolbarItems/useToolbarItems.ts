5a53f132d0a95755679053092cf68d9cad646a32
Ole Martin Pettersen <olemp@puzzlepart.com>
2025-01-17 14:15:49 +0100

    Toolbar

--- SharePointFramework/PortfolioWebParts/src/components/PortfolioOverview/ToolbarItems/useToolbarItems.ts
+++ SharePointFramework/PortfolioWebParts/src/components/PortfolioOverview/ToolbarItems/useToolbarItems.ts
@@ -1,39 +1,14 @@
-import {
-  AppsListFilled,
-  AppsListRegular,
-  ChevronLeftFilled,
-  ChevronLeftRegular,
-  ContentView24Filled,
-  ContentView24Regular,
-  EditFilled,
-  EditRegular,
-  FormNewFilled,
-  FormNewRegular,
-  TextBulletListLtrFilled,
-  TextBulletListLtrRegular,
-  bundleIcon
-} from '@fluentui/react-icons'
 import strings from 'PortfolioWebPartsStrings'
 import { ListMenuItem, ListMenuItemDivider, ListMenuItemHeader } from 'pp365-shared-library'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import _ from 'underscore'
 import { IPortfolioOverviewContext } from '../context'
 import { SET_VIEW_FORM_PANEL, TOGGLE_COMPACT, TOGGLE_FILTER_PANEL } from '../reducer'
+import { Icons } from './icons'
 import { useExcelExport } from './useExcelExport'
 import { useProgramMenuItems } from './useProgramMenuItems'
 import { useViewsMenuItems } from './useViewsMenuItems'
 
-/**
- * Object containing icons used in the toolbar.
- */
-const Icons = {
-  ContentView: bundleIcon(ContentView24Filled, ContentView24Regular),
-  AppsList: bundleIcon(AppsListFilled, AppsListRegular),
-  TextBulletList: bundleIcon(TextBulletListLtrFilled, TextBulletListLtrRegular),
-  ChevronLeft: bundleIcon(ChevronLeftFilled, ChevronLeftRegular),
-  FormNew: bundleIcon(FormNewFilled, FormNewRegular),
-  Edit: bundleIcon(EditFilled, EditRegular)
-}
 
 /**
  * Returns an array of menu items for the toolbar in the PortfolioOverview component.
@@ -43,32 +18,50 @@ const Icons = {
  * @returns An array of `IListMenuItem` objects representing the toolbar items.
  */
 export function useToolbarItems(context: IPortfolioOverviewContext) {
+  const [selectedPortfolioId, setSelectedPortfolioId] = useState(context.props.selectedPortfolioId)
   const userCanManageViews = context.props.configuration.userCanAddViews
   const userCanEditGlobalViews = userCanManageViews && context.props.isSiteAdmin
   const userEmail = context.props.pageContext.user.email ?? context.props.pageContext.user.loginName
   const userCanEditView = userCanManageViews && context.state.currentView?.author === userEmail
-  const { exportToExcel } = useExcelExport(context)
+  const exportToExcel = useExcelExport(context)
   const sharedViews = useViewsMenuItems(context, (view) => !view.isPersonal)
   const personalViews = useViewsMenuItems(context, (view) => view.isPersonal)
   const programViews = useProgramMenuItems(context)
-  const checkedValues = useMemo(
-    () => ({
-      views: [context.state.currentView?.id.toString()],
-      renderMode: context.state.isCompact ? ['compactList'] : ['list']
-    }),
-    [context.state.currentView?.id, context.state.isCompact]
-  )
 
   return useMemo<ListMenuItem[]>(
     () =>
       [
         context.props.showExcelExportButton &&
-          new ListMenuItem(null, strings.ExcelExportButtonLabel)
-            .setIcon('ExcelLogoInverse')
-            .setOnClick(exportToExcel)
-            .setStyle({
-              color: '#10793F'
-            }),
+        new ListMenuItem(null, strings.ExcelExportButtonLabel)
+          .setIcon('ExcelLogoInverse')
+          .setOnClick(exportToExcel)
+          .setStyle({
+            color: '#10793F'
+          }),
+        new ListMenuItem(context.props.portfolios.find(({ uniqueId }) => selectedPortfolioId === uniqueId)?.title ?? strings.SelectedPortfolioLabel, strings.PortfolioSelectorDescription)
+          .setIcon(Icons.Collections)
+          .setWidth('fit-content')
+          .setStyle({
+            minWidth: '145px',
+            display: (context.props.showPortfolioSelector && context.props.portfolios?.length > 1) ? 'flex' : 'none'
+          })
+          .setDisabled(context.state.isChangingView)
+          .setItems(
+            context.props.portfolios.map<ListMenuItem>((v) =>
+              new ListMenuItem(v.title)
+                .makeCheckable({
+                  name: 'portfolios',
+                  value: v.uniqueId.toString()
+                })
+                .setOnClick(() => {
+                  setSelectedPortfolioId(v.uniqueId)
+                  context.props.onSetPortfolio(v.uniqueId)
+                })
+            ),
+            {
+              portfolios: [selectedPortfolioId]
+            }
+          ),
         new ListMenuItem(context.state.currentView?.title, strings.PortfolioViewsListName)
           .setIcon(Icons.ContentView)
           .setWidth('fit-content')
@@ -106,10 +99,10 @@ export function useToolbarItems(context: IPortfolioOverviewContext) {
               ...personalViews,
               context.props.showProgramViews && ListMenuItemDivider,
               context.props.showProgramViews &&
-                ListMenuItemHeader(strings.ProgramsHeaderText).setStyle({
-                  display:
-                    context.props.showProgramViews && !_.isEmpty(programViews) ? 'flex' : 'none'
-                }),
+              ListMenuItemHeader(strings.ProgramsHeaderText).setStyle({
+                display:
+                  context.props.showProgramViews && !_.isEmpty(programViews) ? 'flex' : 'none'
+              }),
               new ListMenuItem(strings.SelectProgramText)
                 .setItems(programViews)
                 .setIcon(Icons.ChevronLeft)
@@ -136,12 +129,15 @@ export function useToolbarItems(context: IPortfolioOverviewContext) {
                   )
                 })
             ],
-            checkedValues
+            {
+              views: [context.state.currentView?.id.toString()],
+              renderMode: context.state.isCompact ? ['compactList'] : ['list']
+            }
           ),
         context.props.showFilters &&
-          new ListMenuItem(null, strings.FilterText).setIcon('Filter').setOnClick(() => {
-            context.dispatch(TOGGLE_FILTER_PANEL())
-          })
+        new ListMenuItem(null, strings.FilterText).setIcon('Filter').setOnClick(() => {
+          context.dispatch(TOGGLE_FILTER_PANEL())
+        })
       ].filter(Boolean),
     [context.state, context.props]
   )
