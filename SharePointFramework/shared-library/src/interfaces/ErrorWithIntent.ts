f17cddec577c164bd20a7a24a91bdaad6bd51792
Ole Martin Pettersen <olemp@puzzlepart.com>
2025-01-17 12:22:14 +0100

    ErrorWithIntent

---
+++ SharePointFramework/shared-library/src/interfaces/ErrorWithIntent.ts
@@ -0,0 +1,11 @@
+import { MessageBarProps } from "@fluentui/react-components";
+
+export class ErrorWithIntent extends Error {
+    constructor(
+        public message: string,
+        public intent: MessageBarProps['intent'],
+        public name = 'Error'
+    ) {
+        super(message)
+    }
+}
