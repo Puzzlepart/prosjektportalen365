32cba75fe5d41ca2c0ab5dceae814d3bac426306
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2025-03-17 12:10:10 +0100

    Minor fixes and changes

--- SharePointFramework/PortfolioWebParts/src/components/PortfolioAggregation/ToolbarItems/useExcelExport.ts
+++ SharePointFramework/PortfolioWebParts/src/components/PortfolioAggregation/ToolbarItems/useExcelExport.ts
@@ -3,7 +3,6 @@ import _ from 'lodash'
 import ExcelExportService from 'pp365-shared-library/lib/services/ExcelExportService'
 import { useCallback } from 'react'
 import { IPortfolioAggregationContext } from '../context'
-import _ from 'lodash'
 
 /**
  * Hook that provides functionality for exporting data to Excel.
