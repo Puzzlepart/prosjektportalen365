42e6d6f9c6ccaab833952b280af529de4712ace2
Ole Martin Pettersen <olemp@puzzlepart.com>
2025-04-04 16:32:41 +0200

    build:  validateprojecttemplate.js task 🏗️

---
+++ Templates/.tasks/validateProjectTemplate.js
@@ -0,0 +1,28 @@
+// validateProjectTemplate.js
+const fs = require('fs')
+const path = require('path')
+
+// Example of token {{SiteFields_Project_Group}}
+const tokenRegex = /{{(.*?)}}/g
+const templates = {
+    DefaultTemplate: path.resolve(__dirname, '../Content/Portfolio_content.en-US/ProjectTemplates/DefaultTemplate.txt'),
+    Standardmal: path.resolve(__dirname, '../Content/Portfolio_content.no-NB/ProjectTemplates/Standardmal.txt'),
+}
+
+for (const key of Object.keys(templates)) {
+    const templatePath = templates[key]
+    const templateContent = fs.readFileSync(templatePath, 'utf-8')
+    let tokens = new Set()
+    let match
+    while ((match = tokenRegex.exec(templateContent)) !== null) {
+        tokens.add(match[1].trim())
+    }
+    // Sort tokens alphabetically and remove duplicates
+    tokens = new Set(Array.from(tokens).sort())
+
+
+    let markdownContent = `The template ${key} contains the following tokens that has not been found in the .resx files:\n\n`
+    markdownContent += Array.from(tokens).map(t => `- ${t}`).join('\n')
+
+    fs.writeFileSync(`${key}-tokens.md`, markdownContent, { flag: 'w' })
+}
