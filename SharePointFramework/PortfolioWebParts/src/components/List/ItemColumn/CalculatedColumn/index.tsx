c2de0055c8c85cb79a85a2898591d695d9001991
Srikrushna Patro <srikrushna.patro@crayon.no>
2025-11-04 10:59:07 +0100

    refactor: improve type definitions of calculated column class

--- SharePointFramework/PortfolioWebParts/src/components/List/ItemColumn/CalculatedColumn/index.tsx
+++ SharePointFramework/PortfolioWebParts/src/components/List/ItemColumn/CalculatedColumn/index.tsx
@@ -1,37 +1,30 @@
 import strings from 'PortfolioWebPartsStrings'
 import { tryParseCalculated } from 'pp365-shared-library'
-import React, { useMemo } from 'react'
+import React, { FC, useMemo } from 'react'
 import { BooleanColumn } from '../BooleanColumn'
 import { CurrencyColumn } from '../CurrencyColumn'
 import { DateColumn } from '../DateColumn'
 import { NumberColumn } from '../NumberColumn'
 import { ColumnRenderComponent } from '../types'
-import { ICalculatedColumnProps } from './types'
+import { ICalculatedColumnProps, ResultType } from './types'
+
+const componentRegistry: Record<ResultType, FC<ICalculatedColumnProps>> = {
+  currency: CurrencyColumn,
+  boolean: BooleanColumn,
+  date: DateColumn,
+  number: NumberColumn
+}
 
 export const CalculatedColumn: ColumnRenderComponent<ICalculatedColumnProps> = (props) => {
-  const { columnValue, resultType, includeTime } = props
+  const { columnValue, resultType } = props
   const parsedValue = useMemo(() => tryParseCalculated(columnValue, ''), [columnValue])
+  const Component = componentRegistry[resultType] ?? NumberColumn
   const commonProps = { ...props, columnValue: parsedValue }
-
-  switch (resultType) {
-    case 'currency':
-      return <CurrencyColumn {...commonProps} />
-
-    case 'boolean':
-      return <BooleanColumn {...commonProps} />
-
-    case 'date':
-      return <DateColumn {...commonProps} includeTime={includeTime} />
-
-    case 'number':
-    default:
-      return <NumberColumn {...commonProps} />
-  }
+  return <Component {...commonProps} />
 }
 
 CalculatedColumn.defaultProps = {
-  resultType: 'number',
-  includeTime: false
+  resultType: 'number'
 }
 
 CalculatedColumn.key = 'calculated'
