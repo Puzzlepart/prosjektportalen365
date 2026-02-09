cfa8e1ab1d1c91dda8587004fc07904fb83df180
Aadne <aadne.langedal@inmeta.no>
2026-02-09 12:29:52 +0100

    feat(projectwebparts): Collapse for multiple child projects

--- SharePointFramework/ProjectWebParts/src/components/ProjectInformation/ChildrenProjects/index.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectInformation/ChildrenProjects/index.tsx
@@ -1,15 +1,37 @@
 import strings from 'ProjectWebPartsStrings'
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { isEmpty } from 'underscore'
 import { useChildrenProjects } from './useChildrenProjects'
 import styles from './ChildrenProjects.module.scss'
 import { WebPartTitle, customLightTheme } from 'pp365-shared-library'
 import { Button, FluentProvider, IdPrefixProvider, useId } from '@fluentui/react-components'
-import { CubeRegular } from '@fluentui/react-icons'
+import { CubeRegular, ChevronRightFilled, ChevronDownFilled } from '@fluentui/react-icons'
+
+const COLLAPSE_TRESHOLD = 3
+const COLLAPSE_NUMBER = 2
 
 export const ChildrenProjects: FC = () => {
   const childProjects = useChildrenProjects()
   const fluentProviderId = useId('fp-children-projects-list')
+  const shouldCollapse = childProjects.length >= COLLAPSE_TRESHOLD
+  const [childProjectsStateArray, setChildProjectsStateArray] = useState(childProjects)
+  const chevronIcon = childProjectsStateArray.length > COLLAPSE_NUMBER ? <ChevronDownFilled/> : <ChevronRightFilled/>
+
+
+  useEffect(() => {
+    setChildProjectsStateArray(childProjects.slice(0,COLLAPSE_NUMBER))
+  },
+  [childProjects]
+  )
+
+function Collapse() {
+  const isExpanded = childProjectsStateArray.length === childProjects.length
+  if (isExpanded) {
+    setChildProjectsStateArray(childProjects.slice(0, COLLAPSE_NUMBER))
+  } else {
+    setChildProjectsStateArray(childProjects)
+  }
+}
 
   if (isEmpty(childProjects)) return null
 
@@ -19,15 +41,26 @@ export const ChildrenProjects: FC = () => {
         title={strings.ChildProjectsHeaderText}
         description={strings.ChildProjectsHeaderDescription}
       />
+      {shouldCollapse ? (
+        <Button
+          onClick={() => (Collapse())}
+          className={styles.button}
+          appearance='subtle'
+          iconPosition='before'
+          style={{ marginBottom: '8px' }}
+          icon={chevronIcon}
+        >
+          {childProjectsStateArray.length > COLLAPSE_NUMBER ? "Vis mindre" : "Vis flere"}
+        </Button>
+      ) : null}
       <IdPrefixProvider value={fluentProviderId}>
         <FluentProvider theme={customLightTheme}>
-          {childProjects.map((project, idx) => {
+          {childProjectsStateArray.map((project, idx) => {
             const onClick = () => {
               if (project.Path) {
                 window.open(project.Path, '_self')
               }
             }
-
             return (
               <Button
                 key={idx}
