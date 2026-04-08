09857d5f599ddcfdce32b94ce145d08fbbee7716
Tarjei Ormestøyl <tarjei.ormestoyl@crayon.no>
2026-04-08 09:00:50 +0200

    fix: More dynamic handling of displaying project status sections with content

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/useSections.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/useSections.tsx
@@ -1,6 +1,35 @@
 import { stringIsNullOrEmpty } from '@pnp/core'
-import { SectionType } from 'pp365-shared-library/lib/models'
+import { SectionModel, SectionType, StatusReport } from 'pp365-shared-library/lib/models'
+import { IProjectInformationData } from 'pp365-shared-library'
 import { useProjectStatusContext } from '../context'
+
+/**
+ * Check if a section has any content worth displaying.
+ *
+ * @param section Section model
+ * @param selectedReport Currently selected status report
+ * @param properties Project properties data
+ */
+function sectionHasContent(
+  section: SectionModel,
+  selectedReport: StatusReport,
+  properties: IProjectInformationData
+): boolean {
+  const { value, comment } = selectedReport?.getStatusValue(section.fieldName) ?? {}
+  if (!stringIsNullOrEmpty(value) || !stringIsNullOrEmpty(comment)) return true
+
+  if (section.type === SectionType.ProjectPropertiesSection && section.viewFields?.length > 0) {
+    const fieldValuesAsText = {
+      ...properties?.fieldValues?.['_fieldValuesAsText'],
+      ...selectedReport?.fieldValues?.['_fieldValuesAsText']
+    }
+    return section.viewFields.some(
+      (fieldName) => !stringIsNullOrEmpty(fieldValuesAsText?.[fieldName])
+    )
+  }
+
+  return false
+}
 
 /**
  * Component logic hook for `Sections`
@@ -10,6 +39,6 @@ export function useSections() {
   const { data, isDataLoaded, selectedReport } = context.state
   if (!isDataLoaded) return data.sections
   return data.sections
-    .filter((sec) => !stringIsNullOrEmpty(selectedReport?.getStatusValue(sec.fieldName)?.value))
+    .filter((sec) => sectionHasContent(sec, selectedReport, data.properties))
     .filter((sec) => sec.showAsSection || sec.type === SectionType.SummarySection)
 }
