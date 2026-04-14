e3df4b312b00ec347d0433142b2684678ca2b41e
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2026-04-14 15:41:31 +0200

    refactor: sectionHasContent into useSection hook

--- SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/useSections.tsx
+++ SharePointFramework/ProjectWebParts/src/components/ProjectStatus/Sections/useSections.tsx
@@ -1,44 +1,35 @@
 import { stringIsNullOrEmpty } from '@pnp/core'
-import { SectionModel, SectionType, StatusReport } from 'pp365-shared-library/lib/models'
-import { IProjectInformationData } from 'pp365-shared-library'
+import { SectionModel, SectionType } from 'pp365-shared-library/lib/models'
 import { useProjectStatusContext } from '../context'
 
 /**
- * Check if a section has any content worth displaying.
- *
- * @param section Section model
- * @param selectedReport Currently selected status report
- * @param properties Project properties data
- */
-function sectionHasContent(
-  section: SectionModel,
-  selectedReport: StatusReport,
-  properties: IProjectInformationData
-): boolean {
-  const { value, comment } = selectedReport?.getStatusValue(section.fieldName) ?? {}
-  if (!stringIsNullOrEmpty(value) || !stringIsNullOrEmpty(comment)) return true
-
-  if (section.type === SectionType.ProjectPropertiesSection && section.viewFields?.length > 0) {
-    const fieldValuesAsText = {
-      ...properties?.fieldValues?.['_fieldValuesAsText'],
-      ...selectedReport?.fieldValues?.['_fieldValuesAsText']
-    }
-    return section.viewFields.some(
-      (fieldName) => !stringIsNullOrEmpty(fieldValuesAsText?.[fieldName])
-    )
-  }
-
-  return false
-}
-
-/**
- * Component logic hook for `Sections`
+ * Component logic hook for `Sections`. Returns the sections that have
+ * displayable content and are configured to show, filtering against
+ * the currently selected status report and project properties.
  */
 export function useSections() {
   const context = useProjectStatusContext()
   const { data, isDataLoaded, selectedReport } = context.state
   if (!isDataLoaded) return data.sections
+
+  const sectionHasContent = (section: SectionModel): boolean => {
+    const { value, comment } = selectedReport?.getStatusValue(section.fieldName) ?? {}
+    if (!stringIsNullOrEmpty(value) || !stringIsNullOrEmpty(comment)) return true
+
+    if (section.type === SectionType.ProjectPropertiesSection && section.viewFields?.length > 0) {
+      const fieldValuesAsText = {
+        ...data.properties?.fieldValues?.['_fieldValuesAsText'],
+        ...selectedReport?.fieldValues?.['_fieldValuesAsText']
+      }
+      return section.viewFields.some(
+        (fieldName) => !stringIsNullOrEmpty(fieldValuesAsText?.[fieldName])
+      )
+    }
+
+    return false
+  }
+
   return data.sections
-    .filter((sec) => sectionHasContent(sec, selectedReport, data.properties))
+    .filter((sec) => sectionHasContent(sec))
     .filter((sec) => sec.showAsSection || sec.type === SectionType.SummarySection)
 }
