93e350406a0b14bfec7620178069a26b2b12f5c9
Remi Blom-Ohlsen <remigta2@gmail.com>
2026-06-22 09:54:35 +0200

    feat: archive channel

--- Install/Build-Release.ps1
+++ Install/Build-Release.ps1
@@ -20,7 +20,7 @@ Param(
     [Parameter(Mandatory = $false)]
     [switch]$SkipBundle,
     [Parameter(Mandatory = $false)]
-    [ValidateSet("test", "kurs", "i18n")]
+    [ValidateSet("test", "kurs", "i18n", "arkiv")]
     [string]$Channel,
     [Parameter(Mandatory = $false, HelpMessage = "Skip import of PnP.PowerShell module")]
     [switch]$SkipImportModule
